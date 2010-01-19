f21f1ce7136a229f86fdd5184e76db0f333cf8e2
Steve Fischer <sfischer@upenn.edu>
2010-01-19 11:35:25 +0000

    combine get and unpack into one step and make it idempotent
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@32960 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/GetAndUnpackResource.pm
@@ -0,0 +1,105 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::GetAndUnpackResource;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $commonTargetDir =  $self->getParamValue('commonTargetDir');
+    my $dataSourceName = $self->getParamValue('dataSourceName');
+    $dataSource = $self->getDataSource($dataSourceName);
+
+    my $localDataDir = $self->getLocalDataDir();
+    my $targetDir = "$localDataDir/$dataSourceName";
+
+    if ($undo) {
+      $self->runCmd(0, "rm -fr $targetDir");
+    } else {
+	my $commonDoneFlag;
+	if ($commonTargetDir) {
+	    $targetDir = $commonTargetDir;
+	    $commonDoneFlag = "$commonTargetDir/commonResourceAlreadyAcquired";
+	}
+	if ($commonDoneFlag && -e $commonDoneFlag) {
+	    $self->log("common resource already acquired");
+	} else {
+	    $self->getResource($test, $dataSource, $targetDir);
+	    $self->unpackResource($test, $dataSource, $targetDir);
+	    if ($commonDoneFlag) {
+		open(F, $commonDoneFlag) || die "Can't open common done flag '$commonDoneFlag'";
+		print F "common resource acquired  by step " . $self->getName() ."\n";
+		close(F);
+	    }
+	}
+    }
+}
+
+sub getResource {
+    my ($self, $test, $dataSource, $targetDir) = @_;
+
+    my $WgetArgs=  $dataSource->getWgetArgs();
+    my $manualArgs=  $dataSource->getManualArgs();
+    my $UrlArg=  $dataSource->getUrl();
+    
+    die "Resource $dataSourceName must provide either an URL and WgetArgs or ManualArgs, but not both\n"
+	unless 
+	(($WgetArgs && $UrlArg && !$manualArgs)
+	 || ($manualArgs && !$WgetArgs && !$manualArgs));
+
+    $self->runCmd(0,"mkdir -p $targetDir");
+
+    if ($WgetArgs) {
+	my $cmd = "wget --directory-prefix=$targetDir --output-file=$logFile $WgetArgs \"$UrlArg\"";
+	$self->runCmd($test, $cmd);
+    } else {
+	$manualArgs=~/.*="(.*)"/;
+	my $cmd='cp -r $manualArgs $targetDir';
+	$self->runCmd($test, $cmd);
+    }
+}
+
+sub unpackResource {
+    my ($self, $test, $dataSource) = @_;
+
+    my $unpacks =  $dataSource->getUnpacks();
+
+    my $unpackers;
+    if (ref($unpacks) eq 'ARRAY') {
+	$unpackers = $unpacks;
+    } else {
+	$unpackers = [$unpacks];
+    }
+  
+    map { _formatForCLI($_) } @$unpackers;
+
+    foreach my $unpacker (@$unpackers) {
+	if ($unpacker){  
+	    $self->runCmd($test,$unpacker);
+	}
+    }
+}
+
+# remove line wrappings for command line processing
+sub _formatForCLI {
+    $_[0] =~ s/\\$//gm;
+    $_[0] =~ s/[\n\r]+/ /gm;
+}
+
+
+sub getParamsDeclaration {
+    return (
+	'dataSourceName',
+	'commonTargetDir'
+           );
+}
+
+sub getConfigDeclaration {
+    return (
+           # [name, default, description]
+           );
+}
+
+
+
