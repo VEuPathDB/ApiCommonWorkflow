fa9b094aab475881779a329a2e9aceeb8b103200
Steve Fischer <sfischer@upenn.edu>
2010-01-23 02:09:29 +0000

    add tool property and handle citation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@33033 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/InsertAnalysisMethod.pm
+++ Main/lib/perl/WorkflowSteps/InsertAnalysisMethod.pm
@@ -10,18 +10,20 @@ sub run {
   my ($self, $test, $undo) = @_;
 
   my $name = $self->getParamValue('name');
+  my $version = $self->getParamValue('tool');
   my $version = $self->getParamValue('version');
   my $input = $self->getParamValue('input');
   my $output = $self->getParamValue('output');
   my $parameters = $self->getParamValue('parameters');
   my $description = $self->getParamValue('description');
   my $pubmedId = $self->getParamValue('pubmedId');
-  my $citationString = $self->getParamValue('citation');
   my $url = $self->getParamValue('url');
   my $credits = $self->getParamValue('credits');
 
+  my $citation = `pubmedIdToCitation $pubmedId`;
+  die "failed calling 'pubmedIdToCitation $pubmedId'" if $? >> 8;
 
-  my $args = "--name '$name' --version '$version' --input '$input' --output '$output' --parameters '$parameters' --description '$description' --pubmedId '$pubmedId' --citation '$citation' --url '$url' --credits '$credits' ";
+  my $args = "--name '$name' --tool $tool --version '$version' --input '$input' --output '$output' --parameters '$parameters' --description '$description' --pubmedId '$pubmedId' --citation '$citation' --url '$url' --credits '$credits' ";
 
   $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertAnalysisMethod",$args);
 
