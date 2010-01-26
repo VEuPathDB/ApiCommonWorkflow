c748fa4c51b9036c5e212894a467016f82685404
Steve Fischer <sfischer@upenn.edu>
2010-01-26 17:35:19 +0000

    add category arg
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@33050 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/InsertAnalysisMethod.pm
+++ Main/lib/perl/WorkflowSteps/InsertAnalysisMethod.pm
@@ -12,6 +12,7 @@ sub run {
   my $name = $self->getParamValue('name');
   my $tool = $self->getParamValue('tool');
   my $version = $self->getParamValue('version');
+  my $category = $self->getParamValue('category');
   my $input = $self->getParamValue('input');
   my $output = $self->getParamValue('output');
   my $parameters = $self->getParamValue('parameters');
@@ -23,7 +24,7 @@ sub run {
   my $citation = `pubmedIdToCitation $pubmedId`;
   die "failed calling 'pubmedIdToCitation $pubmedId'" if $? >> 8;
 
-  my $args = "--name '$name' --tool $tool --version '$version' --input '$input' --output '$output' --parameters '$parameters' --description '$description' --pubmedId '$pubmedId' --citation '$citation' --url '$url' --credits '$credits' ";
+  my $args = "--name '$name' --tool $tool --version '$version' --input '$input' --output '$output' --parameters '$parameters' --description '$description' --pubmedId '$pubmedId' --citation '$citation' --url '$url' --credits '$credits' --category '$category'";
 
   $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertAnalysisMethod",$args);
 
@@ -32,7 +33,9 @@ sub run {
 sub getParamsDeclaration {
     return (
 	'name',
+	'tool',
 	'version',
+	'category',
 	'input',
 	'output',
 	'parameters',
