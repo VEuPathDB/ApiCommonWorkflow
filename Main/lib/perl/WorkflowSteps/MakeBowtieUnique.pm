c2bae8e01e36cc43411ed876c31d6c54d1176113
Steve Fischer <sfischer@upenn.edu>
2010-04-14 19:42:50 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@34484 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeBowtieUnique.pm
@@ -0,0 +1,64 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeBowtieUnique;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $gnuFile = $self->getParamValue('inputGenomeNonUniqueFile');
+    my $tnuFile = $self->getParamValue('inputTranscriptNonUniqueFile');
+    my $guFile = $self->getParamValue('inputGenomeUniqueFile');
+    my $tuFile = $self->getParamValue('inputTranscriptUniqueFile');
+    my $buFile = $self->getParamValue('outputBowtieUniqueFile');
+    my $bnuFile = $self->getParamValue('outputBowtieNonUniqueFile');
+    my $readType = $self->getParamValue('readType');
+
+    $self->error() unless ($readType eq 'single' or $readType eq 'paired');
+
+    my $workflowDataDir = $self->getWorkflowDataDir();
+    my $stepDir = $self->getStepDir();
+
+    my $cmd = "merge_GU_and_TU.pl $gnuFile $tnuFile $guFile $tuFile $buFile $bnuFile $readType";
+
+    if ($undo) {
+	$self->runCmd(0, "rm -f $workflowDataDir/$buFile");
+	$self->runCmd(0, "rm -f $workflowDataDir/$bnuFile");
+    } else {
+	if ($test) {
+	    $self->testInputFile('inputTrancriptUniqueFile', "$workflowDataDir/$tuFile");
+	    $self->testInputFile('inputGenomeUniqueFile', "$workflowDataDir/$guFile");
+	    $self->testInputFile('inputTrancriptNonUniqueFile', "$workflowDataDir/$tnuFile");
+	    $self->testInputFile('inputGenomeNonUniqueFile', "$workflowDataDir/$gnuFile");
+
+	    $self->runCmd(0,"echo test > $workflowDataDir/$buFile");
+	    $self->runCmd(0,"echo test > $workflowDataDir/$bnuFile");
+
+	}
+	$self->runCmd($test, $cmd);
+
+    }
+}
+
+sub getParamsDeclaration {
+  return (
+      'inputGenomeNonUniqueFile',
+      'inputTranscriptNonUniqueFile',
+      'inputGenomeUniqueFile',
+      'inputTranscriptUniqueFile',
+      'outputBowtieUniqueFile',
+      'outputBowtieNonUniqueFile',
+      'readType',
+      );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
+
