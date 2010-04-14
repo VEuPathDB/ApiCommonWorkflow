9c2a43f811c1708bf640fc967165a7ad5337264f
Steve Fischer <sfischer@upenn.edu>
2010-04-14 16:50:19 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@34478 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/BowtieToUniqueAndNonUnique.pm
@@ -0,0 +1,58 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::BowtieToUniqueAndNonUnique;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $bowtieFile = $self->getParamValue('inputBowtieFile');
+    my $uFile = $self->getParamValue('outputUniqueFile');
+    my $nuFile = $self->getParamValue('outputNonuniqueFile');
+    my $transcriptOrGenome = $self->getParamValue('transcriptOrGenome');
+
+    my $workflowDataDir = $self->getWorkflowDataDir();
+    my $stepDir = $self->getStepDir();
+
+    my $c;
+    if ($transcriptOrGenome eq 'transcript') {$c = 'make_TU_and_TNU.pl'}
+    elsif ($transcriptOrGenome eq 'genome') {$c = 'make_GU_and_GNU.pl'}
+    else {$self->error("Illegal value for parameter 'transcriptOrGenome'.  Valid values are 'transcript' and 'genome".)}
+
+    my $cmd = "$c $bowtieFile $uFile $nuFile";
+
+    if ($undo) {
+	$self->runCmd(0, "rm -f $workflowDataDir/$gFile");
+	$self->runCmd(0, "rm -f $workflowDataDir/$nuFile");
+    } else {
+	if ($test) {
+	    $self->testInputFile('inputBowtieFile', "$workflowDataDir/$bowtieFile");
+
+	    $self->runCmd(0,"echo test > $workflowDataDir/$uFile");
+	    $self->runCmd(0,"echo test > $workflowDataDir/$nuFile");
+
+	}
+	$self->runCmd($test, $cmd);
+
+    }
+}
+
+sub getParamsDeclaration {
+  return (
+      'inputBowtieFile',
+      'outputUniqueFile',
+      'outputNonUniqueFile',
+      'transcriptOrGenome',
+      );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
+
