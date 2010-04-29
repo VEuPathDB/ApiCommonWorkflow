44ea6a0c4f6da0c892fe698f0f18fa7754c10ebb
Steve Fischer <sfischer@upenn.edu>
2010-04-29 15:01:57 +0000

    more expr stuff
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@34730 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/AnalyzeTranscriptExpression.pm
@@ -0,0 +1,47 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::AnalyzeTranscriptExpression;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $gnuFile = $self->getParamValue('configXmlFile');
+    my $tnuFile = $self->getParamValue('resourceDataDir');
+    my $cnuFile = $self->getParamValue('outputDir');
+
+    my $workflowDataDir = $self->getWorkflowDataDir();
+    my $stepDir = $self->getStepDir();
+
+    my $cmd = "doTranscriptExpression $workflowDataDir/$configXmlFile $workflowDataDir/$resourceDataDir $workflowDataDir/$outputDir";
+
+    if ($undo) {
+	$self->runCmd(0, "rm -f $workflowDataDir/$outputDir");
+    } else {
+	if ($test) {
+	    $self->testInputFile('', "$workflowDataDir/$configXmlFile");
+	    $self->testInputFile('', "$workflowDataDir/$resourceDataDir");
+	    $self->runCmd(0,"mkdir $workflowDataDir/$outputDir");
+	}
+	$self->runCmd($test, $cmd);
+    }
+}
+
+sub getParamsDeclaration {
+  return (
+      'configXmlFile',
+      'resourceDataDir',
+      'outputDir',
+      );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
+
