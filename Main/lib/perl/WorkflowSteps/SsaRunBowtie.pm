361a5a1f233c821695bdede0e192ed52153ffef1
Steve Fischer <sfischer@upenn.edu>
2010-05-12 16:39:34 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@35025 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/SsaRunBowtie.pm
@@ -0,0 +1,46 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::SsaRunBowtie;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $inputIndexesDir = $self->getParamValue('inputIndexesDir');
+    my $inputGeneModelFile = $self->getParamValue('inputShortSeqsFile');
+    my $outputFile = $self->getParamValue('outputFile');
+
+    my $workflowDataDir = $self->getWorkflowDataDir();
+    my $stepDir = $self->getStepDir();
+
+    my $cmd = "bowtie -a --best --strata -f $workflowDataDir/$inputIndexesDir $workflowDataDir/$inputShortSeqsFile -v 3 --suppress 6,7,8 -p 1 > $workflowDataDir/$outputFile";
+
+    if ($undo) {
+	$self->runCmd(0, "rm -f $workflowDataDir/$outputFile");
+    } else {
+	if ($test) {
+	    $self->testInputFile('inputCoverageFile', "$workflowDataDir/$inputIndexesDir");
+	    $self->testInputFile('inputGeneModelFile', "$workflowDataDir/$inputGeneShortSeqsFile");
+	    $self->runCmd(0,"echo test > $workflowDataDir/$outputFile");
+	}
+	$self->runCmd($test, $cmd);
+    }
+}
+
+sub getParamsDeclaration {
+  return (
+      'inputIndexesDir',
+      'inputShortSeqsFile',
+      'outputFile',
+      );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
+
