9d6b16a3da1b21bed8ab00bc67a99515c609660a
Steve Fischer <sfischer@upenn.edu>
2010-07-07 17:10:28 +0000

    initial coding
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@35892 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/InsertOrganismProject.pm
@@ -0,0 +1,33 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertOrganismProject;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $organism = $self->getParamValue('organism');
+  my $project = $self->getParamValue('project');
+
+  my $args = "--organism $organism --project $project'";
+
+  $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertOrganismProjectFeature", $args);
+
+}
+
+sub getParamDeclaration {
+  return (
+	  'organism',
+	  'project',
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
