e317cd177f7253374a2fa5b7246ed490d647df7b
Wei Li <weili1@pennmedicine.upenn.edu>
2010-07-13 17:00:01 +0000

    create cdf file for affy array
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@35996 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeCdfFileForAffyArray.pm
@@ -0,0 +1,51 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeCdfFileForAffyArray;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $gene2probesInputFile = $self->getParamValue('gene2probesInputFile');
+  my $probename2sequenceInputFile = $self->getParamValue('probename2sequenceInputFile');
+  my $cdfFile = $self->getParamValue('cdfFile');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd1 = "get_pbase-tbase.pl $workflowDataDir/$probename2sequenceInputFile 1 > $workflowDataDir/tbase-pbase.out"
+
+  my $cmd2 = "create_cdf.pl $workflowDataDir/$cdfFile $workflowDataDir/$gene2probesInputFile $workflowDataDir/tbase-pbase.out"
+
+
+
+  if ($undo) {
+    $self->runCmd(0, "rm -f $workflowDataDir/tbase-pbase.out");
+  } else {
+      if ($test) {
+	  $self->testInputFile('gene2probesInputFile', "$workflowDataDir/$gene2probesInputFile");
+	  $self->testInputFile('probename2sequenceInputFile', "$workflowDataDir/$probename2sequenceInputFile");
+	  $self->testInputFile('gene2probesInputFile', "$workflowDataDir/$gene2probesInputFile");
+      }else{
+	  $self->runCmd($test,$cmd1);
+	  $self->runCmd($test,$cmd2);
+      }
+  }
+}
+
+sub getParamDeclaration {
+  return (
+	  'gene2probesInputFile',
+	  'probename2sequenceInputFile',
+	  'gene2probesInputFile',
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
