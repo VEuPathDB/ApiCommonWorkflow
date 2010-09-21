d082b34fb083cbc0fc23f12120f3edab58a158e8
Wei Li <weili1@pennmedicine.upenn.edu>
2010-09-21 14:44:36 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@36935 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CopyExpressionProfilesToDownloadDir.pm
@@ -0,0 +1,50 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CopyExpressionProfilesToDownloadDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameters
+  my $copyFromDir = $self->getParamValue('copyFromDir');
+  my $copyToDir = $self->getParamValue('copyToDir');
+  my $configFile = $self->getParamValue('configFile');
+
+  my $apiSiteFilesDir = $self->getSharedConfig('apiSiteFilesDir');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  $self->runCmdOnCluster(0, "mkdir -p $apiSiteFilesDir/$copyToDir");
+
+  my $cmd = "CopyExpressionProfilesToDownloadDir --inputDir $workflowDataDir/$copyFromDir  --outputDir $apiSiteFilesDir/$copyToDir --configFile $workflowDataDir/$configFile";
+
+  if ($test) {
+    $self->testInputFile('copyFromDir', "$workflowDataDir/$copyFromDir");
+  }
+
+  if ($undo) {
+    $self->runCmd(0, "rm -fr $apiSiteFilesDir/$copyToDir");
+  } else {
+    $self->runCmd($test, $cmd);
+  }
+
+}
+
+sub getParamsDeclaration {
+  return (
+          'copyFromDir',
+          'copyToDir',
+          'configFile',
+         );
+}
+
+sub getConfigDeclaration {
+  return (
+         # [name, default, description]
+         # ['', '', ''],
+         );
+}
+
+
