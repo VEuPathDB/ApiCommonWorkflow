add7f68107498416da3b309c48a6cd8080078973
Deborah Pinney <pinney@upenn.edu>
2010-11-02 17:27:06 +0000

    changes made during run to fix errors
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@37529 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/SsaMakeIntensityFile.pm
+++ Main/lib/perl/WorkflowSteps/SsaMakeIntensityFile.pm
@@ -8,44 +8,42 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
     my ($self, $test, $undo) = @_;
 
-    my $inputCoverageFile = $self->getParamValue('inputCoverageFile');
+    my $inputSortedFile = $self->getParamValue('inputSortedFile');
     my $inputGeneModelFile = $self->getParamValue('inputGeneModelFile');
     my $outputIntensityFile = $self->getParamValue('outputIntensityFile');
 
     my $workflowDataDir = $self->getWorkflowDataDir();
     my $stepDir = $self->getStepDir();
 
-    my ($uniqueUnNorm,$nuUnNorm) = split(',',$inputCoverageFile);
-
-    my $cmd1 = "cat $workflowDataDir/$uniqueUnNorm $workflowDataDir/$nuUnNorm > $stepDir/unNormAll.cov";
+    my ($uniqueSorted,$nuSorted) = split(',',$inputSortedFile);
 
     my $tempOutputFile="$workflowDataDir/$outputIntensityFile"."ori";
 
-    my $cmd2 = "quantify_one_sample.pl $stepDir/unNormAll.cov $workflowDataDir/$inputGeneModelFile > $tempOutputFile";
+    my $cmd1 = "rum2quantifications.pl $workflowDataDir/$inputGeneModelFile $workflowDataDir/$uniqueSorted $workflowDataDir/$nuSorted $tempOutputFile";
 
-    my $cmd3 = "featurequant2geneprofiles.pl $workflowDataDir/$outputIntensityFile.ori -genes > $workflowDataDir/$outputIntensityFile";
+    my $cmd2 = "featurequant2geneprofiles.pl $workflowDataDir/$outputIntensityFile $tempOutputFile  -genes";
 
     if ($undo) {
-	$self->runCmd(0, "rm -f $workflowDataDir/$outputIntensityFile");
-	$self->runCmd(0, "rm -f $workflowDataDir/$tempOutputFile");
-	$self->runCmd(0, "rm -f $workflowDataDir/unNormAll.cov");
+	$self->runCmd(0, "rm -f $workflowDataDir/$outputIntensityFile.min");
+        $self->runCmd(0, "rm -f $workflowDataDir/$outputIntensityFile.max");
+        $self->runCmd(0, "rm -f $workflowDataDir/$outputIntensityFile.intori");
     } else {
 	if ($test) {
-	    $self->testInputFile('inputUniqueUnNormCoverageFile', "$workflowDataDir/$uniqueUnNorm");
-	    $self->testInputFile('inputNUUnNormCoverageFile', "$workflowDataDir/$nuUnNorm");
+	    $self->testInputFile('inputUniqueSortedFile', "$workflowDataDir/$uniqueSorted");
+	    $self->testInputFile('inputNUSortedFile', "$workflowDataDir/$nuSorted");
 	    $self->testInputFile('inputGeneModelFile', "$workflowDataDir/$inputGeneModelFile");
-	    $self->runCmd(0,"echo test > $workflowDataDir/$outputIntensityFile");
+	    $self->runCmd(0,"echo test > $workflowDataDir/$outputIntensityFile.min");
+            $self->runCmd(0,"echo test > $workflowDataDir/$outputIntensityFile.max");
 	}
 	$self->runCmd($test, $cmd1);
 	$self->runCmd($test, $cmd2);
-	$self->runCmd($test, $cmd3);
     }
 }
 
 sub getParamsDeclaration {
   return (
       'inputGeneModelFile',
-      'inputCoverageFile',
+      'inputSortedFile',
       'outputIntensityFile',
       );
 }
