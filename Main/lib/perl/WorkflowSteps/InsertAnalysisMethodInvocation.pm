6719f3d85d9ef811164d895769e77b9267b5d2ee
Steve Fischer <sfischer@upenn.edu>
2010-11-11 17:31:36 +0000

    change InsertAnalysisMethod.pm to InsertAnalysisMethodInvocation.pm
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@37651 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/InsertAnalysisMethodInvocation.pm
@@ -0,0 +1,53 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertAnalysisMethodInvocation;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $name = $self->getParamValue('name');
+  my $version = $self->getParamValue('version');
+  my $parameters = $self->getParamValue('parameters');
+
+  my $xmlFile = "$ENV{GUS_HOME}/lib/xml/analysisMethods.xml";
+
+  $self->_parseXmlFile($xmlFile);
+
+  if (!$self->{methods}->{$name} || !$self->{methods}->{$name}->{version} eq $version) {
+      $self->error("Can't resource '$name' with version '$version' in xml file '$xmlFile'");
+  }
+
+  my $args = "--name '$name' --version '$version'  --parameters '$parameters' ";
+
+  $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertAnalysisMethod",$args);
+
+}
+
+sub _parseXmlFile {
+  my ($self, $methodsXmlFile) = @_;
+
+  my $xml = new XML::Simple();
+  $self->{methods} = eval{ $xml->XMLin($xmlString, SuppressEmpty => undef, KeyAttr => 'method', ForceArray=>['method']) };
+#  print STDERR Dumper $self->{data};
+  $self->error("$@\n$xmlString\n") if($@);
+}
+
+
+sub getParamsDeclaration {
+    return (
+	'name',
+	'version',
+	'parameters',
+	);
+}
+
+sub getConfigDeclaration {
+  return (
+	 );
+}
+
+
