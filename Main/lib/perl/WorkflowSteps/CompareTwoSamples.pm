ac6500ab90201d51b77f425952e0752240da0fc3
Wei Li <weili1@pennmedicine.upenn.edu>
2011-03-14 16:55:30 +0000

    dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@39581 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CompareTwoSamples.pm
+++ Main/lib/perl/WorkflowSteps/CompareTwoSamples.pm
@@ -12,6 +12,7 @@ sub run {
     my $inputFilesDir = $self->getParamValue('inputFilesDir');
     my $outputDir = $self->getParamValue('outputDir');
     my $sampleNameList = $self->getParamValue('sampleNameList');
+    my $paired_end = $self->getParamValue('paired_end');
 
     my $workflowDataDir = $self->getWorkflowDataDir();
 
@@ -21,7 +22,7 @@ sub run {
 
     for(my $i =0; $i <= ($sampleNameNum-1); $i++){
 	for(my $j =$i+1 ; $j <= $sampleNameNum; $j++){
-	    my $command = "compare_two_samples.pl  $workflowDataDir/$inputFilesDir/$sampleNames[i].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[j].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[i].count $workflowDataDir/$inputFilesDir/$sampleNames[j].count 0  $workflowDataDir/$outputDir/$sampleNames[i]"."_vs_"."$sampleNames[j]  /home/ganesh/testFischerTest/ min";
+	    my $command = "compare_two_samples.pl  $workflowDataDir/$inputFilesDir/$sampleNames[i].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[j].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[i].count $workflowDataDir/$inputFilesDir/$sampleNames[j].count 0  $workflowDataDir/$outputDir/$sampleNames[i]"."_vs_"."$sampleNames[j]  /home/ganesh/testFischerTest/ min $paired_end";
 	    if ($undo) {
 		$self->runCmd(0, "rm -fr $workflowDataDir/$outputDir/*_vs_*");
 	    }else{
