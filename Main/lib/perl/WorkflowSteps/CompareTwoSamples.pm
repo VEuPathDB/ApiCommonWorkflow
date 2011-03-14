deb1954458696bf43ca55b6aa9577c919844a835
Wei Li <weili1@pennmedicine.upenn.edu>
2011-03-14 20:09:01 +0000

    dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@39598 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CompareTwoSamples.pm
+++ Main/lib/perl/WorkflowSteps/CompareTwoSamples.pm
@@ -22,12 +22,12 @@ sub run {
 
     for(my $i =0; $i <= ($sampleNameNum-1); $i++){
 	for(my $j =$i+1 ; $j <= $sampleNameNum; $j++){
-	    my $command = "compare_two_samples.pl  $workflowDataDir/$inputFilesDir/$sampleNames[i].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[j].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[i].count $workflowDataDir/$inputFilesDir/$sampleNames[j].count 0  $workflowDataDir/$outputDir/$sampleNames[i]"."_vs_"."$sampleNames[j]  /home/ganesh/testFischerTest/ min $paired_end";
+	    my $command = "compare_two_samples.pl  $workflowDataDir/$inputFilesDir/$sampleNames[$i].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[$j].mappingStats $workflowDataDir/$inputFilesDir/$sampleNames[$i].count $workflowDataDir/$inputFilesDir/$sampleNames[$j].count 0  $workflowDataDir/$outputDir/$sampleNames[$i]"."_vs_"."$sampleNames[$j]  /home/ganesh/testFischerTest/ min $paired_end";
 	    if ($undo) {
 		$self->runCmd(0, "rm -fr $workflowDataDir/$outputDir/*_vs_*");
 	    }else{
 		if ($test) {
-		    $self->runCmd(0,"echo hello > $workflowDataDir/$outputDir/$sampleNames[i]"."_vs_"."$sampleNames[j]");
+		    $self->runCmd(0,"echo hello > $workflowDataDir/$outputDir/$sampleNames[$i]"."_vs_"."$sampleNames[$j]");
 		}else{		
 		    $self->runCmd($test,$command);
 		}
