4884a970d46197c843be902c02c038a3e2587fa4
Wei Li <weili1@pennmedicine.upenn.edu>
2011-04-12 18:22:15 +0000

    new branch
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/trypDB-3.2@40191 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/InsertSpliceSiteFeatures.pm
@@ -0,0 +1,52 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertSpliceSiteFeatures;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $configFile = $self->getParamValue('configFile');
+
+  my $inputDir = $self->getParamValue('inputDir');
+
+  my $extDbRlsSpec = $self->getParamValue('extDbRlsSpec');
+
+  my $type = $self->getParamValue('type');
+
+  my $fileNames = $self->getParamValue('fileNames');
+
+  my ($extDbName,$extDbVer) = split(/\|/,$extDbRlsSpec);
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $args = "--dirPath $workflowDataDir/$inputDir --dirPath $workflowDataDir/$inputDir --configFile $workflowDataDir/$configFile  --extDbName $extDbName --extDbVer $extDbVer --type '$type'";
+
+  $args.= " --fileNames $fileNames" if $fileNames;
+  
+  if ($test) {
+    $self->testInputFile('inputDir', "$workflowDataDir/$inputDir");
+    $self->testInputFile('configFile', "$workflowDataDir/$configFile");
+  }
+
+  $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertSpliceSiteFeatures", $args);
+
+}
+
+sub getParamDeclaration {
+  return (
+	  'configFile',
+	  'inputDir',
+	  'extDbRlsSpec',
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
