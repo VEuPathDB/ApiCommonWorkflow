8cf9d4589eeba7f20ffee8d8b8518066c173aceb
Wei Li <weili1@pennmedicine.upenn.edu>
2011-04-21 13:54:42 +0000

    new dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@40354 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CopyMotifFile.pm
@@ -0,0 +1,42 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CopyMotifFile;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameters
+  my $inputFile = $self->getParamValue('inputFile');
+  my $outputFile = $self->getParamValue('outputFile');
+
+  if($undo) {
+    #$self->runCmd(0, "rm -f $outputFile*");
+  } else{
+      if($test){
+	  $self->testInputFile('inputFile', "$inputFile");
+	  $self->testInputFile('outputFile', "$outputFile");
+	  $self->runCmd(0, "echo test > $outputFile");
+      }else {
+	  $self->runCmd($test, "cp $inputFile $outputFile");	   
+       }
+  }
+}
+
+sub getParamsDeclaration {
+  return (
+          'inputFile',
+          'outputDir',
+          'args',
+          'formattedFileName',
+         );
+}
+
+sub getConfigDeclaration {
+  return (
+         # [name, default, description]
+         # ['', '', ''],
+         );
+}
+
