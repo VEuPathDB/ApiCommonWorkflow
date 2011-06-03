2f6ce59612d1ab584b2f6f0f6efbb68ec404ba86
Steve Fischer <sfischer@upenn.edu>
2011-06-03 18:41:32 +0000

    clean up
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@40927 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CopyPairwiseMercatorDirsToWebserviceDir.pm
+++ Main/lib/perl/WorkflowSteps/CopyPairwiseMercatorDirsToWebserviceDir.pm
@@ -8,27 +8,30 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
     my ($self, $test, $undo) = @_;
 
-
     my $mercatorDir = $self->getParamValue('mercatorDir');
-    my $outputDir   = $self->getParamValue('outputDir');
+
+    my $relativeDir = $self->getParamValue('relativeDir');
+
+    my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+
+    my $outputDir   = "$websiteFilesDir/$relativeDir/mercator";
+
     my $workflowDataDir = $self->getWorkflowDataDir();
     opendir(MERCATORDIR,"$workflowDataDir/$mercatorDir") or $self->error("Could not open $workflowDataDir/$mercatorDir for reading.\n");
 
-    
     foreach my $subDir (readdir MERCATORDIR){
 	next if ($subDir =~ m/^\./);
 	if ($undo) {
-	    $self->runCmd(0, "rm -fr $outputDir/*");
+	    $self->runCmd(0, "rm -fr $outputDir");
 	}else{
 	    if ($test) {
 		$self->runCmd(0,"mkdir -p $outputDir/$subDir");
-	    }else{		
+	    }else {
 		$self->runCmd($test,"mkdir -p $outputDir/$subDir");
 		$self->runCmd($test,"cp -R $workflowDataDir/$mercatorDir/$subDir/mercator-output/alignments $outputDir/$subDir");
-		$self->runCmd($test,"cp -R $workflowDataDir/$mercatorDir/$subDir/mercator-output/*.agp $outputDir/$subDir");		    
+		$self->runCmd($test,"cp -R $workflowDataDir/$mercatorDir/$subDir/mercator-output/*.agp $outputDir/$subDir");
 	    }
 	}
-	
     }
 
  
