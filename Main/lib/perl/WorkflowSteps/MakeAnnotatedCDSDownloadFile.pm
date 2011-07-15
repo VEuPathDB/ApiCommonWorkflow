9970fdeb068dbbb8f1cdb9e716f03295fe9c1218
Steve Fischer <sfischer@upenn.edu>
2011-07-15 20:35:17 +0000

    manual merge from trunk
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@41459 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeAnnotatedCDSDownloadFile.pm
+++ Main/lib/perl/WorkflowSteps/MakeAnnotatedCDSDownloadFile.pm
@@ -28,35 +28,35 @@ sub getDownloadFileCmd {
     my $deprecatedGene;
 
     my $sql = <<"EOF";
-    SELECT '$organismSource'
-	||'|'||
-	gf.source_id
-	|| decode(gf.is_deprecated, 1, ' | deprecated=true', '')
-	||' | organism='||
-	replace( gf.organism, ' ', '_')
-	||' | product='||
-	gf.product
-	||' | location='||
-	fl.sequence_source_id
-	||':'||
-	(fl.start_min + taaf.translation_start - 1)
-	||'-'||
-	(fl.end_max - (snas.length - taaf.translation_stop))
-	||'('||
-	decode(fl.is_reversed, 1, '-', '+')
-	||') | length='||
-	(taaf.translation_stop - taaf.translation_start + 1)
-	as defline,
-	SUBSTR(snas.sequence,
-	       taaf.translation_start,
-	       taaf.translation_stop - taaf.translation_start + 1)
-	FROM apidb.featurelocation fl,
-	apidb.geneattributes gf,
-	dots.transcript t,
-	dots.splicednasequence snas,
-	dots.translatedaafeature taaf,
-	dots.nasequence ns
-	WHERE gf.na_feature_id = t.parent_id
+     SELECT '$organismSource'
+                ||'|'||
+            gf.source_id
+                 || decode(gf.is_deprecated, 1, ' | deprecated=true', '')
+                 ||' | organism='||
+           replace( gf.organism, ' ', '_')
+                ||' | product='||
+            gf.product
+                ||' | location='||
+            fl.sequence_source_id
+                ||':'||
+            least(gf.coding_start,gf.coding_end)
+                ||'-'||
+	    greatest(gf.coding_start,gf.coding_end)
+                ||'('||
+            decode(fl.is_reversed, 1, '-', '+')
+                ||') | length='||
+            (abs(gf.coding_start - gf.coding_end) + 1)
+            as defline,
+           SUBSTR(snas.sequence,
+                  taaf.translation_start,
+                  taaf.translation_stop - taaf.translation_start + 1)
+           FROM apidb.featurelocation fl,
+                apidb.geneattributes gf,
+                dots.transcript t,
+                dots.splicednasequence snas,
+                dots.translatedaafeature taaf,
+                dots.nasequence ns
+      WHERE gf.na_feature_id = t.parent_id
         AND fl.na_sequence_id = ns.na_sequence_id
         AND t.na_sequence_id = snas.na_sequence_id
         AND gf.na_feature_id = fl.na_feature_id
