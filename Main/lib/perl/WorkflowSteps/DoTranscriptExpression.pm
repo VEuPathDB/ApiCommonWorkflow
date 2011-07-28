62bd503b19051fe43e3d098ec824b429462fdfc7
Steve Fischer <sfischer@upenn.edu>
2011-07-28 15:25:59 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@41708 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/DoTranscriptExpression.pm
+++ Main/lib/perl/WorkflowSteps/DoTranscriptExpression.pm
@@ -11,38 +11,48 @@ sub run {
 
   my $inputDir = $self->getParamValue('inputDir');
   my $outputDir = $self->getParamValue('outputDir');
-  my $geneProbeMappingFile = $self->getParamValue('geneProbeMappingFile');
+  my $geneProbeMappingTabFile = $self->getParamValue('geneProbeMappingTabFile');
+  my $geneProbeMappingVendorFile = $self->getParamValue('geneProbeMappingVendorFile');
+  my $expectCdfFile = $self->getParamValue('expectCdfFile');
+  my $expectNdfFile = $self->getParamValue('expectNdfFile');
+
+  my $mappingFile = ($expectCdfFile || $expectNdfFile)?
+      $geneProbeMappingVendorFile : $geneProbeMappingTabFile;
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
   my $cmd = <<"EOF";
 doTranscriptExpression.pl --xml_file $workflowDataDir/$inputDir/analysisConfig.xml \\
 --main_directory $workflowDataDir/$outputDir \\
---input_file $workflowDataDir/$geneProbeMappingFile
+--input_file $workflowDataDir/$mappingFile
 EOF
 
   if ($undo) {
-    $self->runCmd(0, "rm -f $workflowDataDir/$outputDir/*.txt");
+    $self->runCmd(0, "rm -f $workflowDataDir/$outputDir");
   } else {
+      $self->runCmd(0, "mkdir $workflowDataDir/$outputDir");
       if ($test) {
 	  $self->testInputFile('inputDir', "$workflowDataDir/$inputDir");
-	  $self->testInputFile('geneProbeMappingFile', "$workflowDataDir/$geneProbeMappingFile");
+	  $self->testInputFile('geneProbeMappingFile', "$workflowDataDir/$mappingFile");
 	  $self->runCmd(0,"echo test > $workflowDataDir/$outputDir/profiles.txt");
-      }else{
+      } else {
 	  makeSymLinks($inputDir, $outputDir);
 	  $self->runCmd($test,$cmd);
       }
   }
 }
 
+# initialize output dir with symlinks to all files in input dir
+# this is needed by doTranscriptExpresssion command
 sub makeSymLinks {
   my ($inputDir, $outputDir) = @_;
 
-  opendir(my $dh, $inputDir) || die "can't opendir $inputDir: $!";
+  opendir(my $dh, "$workflowDataDir/$inputDir") || $self->error("can't opendir $workflowDataDir/$inputDir: $!");
   while(readdir($dh)) {
+      next if /^\.+/;
+      symlink("$workflowDataDir/$inputDir/$_", "$workflowDataDir/$outputDir/$_") || $self->error("Can't make symlink from $workflowDataDir/$inputDir/$_ to $workflowDataDir/$outputDir/$_");
   }
     closedir $dh;
-
 }
 
 sub getConfigDeclaration {
