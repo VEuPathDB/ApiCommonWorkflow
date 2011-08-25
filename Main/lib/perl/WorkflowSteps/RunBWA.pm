4e48fd1f67c6dbffb1b8a5e4630e751fc609ebc1
Steve Fischer <sfischer@upenn.edu>
2011-08-25 17:37:19 +0000

    implement hts snps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@42195 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/RunBWA.pm
+++ Main/lib/perl/WorkflowSteps/RunBWA.pm
@@ -11,23 +11,27 @@ sub run {
     my $genomicSeqsFile = $self->getParamValue('genomicSeqsFile');
     my $inputShortSeqsFile = $self->getParamValue('inputShortSeqsFile');
     my $pairedReadFile = $self->getParamValue('pairedReadFile');
+    my $hasPairedReads = $self->getParamValue('hasPairedReads');
     my $outputFile = $self->getParamValue('outputFile');
     my $inputIndexesDir = $self->getParamValue('inputIndexesDir');
     my $strain = $self->getParamValue('strain');
-    my $percentCutoff = $self->getParamValue('percentCutoff');
-    my $minPercentCutoff = $self->getParamValue('minPercentCutoff');
+    my $isHaploid = $self->getParamValue('isHaploid');
     my $varScanJarFile = $self->getConfig('varScanJarFile');
     my $workflowDataDir = $self->getWorkflowDataDir();
     my $stepDir = $self->getStepDir();
 
+    my $minPercentCutoff = $isHaploid? 51 : 34
+    my $percentCutoff = $isHaploid? 80 : 40
+
     my $cmd = "runBWA_HTS.pl --mateA $workflowDataDir/$inputShortSeqsFile --fastaFile $workflowDataDir/$genomicSeqsFile --bwaIndex $workflowDataDir/$inputIndexesDir/genomicIndexes --strain $strain --outputPrefix $workflowDataDir/$outputFile --percentCutoff $percentCutoff --minPercentCutoff $minPercentCutoff --varscan $varScanJarFile";
-    $cmd .= " --mateB $workflowDataDir/$pairedReadFile" if ($pairedReadFile);
+    $cmd .= " --mateB $workflowDataDir/$pairedReadFile" if ($hasPairedReads);
     if ($undo) {
 	$self->runCmd(0, "rm -f $workflowDataDir/$outputFile.*");
     } else {
 	if ($test) {
 	    $self->testInputFile('inputCoverageFile', "$workflowDataDir/$inputIndexesDir");
 	    $self->testInputFile('inputShortSeqsFile', "$workflowDataDir/$inputShortSeqsFile");
+	    $self->testInputFile('inputShortSeqsFile', "$workflowDataDir/$pairedReadFile") if ($hasPairedReads);
 	    $self->runCmd(0,"echo test > $workflowDataDir/$outputFile.gff");
 	}
 	$self->runCmd($test, $cmd);
