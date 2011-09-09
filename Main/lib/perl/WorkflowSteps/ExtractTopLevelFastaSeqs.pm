f909b48bf22eed368fdae3e58dd9ee2b707728bc
Vishal Nayak <vishalkn@_.com>
2011-09-09 14:44:19 +0000

    changed sql query to generate top level sequences
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@42411 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/ExtractTopLevelFastaSeqs.pm
+++ Main/lib/perl/WorkflowSteps/ExtractTopLevelFastaSeqs.pm
@@ -13,7 +13,7 @@ sub run {
   my $outputFile = $self->getParamValue('outputFile');
 
 
-  my $sql = "select sa.na_sequence_id,ns.sequence from apidbtuning.sequenceattributes sa, dots.nasequence ns where sa.na_sequence_id in (select na_sequence_id from apidbtuning.featurelocation where is_top_level = 1) and sa.na_sequence_id = ns.na_sequence_id and sa.organism in ('$organismFullName')";
+  my $sql = "select sa.source_id,ns.sequence from apidbtuning.sequenceattributes sa, dots.nasequence ns where sa.na_sequence_id in (select na_sequence_id from apidbtuning.featurelocation where is_top_level = 1) and sa.na_sequence_id = ns.na_sequence_id and sa.organism in ('$organismFullName')";
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
