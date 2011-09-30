0e6068609c4eaa25bbd7c2419a536b776119ae1a
John Iodice <iodice@pennmedicine.upenn.edu>
2011-09-30 13:25:58 +0000

    GeneAttributes.product may not be populated -- go to primary tables for gene product
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@42889 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeAnnotatedCDSDownloadFile.pm
+++ Main/lib/perl/WorkflowSteps/MakeAnnotatedCDSDownloadFile.pm
@@ -18,34 +18,58 @@ sub getDownloadFileCmd {
   my $soIds =  $self->getSoIds($test, $self->getParamValue('soTermIdsOrNames')) if $self->getParamValue('soTermIdsOrNames');
 
     my $sql = <<"EOF";
-     SELECT '$organismSource'
-                ||'|'||
-            gf.source_id
-                 || decode(gf.is_deprecated, 1, ' | deprecated=true', '')
-                 ||' | organism='||
-           replace( gf.organism, ' ', '_')
-                ||' | product='||
-            gf.product
-                ||' | location='||
-            fl.sequence_source_id
-                ||':'||
-            least(gf.coding_start,gf.coding_end)
-                ||'-'||
-	    greatest(gf.coding_start,gf.coding_end)
-                ||'('||
-            decode(fl.is_reversed, 1, '-', '+')
-                ||') | length='||
-            (abs(gf.coding_start - gf.coding_end) + 1)
+     select '$organismSource' || '|' || gf.source_id
+            || decode(gf.is_deprecated, 1, ' | deprecated=true', '')
+            || ' | organism=' || replace( gf.organism, ' ', '_')
+            || ' | product=' || product_name.product || ' | location='
+            || fl.sequence_source_id || ':'
+            || least(gf.coding_start,gf.coding_end) ||'-'
+            || greatest(gf.coding_start,gf.coding_end)
+            || '('|| decode(fl.is_reversed, 1, '-', '+') || ') | length='
+            || (abs(gf.coding_start - gf.coding_end) + 1)
             as defline,
-           SUBSTR(snas.sequence,
+           substr(snas.sequence,
                   taaf.translation_start,
-                  taaf.translation_stop - taaf.translation_start + 1)
-           FROM ApidbTuning.FeatureLocation fl,
+                  taaf.translation_stop - taaf.translation_start + 1) as sequence
+           from ApidbTuning.FeatureLocation fl,
                 ApidbTuning.GeneAttributes gf,
                 dots.transcript t,
                 dots.splicednasequence snas,
                 dots.translatedaafeature taaf,
-                dots.nasequence ns
+                dots.nasequence ns,
+                (select gf.na_feature_id,
+                        substr(coalesce(preferred_product.product, any_product.product, gf.product, 'unspecified product'),
+                               1, 300)
+                        || case
+                             when (coalesce(preferred_name.name, any_name.name) is not null)
+                             then ' (' || coalesce(preferred_name.name, any_name.name) || ')'
+                             else ''
+                            end
+                        as product
+                 from dots.GeneFeature gf,
+                      (select na_feature_id, max(product) as product
+                       from apidb.GeneFeatureProduct
+                       where is_preferred = 1
+                       group by na_feature_id
+                      ) preferred_product,
+                      (select na_feature_id, max(product) as product
+                       from apidb.GeneFeatureProduct
+                       group by na_feature_id
+                      ) any_product,
+                      (select na_feature_id, max(name) as name
+                       from apidb.GeneFeatureName
+                       where is_preferred = 1
+                       group by na_feature_id
+                      ) preferred_name,
+                      (select na_feature_id, max(name) as name
+                       from apidb.GeneFeatureName
+                       group by na_feature_id
+                      ) any_name
+                 where gf.na_feature_id = preferred_product.na_feature_id(+)
+                   and gf.na_feature_id = any_product.na_feature_id(+)
+                   and gf.na_feature_id = preferred_name.na_feature_id(+)
+                   and gf.na_feature_id = any_name.na_feature_id(+)
+                ) product_name
       WHERE gf.na_feature_id = t.parent_id
         AND fl.na_sequence_id = ns.na_sequence_id
         AND t.na_sequence_id = snas.na_sequence_id
@@ -56,6 +80,7 @@ sub getDownloadFileCmd {
         AND t.na_feature_id = taaf.na_feature_id
         AND fl.is_top_level = 1
         AND gf.is_deprecated = $deprecated
+        and gf.na_feature_id = product_name.na_feature_id
 EOF
 
 	$sql .= " and ns.sequence_ontology_id in ($soIds)" if $soIds;
