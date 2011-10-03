fe2da16c7b2ddc17388dcbc4b8727150702ad13b
Deborah Pinney <pinney@upenn.edu>
2011-10-03 19:56:05 +0000

    get rid of requirement for predicted transcript, plugin will get all mRNA,ESTs, and transcripts and make assemblysequences
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@42934 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeAssemblySeqs.pm
+++ Main/lib/perl/WorkflowSteps/MakeAssemblySeqs.pm
@@ -10,8 +10,6 @@ sub run {
 
   my $organismAbbrev = $self->getParamValue('organismAbbrev');
   my $useTaxonHierarchy = $self->getParamValue('useTaxonHierarchy');
-#  my $predictedTranscriptsExtDbRlsSpec = $self->getParamValue('predictedTranscriptsExtDbRlsSpec');
-  my $predictedTranscriptsExtDbRlsSpec = "FIX THIS see redmine #4306";
 
   my $vectorFile = $self->getConfig('vectorFile');
   my $phrapDir = $self->getConfig('phrapDir');
@@ -20,27 +18,6 @@ sub run {
   my $taxonIdList = $self->getTaxonIdList($test, $taxonId, $useTaxonHierarchy);
 
   my $args = "--taxon_id_list '$taxonIdList' --repeatFile $vectorFile --phrapDir $phrapDir";
-
-  if($predictedTranscriptsExtDbRlsSpec){
-      
-      my ($extDbName, $extDbVer) = $self->getExtDbInfo($test,$predictedTranscriptsExtDbRlsSpec);
-
-      my $sql = "SELECT e.na_sequence_id 
-               from dots.ExternalNASequence e, sres.sequenceontology s,
-                    sres.externalDatabase d, sres.externalDatabaseRelease r
-              where e.taxon_id in($taxonIdList)
-                and e.external_database_release_id = r.external_database_release_id
-                and r.external_database_id = d.external_database_id
-                and r.version = '$extDbVer'
-                and d.name ='$extDbName'
-                and s.term_name IN ('transcript')
-                and e.sequence_ontology_id = s.sequence_ontology_id
-                and e.na_sequence_id not in
-                    (select a.na_sequence_id from dots.AssemblySequence a)";
-
-      $args .= " --idSQL \"$sql\"";
-
-}
 
 
   $self->runPlugin($test, $undo, "DoTS::DotsBuild::Plugin::MakeAssemblySequences", $args);
