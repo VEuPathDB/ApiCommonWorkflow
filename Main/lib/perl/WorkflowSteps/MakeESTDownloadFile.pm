ebd95d3287bb8ad1f06ef5d7dbc5b69a0a40ebb6
Deborah Pinney <pinney@upenn.edu>
2011-10-07 11:41:35 +0000

    adjust getting taxonIdList, was not succeeding
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@43010 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeESTDownloadFile.pm
+++ Main/lib/perl/WorkflowSteps/MakeESTDownloadFile.pm
@@ -8,14 +8,13 @@ sub getIsSpeciesLevel {
     return 1;
 }
 
-sub getDownloadFileCmd {
-    my ($self, $downloadFileName, $test) = @_;
+sub getDownloadFileCmd { 
+  my ($self, $downloadFileName, $test) = @_;
 
-    my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
 
-    my $taxonId = $self->getOrganismInfo($test, $organismAbbrev)->getSpeciesTaxonId();
-    my $taxonIdList = $self->getTaxonIdList($test, $taxonId);
-
+  my $taxonId = $self->getOrganismInfo($test,$organismAbbrev)->getSpeciesTaxonId();
+  my $taxonIdList =$self->getOrganismInfo($test, $organismAbbrev)->getTaxonIdList($taxonId);
 
     my $sql = <<"EOF";
     SELECT x.source_id
@@ -32,7 +31,7 @@ sub getDownloadFileCmd {
 	AND t.taxon_id = tn.taxon_id
 	AND tn.name_class = 'scientific name'
 	AND t.taxon_id = x.taxon_id
-	AND x.sequence_ontology_id = so.sequence_ontology_id
+	AND x.sequence_ontology_id = so.sequence_ontology_idemacs 
 	AND so.term_name = 'EST'
 EOF
 
