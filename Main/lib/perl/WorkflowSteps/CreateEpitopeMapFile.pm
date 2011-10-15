2cbacd300dd4135479aa516bb56180a8d6d69ceb
Steve Fischer <sfischer@upenn.edu>
2011-10-15 01:47:45 +0000

    upgrade by running commands directly, retiring the createEpitopesMappingFile script
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@43161 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CreateEpitopeMapFile.pm
+++ Main/lib/perl/WorkflowSteps/CreateEpitopeMapFile.pm
@@ -9,40 +9,49 @@ sub run {
     my ($self, $test, $undo) = @_;
 
 
-    my $inputDir = $self->getParamValue('inputDir');
-    my $queryDir = $self->getParamValue('queryDir');
+    my $inputTabFile = $self->getParamValue('inputTabFile');
+    my $dataDir = $self->getParamValue('dataDir');
     my $proteinsFile = $self->getParamValue('proteinsFile');
     my $blastDbDir = $self->getParamValue('blastDbDir');
-    my $outputDir = $self->getParamValue('outputDir');
+    my $outputMatchesFile = $self->getParamValue('outputMatchesFile');
     my $idRegex = $self->getParamValue('idRegex');
 
     my $workflowDataDir = $self->getWorkflowDataDir();
     my $ncbiBlastPath = $self->getConfig('ncbiBlastPath');
 
-    my $cmd = "createEpitopeMappingFileWorkflow  --ncbiBlastPath $ncbiBlastPath --inputDir $workflowDataDir/$inputDir --queryDir $workflowDataDir/$queryDir --outputDir $workflowDataDir/$outputDir --blastDatabase $workflowDataDir/$blastDbDir/AnnotatedProteins.fsa --idRegex '$idRegex' --subjectFile $workflowDataDir/$proteinsFile";
 
+    my $tabFile = "$workflowDataDir/$inputTabFile";
+    my $matchesFile = "$workflowDataDir/$outputMatchesFile";
+    my $fastaFile = "$workflowDataDir/epitopesGenpept.fasta";
 
+    # use the epitope accessions to find full length peptide seqs from genpept
+    my $cmd1 = "retrieveSeqsFromGenPept --inFile $tabFile --outFile $fastaFile";
+
+    # use those to blast against subject proteins
+    my $cmd2 = "runAndParseEpitopeBlast --ncbiBlastPath $ncbiBlastPath --queryFile $fastaFile --database $blastDbDir --epitopeFile $tabFile --outputFile $matchesFile";
+    $cmd2 .= " --regex '$idRegex'" if $idRegex;
+
+    # also find exact matches of short epitope sequences and subject proteins
+    # add them to the matches file
+    my $cmd3 = "exactMapEpitopes --subjectFile $proteinsFile --epitopeFile $tabFile --outputFile $matchesFile --rejectDuplicates";
+
+    #TODO: Make sure we don't get exact matches for epitope:sequence pairs that we have matches on BLAST hits for
 
     if ($undo) {
-      $self->runCmd(0, "rm -fr $workflowDataDir/$outputDir");
+      $self->runCmd(0, "rm -fr $matchesFile");
+      $self->runCmd(0, "rm -fr $fastaFile");
     } else {
 	if ($test) {
-	    $self->testInputFile('proteinsFile', "$workflowDataDir/$proteinsFile");
-	    $self->testInputFile('inputDir', "$workflowDataDir/$inputDir");
-	    $self->runCmd(0,"mkdir -p $workflowDataDir/$outputDir");
-	    $self->runCmd(0,"echo hello > $workflowDataDir/$outputDir/IEDBExport.out");
+	  $self->testInputFile('proteinsFile', "$workflowDataDir/$proteinsFile");
+	  $self->testInputFile('inputTabFile', "$tabFile");
 	}else{
-	    $self->runCmd($test, $cmd);
+	  $self->error("Output file '$matchesFile' already exists") if -e $matchesFile;
+	  $self->error("Output file '$fastaFile' already exists") if -e $fastaFile;
+	  $self->runCmd($test, $cmd1);
+	  $self->runCmd($test, $cmd2);
+	  $self->runCmd($test, $cmd3);
 	}
     }
-}
-
-sub getParamsDeclaration {
-    return ('inputDir',
-            'blastDbDir',
-            'proteinsFile',
-            'outputDir'
-           );
 }
 
 
