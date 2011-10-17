63bcaaee094bf00f1591c21e5ae93d14b9d8960f
Steve Fischer <sfischer@upenn.edu>
2011-10-17 02:26:27 +0000

    debug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@43166 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CreateEpitopeMapFile.pm
+++ Main/lib/perl/WorkflowSteps/CreateEpitopeMapFile.pm
@@ -13,7 +13,7 @@ sub run {
     my $dataDir = $self->getParamValue('dataDir');
     my $proteinsFile = $self->getParamValue('proteinsFile');
     my $blastDbDir = $self->getParamValue('blastDbDir');
-    my $outputMatchesFile = $self->getParamValue('outputMatchesFile');
+    my $outputMappingFile = $self->getParamValue('outputMappingFile');
     my $idRegex = $self->getParamValue('idRegex');
 
     my $workflowDataDir = $self->getWorkflowDataDir();
@@ -21,31 +21,31 @@ sub run {
 
 
     my $tabFile = "$workflowDataDir/$inputTabFile";
-    my $matchesFile = "$workflowDataDir/$outputMatchesFile";
+    my $mappingFile = "$workflowDataDir/$outputMappingFile";
     my $fastaFile = "$workflowDataDir/epitopesGenpept.fasta";
 
     # use the epitope accessions to find full length peptide seqs from genpept
     my $cmd1 = "retrieveSeqsFromGenPept --inFile $tabFile --outFile $fastaFile";
 
     # use those to blast against subject proteins
-    my $cmd2 = "runAndParseEpitopeBlast --ncbiBlastPath $ncbiBlastPath --queryFile $fastaFile --database $blastDbDir --epitopeFile $tabFile --outputFile $matchesFile";
+    my $cmd2 = "runAndParseEpitopeBlast --ncbiBlastPath $ncbiBlastPath --queryFile $fastaFile --database $blastDbDir --epitopeFile $tabFile --outputFile $mappingFile";
     $cmd2 .= " --regex '$idRegex'" if $idRegex;
 
-    # also find exact matches of short epitope sequences and subject proteins
-    # add them to the matches file
-    my $cmd3 = "exactMapEpitopes --subjectFile $proteinsFile --epitopeFile $tabFile --outputFile $matchesFile --rejectDuplicates";
+    # also find exact mapping of short epitope sequences and subject proteins
+    # add them to the mapping file
+    my $cmd3 = "exactMapEpitopes --subjectFile $proteinsFile --epitopeFile $tabFile --outputFile $mappingFile --rejectDuplicates";
 
-    #TODO: Make sure we don't get exact matches for epitope:sequence pairs that we have matches on BLAST hits for
+    #TODO: Make sure we don't get exact mapping for epitope:sequence pairs that we have mapping on BLAST hits for
 
     if ($undo) {
-      $self->runCmd(0, "rm -fr $matchesFile");
+      $self->runCmd(0, "rm -fr $mappingFile");
       $self->runCmd(0, "rm -fr $fastaFile");
     } else {
 	if ($test) {
 	  $self->testInputFile('proteinsFile', "$workflowDataDir/$proteinsFile");
 	  $self->testInputFile('inputTabFile', "$tabFile");
 	}else{
-	  $self->error("Output file '$matchesFile' already exists") if -e $matchesFile;
+	  $self->error("Output file '$mappingFile' already exists") if -e $mappingFile;
 	  $self->error("Output file '$fastaFile' already exists") if -e $fastaFile;
 	  $self->runCmd($test, $cmd1);
 	  $self->runCmd($test, $cmd2);
