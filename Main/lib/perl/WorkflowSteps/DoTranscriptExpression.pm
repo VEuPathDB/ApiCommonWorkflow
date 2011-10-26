8a9ce152950d7626aad7b8358aeb44304491c98d
Wei Li <weili1@pennmedicine.upenn.edu>
2011-10-26 18:53:59 +0000

    new dev to make input file optional
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@43366 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/DoTranscriptExpression.pm
+++ Main/lib/perl/WorkflowSteps/DoTranscriptExpression.pm
@@ -14,11 +14,9 @@ sub run {
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
-  my $cmd = <<"EOF";
-doTranscriptExpression.pl --xml_file $workflowDataDir/$inputDir/analysisConfig.xml \\
---main_directory $workflowDataDir/$inputDir/ \\
---input_file $workflowDataDir/$probeGenesMappingFile
-EOF
+  my $cmd = "doTranscriptExpression.pl --xml_file $workflowDataDir/$inputDir/analysisConfig.xml --main_directory $workflowDataDir/$inputDir/";
+
+  $cmd .= " --input_file $workflowDataDir/$probeGenesMappingFile" if $probeGenesMappingFile;
 
   if ($undo) {
     $self->runCmd(0, "rm -f $workflowDataDir/$inputDir/*.txt");
