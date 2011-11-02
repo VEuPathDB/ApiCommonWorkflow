ca06a1226a3394e62d5af13fd54b45cd15151802
Deborah Pinney <pinney@upenn.edu>
2011-11-02 14:07:44 +0000

    use getSoId to be consistent with other steps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@43515 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeMixedGenomicDownloadFile.pm
+++ Main/lib/perl/WorkflowSteps/MakeMixedGenomicDownloadFile.pm
@@ -13,9 +13,12 @@ sub getDownloadFileCmd {
     my $organismAbbrev = $self->getParamValue('organismAbbrev');
     my $ncbiTaxonId = $self->getOrganismInfo($organismAbbrev)->getNcbiTaxonId();
 
-    my $cellularLocationSoTerms =  $self->getSoIds($test, $self->getParamValue('cellularLocationSoTerms'));
+    my $soTerms =  $self->getSoIds($test, $self->getParamValue('cellularLocationSoTerms'));
 
-    my $sql = " SELECT '$organismSource'
+    my $soIds =  $self->getSoIds($test, $soTerms);
+
+    my $sql = <<"EOF";
+       SELECT '$organismSource'
                 ||'|'||
                sa.source_id
                 ||' | organism='||
@@ -28,12 +31,12 @@ sub getDownloadFileCmd {
                ns.sequence
            FROM dots.nasequence ns,
                 ApidbTuning.SequenceAttributes sa,
-                SRes.SequeneceOnotlogy so 
           WHERE ns.na_sequence_id = sa.na_sequence_id
             AND sa.ncbi_tax_id = $ncbiTaxonId
             AND sa.is_top_level = 1
-            AND so.term_name in ($cellularLocationSoTerms)
-            AND ns.sequence_ontology_id = so.sequence_ontology_idxs";
+            AND ns.sequence_ontology_id in ($soIds)
+EOF
+
     my $cmd = "gusExtractSequences --outputFile $downloadFileName  --idSQL \"$sql\" ";
     return $cmd;
 }
