9968a9c798ac85c42b5818a91b9b7366cdec49b9
Deborah Pinney <pinney@upenn.edu>
2011-11-03 14:13:06 +0000

    change isDeprecatedGenes to hasDeprecatedGenes to be consistent with graphs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/portToxoToFlow@43533 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeAnnotatedProteinsDownloadFile.pm
+++ Main/lib/perl/WorkflowSteps/MakeAnnotatedProteinsDownloadFile.pm
@@ -9,7 +9,7 @@ sub getDownloadFileCmd {
     my ($self, $downloadFileName, $test) = @_;
 
 
-    my $deprecated = ($self->getParamValue('isDeprecatedGenes') eq 'true') ? 1 :0;
+    my $deprecated = ($self->getParamValue('hasDeprecatedGenes') eq 'true') ? 1 :0;
     my $organismSource = $self->getParamValue('organismSource');
     my $organismAbbrev = $self->getParamValue('organismAbbrev');
     my $soTerms = $self->getParamValue('cellularLocationSoTerms');
