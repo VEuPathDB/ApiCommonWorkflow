9904961ee392a3ca986c9294df53a4f9a8771c05
Steve Fischer <sfischer@upenn.edu>
2011-11-28 03:33:11 +0000

    revert name of param to inputDir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@44090 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/InsertSimpleRadAnalysisFromConfig.pm
+++ Main/lib/perl/WorkflowSteps/InsertSimpleRadAnalysisFromConfig.pm
@@ -9,7 +9,7 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
   my ($self, $test, $undo) = @_;
 
-  my $analysisWorkingDir = $self->getParamValue('analysisWorkingDir');
+  my $analysisWorkingDir = $self->getParamValue('inputDir');
 
   my $configFile = $self->getParamValue('configFile');
 
@@ -29,7 +29,7 @@ sub run {
   $args.=" --useSqlLdr" if($useSqlLdr eq "true"); 
 
   if ($test) {
-    $self->testInputFile('analysisWorkingDir', "$workflowDataDir/$analysisWorkingDir");
+    $self->testInputFile('inputDir', "$workflowDataDir/$analysisWorkingDir");
     $self->testInputFile('configFile', "$workflowDataDir/$configFile");
   }
 
