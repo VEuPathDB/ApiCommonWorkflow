acc6f9f0d12674986a0afdddfca0585c460c2edf
Steve Fischer <sfischer@upenn.edu>
2011-12-09 16:24:03 +0000

    support protein as well as dna
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@44432 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeGfClientTaskInputDir.pm
+++ Main/lib/perl/WorkflowSteps/MakeGfClientTaskInputDir.pm
@@ -1,4 +1,4 @@
-package ApiCommonWorkflow::Main::WorkflowSteps::MakeGfClientTaskInputDir;
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeGfClientTaskInputDirNew;
 
 @ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
 
@@ -6,55 +6,62 @@ use strict;
 use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 
 sub run {
-    my ($self, $test, $undo) = @_;
+  my ($self, $test, $undo) = @_;
 
-    # get parameter values
-    my $taskInputDir = $self->getParamValue("taskInputDir");
-    my $maxIntronSize = $self->getParamValue("maxIntronSize");
-    my $queryFile = $self->getParamValue("queryFile");
-    my $targetDir = $self->getParamValue("targetDir");
+  # get parameter values
+  my $taskInputDir = $self->getParamValue("taskInputDir");
+  my $maxIntronSize = $self->getParamValue("maxIntronSize");
+  my $queryFile = $self->getParamValue("queryFile");
+  my $queryType = $self->getParamValue("queryType");
+  my $targetDir = $self->getParamValue("targetDir");
 
-    my $clusterServer = $self->getSharedConfig('clusterServer');
-    my $taskSize = $self->getConfig("taskSize");
-    my $gaBinPath = $self->getConfig("$clusterServer.gaBinPath");
+  my $clusterServer = $self->getSharedConfig('clusterServer');
+  my $taskSize = $self->getConfig("taskSize");
+  my $gaBinPath = $self->getConfig("$clusterServer.gaBinPath");
 
-    my $clusterWorkflowDataDir = $self->getClusterWorkflowDataDir();
-    my $workflowDataDir = $self->getWorkflowDataDir();
+  my $clusterWorkflowDataDir = $self->getClusterWorkflowDataDir();
+  my $workflowDataDir = $self->getWorkflowDataDir();
+ 
+  $self->error("Parameter queryType=$queryType is invalid.  It must be either dna or protein") unless $queryType eq 'dna' || $queryType eq 'protein';
 
-    if ($undo) {
-	$self->runCmd(0,"rm -rf $workflowDataDir/$taskInputDir");
-    }else {
+  if ($undo) {
+    $self->runCmd(0,"rm -rf $workflowDataDir/$taskInputDir");
+  }else {
 
-	if ($test) {
-	    $self->testInputFile('queryFile', "$workflowDataDir/$queryFile");
-	    $self->testInputFile('targetDir', "$workflowDataDir/$targetDir");
-	}
+   if ($test) {
+        $self->testInputFile('queryFile', "$workflowDataDir/$queryFile");
+        $self->testInputFile('targetDir', "$workflowDataDir/$targetDir");
+    }
 
-	$self->runCmd(0,"mkdir -p $workflowDataDir/$taskInputDir");
+    $self->runCmd(0,"mkdir -p $workflowDataDir/$taskInputDir");
 
-	# make controller.prop file
-	$self->makeDistribJobControllerPropFile($taskInputDir, 1, $taskSize,
-						"DJob::DistribJobTasks::GenomeAlignWithGfClientTask");
-	# make task.prop file
-	my $taskPropFile = "$workflowDataDir/$taskInputDir/task.prop";
-	open(F, ">$taskPropFile") || die "Can't open task prop file '$taskPropFile' for writing";
+    # make controller.prop file
+    $self->makeDistribJobControllerPropFile($taskInputDir, 1, $taskSize,
+				       "DJob::DistribJobTasks::GenomeAlignWithGfClientTask");
+    # make task.prop file
+    my $taskPropFile = "$workflowDataDir/$taskInputDir/task.prop";
+    my $blatParams = $queryType eq 'protein' ? 'blatParams=-minScore=25 -minIdentity=20' : '';
 
-	print F
-	    "gaBinPath=$gaBinPath
+    open(F, ">$taskPropFile") || die "Can't open task prop file '$taskPropFile' for writing";
+
+    print F
+"gaBinPath=$gaBinPath
 targetDirPath=$clusterWorkflowDataDir/$targetDir/nib
 queryPath=$clusterWorkflowDataDir/$queryFile
 nodePort=5550
 maxIntron=$maxIntronSize
+queryType=$queryType
+$blatParams
 ";
-	close(F);
-    }
+  close(F);
+ }
 }
 
 sub getConfigDeclaration {
-    return (
-	# [name, default, description]
-	['taskSize', "", ""],
-	['gaBinPath', "", ""],
-	);
+  return (
+	  # [name, default, description]
+	  ['taskSize', "", ""],
+	  ['gaBinPath', "", ""],
+	 );
 }
 
