9237ebcbb457a6a33dba8cdca63f1ed03de2234b
Steve Fischer <sfischer@upenn.edu>
2011-12-16 02:55:17 +0000

    fix readdir bug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@44618 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/InsertFastaSubset.pm
+++ Main/lib/perl/WorkflowSteps/InsertFastaSubset.pm
@@ -20,7 +20,7 @@ sub run {
 
  # my $args = "--externalDatabaseName $extDbName --externalDatabaseVersion $extDbRlsVer --sequenceFile $workflowDataDir/$fastaFile --sourceIdsFile  $workflowDataDir/$idsFile --regexSourceId  '>gi\\|\\d+\\|\\w+\\|(\\w+\\.?\\w+)\\|' --regexDesc '^>gi\\|\\d+\\|\\w+\\|\\w+\\.?\\w+\\|(\.+)' --regexSecondaryId '>gi\\|(\\d+)\\|' --tableName DoTS::ExternalAASequence";
 
-my $args = "--externalDatabaseName $extDbName --externalDatabaseVersion $extDbRlsVer --sequenceFile $workflowDataDir/$fastaFile --sourceIdsFile  $workflowDataDir/$idsFile --regexSecondaryId '>gi\\|\\d+\\|\\w+\\|(\\w+\\.?\\w+)\\|' --regexDesc '^>gi\\|\\d+\\|\\w+\\|\\w+\\.?\\w+\\|(\.+)' --regexSourceId '>gi\\|(\\d+)\\|' --tableName DoTS::ExternalAASequence";
+my $args = "--externalDatabaseName $extDbName --externalDatabaseVersion $extDbVer --sequenceFile $workflowDataDir/$fastaFile --sourceIdsFile  $workflowDataDir/$idsFile --regexSecondaryId '>gi\\|\\d+\\|\\w+\\|(\\w+\\.?\\w+)\\|' --regexDesc '^>gi\\|\\d+\\|\\w+\\|\\w+\\.?\\w+\\|(\.+)' --regexSourceId '>gi\\|(\\d+)\\|' --tableName DoTS::ExternalAASequence";
 
   if ($test) {
     $self->testInputFile('fastaFile', "$workflowDataDir/$fastaFile");
