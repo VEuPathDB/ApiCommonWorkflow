b9b8b3da99f35065cf8704d743f4e2fe94164fe6
Steve Fischer <sfischer@upenn.edu>
2011-12-20 22:28:16 +0000

    update to support useTopLevel
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/101@44702 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/ExtractTranscriptSeqsForSsa.pm
+++ Main/lib/perl/WorkflowSteps/ExtractTranscriptSeqsForSsa.pm
@@ -10,28 +10,40 @@ sub run {
 
   my $outputFile = $self->getParamValue('outputFile');
   my $organismAbbrev = $self->getParamValue('organismAbbrev');
-  my $useTopLevel = $self->getParamValue('useTopLevel');
+  my $useTopLevel = $self->getBooleanParamValue('useTopLevel');
 
   my $ncbiTaxonId = $self->getOrganismInfo($test, $organismAbbrev)->getNcbiTaxonId();
 
-  my $sql = "select ga.source_id || ':'
+  my $sql = "select gf.source_id || ':'
                     || substr(ns.source_id, 1, 50) ||':'
                     ||(nl.start_min - 1)||'-'||nl.end_max||'_'||(decode(nl.is_reversed, 1, '-', '+')),
                     snas.sequence
                           from dots.Transcript t, dots.SplicedNaSequence snas,
-                               dots.GeneFeature ga, dots.NaLocation nl,
-                               dots.NaSequence ns, sres.Taxon, sres.SequenceOntology so
-                          where ga.na_sequence_id = ns.na_sequence_id
-                            and ga.na_feature_id = t.parent_id
+                               dots.GeneFeature gf, dots.NaLocation nl,
+                               dots.NaSequence ns, sres.Taxon ta, sres.SequenceOntology so,
+                               apidbtuning.geneattributes ga
+                          where gf.na_sequence_id = ns.na_sequence_id
+                            and gf.na_feature_id = t.parent_id
                             and t.na_sequence_id = snas.na_sequence_id
-                            and ga.sequence_ontology_id = so.sequence_ontology_id
+                            and gf.sequence_ontology_id = so.sequence_ontology_id
                             and so.term_name != 'repeat_region'
-                            and snas.taxon_id = taxon.taxon_id
-                            and taxon.ncbi_tax_id = $ncbiTaxonId
-                            and nl.na_feature_id = ga.na_feature_id";
+                            and snas.taxon_id = ta.taxon_id
+                            and ta.ncbi_tax_id = $ncbiTaxonId
+                            and nl.na_feature_id = gf.na_feature_id
+                            and gf.source_id = ga.source_id";
 
 
+  if ($useTopLevel) {
+    $sql = "SELECT ga.source_id||':'||ga.sequence_id||':'||(ga.start_min - 1)||'-'||ga.end_max||'_'||(decode(ga.is_reversed, 1, '-', '+')), snas.sequence
+             FROM dots.transcript t,
+                  dots.splicednasequence snas,
+                  apidbtuning.geneattributes ga
+             WHERE ga.na_feature_id = t.parent_id
+              AND t.na_sequence_id = snas.na_sequence_id
+              AND ga.so_term_name != 'repeat_region'
+              AND ga.ncbi_tax_id = $ncbiTaxonId";
 
+  }
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
