11694ea63ac7c010e5b89a548a3cbdf9df158328
Steve Fischer <sfischer@upenn.edu>
2012-01-10 18:29:19 +0000

    do rum postprocessing locally
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/101@45001 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/RunRUMPostprocessing.pm
@@ -0,0 +1,46 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::RunRUMPostprocessing;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # reads files in mainResultDir and produces more files in there (and deletes some too).
+
+  my $genomeFastaFile = $self->getParamValue('genomeFastaFile');
+  my $transcriptFastaFile = $self->getParamValue('transcriptFastaFile');
+  my $geneAnnotationFile = $self->getParamValue('geneAnnotationFile');
+  my $mainResultDir = $self->getParamValue('mainResultDir');
+  my $createJunctionsFile = $self->getBooleanParamValue('createJunctionsFile');
+  my $strandSpecific = $self->getBooleanParamValue('strandSpecific');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $haveTranscripts = -e "$workflowDataDir/$transcriptsFastaFile"? "--haveTranscripts" : "";
+  my $createJunctions = $createJuctionsFile? "--createJuctions" : "";
+  my $ss = $strandSpecific? "--strandSpecific" : "";
+
+  my $cmd= "postProcessRUMTask --genomeFastaFile $workflowDataDir/$genomeFastaFile --geneAnnotationFile $workflowDataDir/$geneAnnotationFile --mainResultDir $workflowDataDir/$mainResultDir $haveTranscripts $createJunctions $ss";
+
+  if($undo){
+      # can't undo this step.  must undo cluster task
+  }else{
+      if ($test) {
+	  $self->testInputFile('genomeFastaFile', "$workflowDataDir/$genomeFastaFile");
+	  $self->testInputFile('geneAnnotationFile', "$workflowDataDir/$geneAnnotationFile");
+      }else{
+	  $self->runCmd($test, $cmd);
+      }
+  }
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
