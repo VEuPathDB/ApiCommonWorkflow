c3aef332770c7a51c9f77163cbb97a0d95569f83
Deborah Pinney <pinney@upenn.edu>
2012-01-20 13:17:05 +0000

    new step to run runpsipred_single locally
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@45213 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/RunPsipredSingle.pm
@@ -0,0 +1,48 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::RunPsipredSingle;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $inputFile = $self->getParamValue('inputFile');
+  my $psipredPath = $self->getParamValue('psipredPath');
+
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd1 = "mkdir -p $workflowDataDir/result";
+
+  my $cmd2 = "psipredRun --inputFile $inputProteinsFile --outputDir $workflowDataDir/result --psipredPath $psipredPath";
+
+  if ($undo) {
+    $self->runCmd(0, "rm -rf $workflowDataDir/result");
+  } else {
+      if ($test){
+	  $self->testInputFile('inputFile', "$workflowDataDir/$inputFile");
+	  $self->runCmd(0,"echo test > $workflowDataDir/$outputFile");
+      }else{
+	  $self->runCmd($test,$cmd1);
+          $self->runCmd($test,$cmd2);
+      }
+  }
+}
+
+
+sub getParamsDeclaration {
+  return ('inputFile',
+	  'psipredPath'
+	 );
+}
+
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+ 	 );
+}
+
+
