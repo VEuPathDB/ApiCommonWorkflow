a42b89562c695b3026b6355a6bfb6e24345f9dc7
Brian Brunk <brunkb@upenn.edu>
2012-01-27 18:23:53 +0000

    Isolates download files done by species now
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@45363 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeIsolatesDownloadFile.pm
@@ -0,0 +1,54 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeIsolatesDownloadFile;
+
+## NOTE: this stepclass is specifically extracting the isolates from GenBank
+##       If we want another data source, we'll need to extend this step class and add an externaldb param.
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::DownloadFileMaker);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::DownloadFileMaker;
+
+sub getIsSpeciesLevel {
+    return 1;
+}
+
+sub getDownloadFileCmd { 
+  my ($self, $downloadFileName, $test) = @_;
+
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+
+  my $organismInfo = $self->getOrganismInfo($test, $organismAbbrev);
+  my $taxonId = $organismInfo->getSpeciesTaxonId();
+  my $taxonIdList = $organismInfo->getTaxonIdList($taxonId);
+
+    my $sql = <<"EOF";
+        SELECT
+        enas.source_id
+        ||' | organism='|| 
+        replace(tn.name, ' ', '_')
+        ||' | description='||
+        decode (enas.description,null,'Not Available',enas.description)
+        ||' | length='||
+        enas.length
+        as defline,
+        enas.sequence
+        From dots.ExternalNASequence enas,
+             SRES.sequenceontology so,
+             sres.taxonname tn,
+             sres.externaldatabase ed,
+             sres.externaldatabaserelease edr,
+             dots.isolatesource i
+        Where tn.taxon_id in ($taxonIdList)
+            AND enas.taxon_id = tn.taxon_id
+            AND tn.name_class = 'scientific name'
+            AND enas.external_database_release_id = edr.external_database_release_id
+            AND edr.external_database_id = ed.external_database_id
+            AND ed.name like '\%GBIsolates\%'
+            AND enas.na_sequence_id = i.na_sequence_id
+            AND so.sequence_ontology_id = enas.sequence_ontology_id
+EOF
+
+    my $cmd = "gusExtractSequences --outputFile $downloadFileName  --allowEmptyOutput --idSQL \"$sql\"";
+    return $cmd;
+}
+
+1;
