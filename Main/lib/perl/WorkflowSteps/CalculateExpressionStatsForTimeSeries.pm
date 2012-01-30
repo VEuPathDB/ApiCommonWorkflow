4d0b7ac227868813850730d547863f4d296be3af
Ja’Shon Cade <jcade@upenn.edu>
2012-01-30 20:27:51 +0000

    removed isLogged param, this will now be looked up in the DB
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@45421 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CalculateExpressionStatsForTimeSeries.pm
+++ Main/lib/perl/WorkflowSteps/CalculateExpressionStatsForTimeSeries.pm
@@ -8,31 +8,37 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 
 sub run {
   my ($self, $test, $undo) = @_;
-
-#  my $profileSetSpecs = $self->getParamValue('profileSetSpecs');
-  my $profileSetSpecs = ""; # see redmine issue 4257
-
-  my $extDbRlsSpec = $self->getParamValue('extDbRlsSpec');
-
-  my $isLogged = ""; # ($self->getParamValue('isLogged') eq 'true') ? 1 :0;  see redmine 4257
-
-  my $mappingFile = ""; # $self->getParamValue('mappingFile'); see redmine 4257
-
-  my $workflowDataDir = $self->getWorkflowDataDir();
-
-  my $args = "--externalDatabaseSpec '$extDbRlsSpec'  --profileSetSpecs '$profileSetSpecs'";
-
-  $args .= " --timePointsMappingFile '$workflowDataDir/$mappingFile'" if $mappingFile;
-
-  $args .= " --isLogged" if $isLogged;
-
-  
-  if ($test) {
-    $self->testInputFile('inputDir', "$workflowDataDir/$mappingFile") if $mappingFile;
+  my @configLines;
+  my $configFile = $self->getParamValue('configFile');
+  open (cf, "< $configFile");
+  while (<cf>) {
+    chomp;
+    push (@configLines,$_)
   }
 
-  $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::CalculateProfileSummaryStats", $args);
+  unless ($configLines[0] eq "NOT_A_TIME_SERIES") {
+    my $profileSetSpecs = $configLines[1]; 
 
+    my $extDbRlsSpec = $self->getParamValue('extDbRlsSpec');
+
+    my $mappingFile = @configLines[0];
+
+    my $workflowDataDir = $self->getWorkflowDataDir();
+
+    my $args = "--externalDatabaseSpec '$extDbRlsSpec'  --profileSetSpecs '$profileSetSpecs'";
+
+    $args .= " --timePointsMappingFile '$workflowDataDir/$mappingFile'" unless ($mappingFile eq "NO_MAPPING_FILE");
+
+
+
+
+    if ($test) {
+      $self->testInputFile('inputDir', "$workflowDataDir/$mappingFile") if $mappingFile;
+    }
+
+    $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::CalculateProfileSummaryStats", $args);
+
+  }
 }
 
 sub getConfigDeclaration {
