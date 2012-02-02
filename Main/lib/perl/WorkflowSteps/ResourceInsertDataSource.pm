53aa60158bcd64c928e2c42208be0e05a40d2354
Steve Fischer <sfischer@upenn.edu>
2012-02-02 02:48:17 +0000

    add type and subType
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@45485 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/ResourceInsertDataSource.pm
+++ Main/lib/perl/WorkflowSteps/ResourceInsertDataSource.pm
@@ -10,32 +10,29 @@ sub run {
     my $dataSourceName = $self->getParamValue('resourceName');
     my $dataSourceXmlFile = $self->getParamValue('resourceXmlFileName');
     my $dataDirPath = $self->getParamValue('dataDir');
+
     my $dataSource = $self->getDataSource($dataSourceName, $dataSourceXmlFile, $dataDirPath);
     
     my $version = $dataSource->getVersion();
     my $organismAbbrev = $dataSource->getOrganismAbbrev();
     my $scope = $dataSource->getScope();
+    my $type = $dataSource->getType();
+    my $subType = $dataSource->getSubType();
 
     my $isSpeciesScope = $scope eq 'species'? "--isSpeciesScope" : "";
+    my $tp = $type? "--type $type" : "";
+    my $stp = $subType? "--subType $subType" : "";
+
     my $taxonId;
     if ($organismAbbrev) {
 	my $t = $self->getOrganismInfo($test, $organismAbbrev)->getTaxonId();
 	$taxonId = "--taxonId $t";
     }
 
-    my $dbPluginArgs = "--dataSourceName '$dataSourceName' --version '$version' $isSpeciesScope $taxonId";
+    my $dbPluginArgs = "--dataSourceName '$dataSourceName' --version '$version' $isSpeciesScope $taxonId $tp $stp";
 
     $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertDataSource", $dbPluginArgs);
 
-}
-
-
-sub getParamsDeclaration {
-    return (
-	'resourceName',
-	'resourceXmlFileName',
-        'dataDir'
-	);
 }
 
 sub getConfigDeclaration {
