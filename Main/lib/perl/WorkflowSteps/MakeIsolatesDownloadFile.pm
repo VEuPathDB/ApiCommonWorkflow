616785ec98f9d54a184f18c19c9eaaf4bb13ea76
Brian Brunk <brunkb@upenn.edu>
2012-02-02 18:10:42 +0000

    fixed to use new datasource schema
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@45499 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeIsolatesDownloadFile.pm
+++ Main/lib/perl/WorkflowSteps/MakeIsolatesDownloadFile.pm
@@ -35,14 +35,15 @@ sub getDownloadFileCmd {
              sres.taxonname tn,
              sres.externaldatabase ed,
              sres.externaldatabaserelease edr,
+             apidb.datasource ds,
              dots.isolatesource i
         Where tn.taxon_id in ($taxonIdList)
             AND enas.taxon_id = tn.taxon_id
             AND tn.name_class = 'scientific name'
             AND enas.external_database_release_id = edr.external_database_release_id
             AND edr.external_database_id = ed.external_database_id
-            AND edr.data_type = 'isolates'
-            AND edr.data_subtype = 'Sequencing Typed'
+            AND ed.name = ds.name
+            AND ds.subtype = 'sequencing_typed'
             AND enas.na_sequence_id = i.na_sequence_id
             AND so.sequence_ontology_id = enas.sequence_ontology_id
 EOF
