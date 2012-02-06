493ef78e29926bef49ce6f6bb8163865f821969f
Wei Li <weili1@pennmedicine.upenn.edu>
2012-02-06 16:23:55 +0000

    add makeDataDir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@45606 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CopyProviderProbeMappingFiles.pm
+++ Main/lib/perl/WorkflowSteps/CopyProviderProbeMappingFiles.pm
@@ -16,26 +16,26 @@ sub run {
   my $makeCdfFile = $self->getBooleanParamValue('makeCdfFile');
   my $makeNdfFile = $self->getBooleanParamValue('makeNdfFile');
 
-  $self->error("Error:  parameters makeCdfFile and makeCdfFile cannot both be true") if $makeCdfFile && $makeNdfFile;
-  $self->error("Input tab file '$inputTabFile' does not exist") unless -e $inputTabFile;
   my $workflowDataDir = $self->getWorkflowDataDir();
+  $self->error("Error:  parameters makeCdfFile and makeCdfFile cannot both be true") if $makeCdfFile && $makeNdfFile;
+  $self->error("Input tab file '$workflowDataDir/$inputTabFile' does not exist") unless (-e "$workflowDataDir/$inputTabFile");
 
   my $cmd1 = "cp $workflowDataDir/$inputTabFile $workflowDataDir/$outputTabFile";
   my $cmd2;
   if ($makeCdfFile) {
-      $self->error("Input cdf file '$inputCdfFile' does not exist") unless -e $inputCdfFile;
+      $self->error("Input cdf file '$workflowDataDir/$inputCdfFile' does not exist") unless (-e "$workflowDataDir/$inputCdfFile");
       $cmd2 = "cp $workflowDataDir/$inputCdfFile $workflowDataDir/$outputVendorFile";
   } elsif ($makeNdfFile) {
-      $self->error("Input ndf file '$inputNdfFile' does not exist") unless -e $inputNdfFile;
+      $self->error("Input ndf file '$workflowDataDir/$inputNdfFile' does not exist") unless (-e "$workflowDataDir/$inputNdfFile");
       $cmd2 = "cp $workflowDataDir/$inputNdfFile $workflowDataDir/$outputVendorFile";
   } else {
     $cmd2 = "touch $workflowDataDir/$outputVendorFile";
   }
 
   if ($test) {
-    $self->testInputFile('inputTabFile', "$inputTabFile");
-    $self->testInputFile('inputCdfFile', "$inputCdfFile");
-    $self->testInputFile('inputNdfFile', "$inputNdfFile");
+    $self->testInputFile('inputTabFile', "$workflowDataDir/$inputTabFile");
+    $self->testInputFile('inputCdfFile', "$workflowDataDir/$inputCdfFile");
+    $self->testInputFile('inputNdfFile', "$workflowDataDir/$inputNdfFile");
     $self->runCmd(0, "echo test > $workflowDataDir/$outputTabFile");
     $self->runCmd(0, "echo test > $workflowDataDir/$outputVendorFile");
   }
