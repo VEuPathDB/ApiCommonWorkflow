b583b1ce07561d6c7a40f5a46920b18fe019f2e7
Steve Fischer <sfischer@upenn.edu>
2012-02-23 23:40:21 +0000

    adjust test mode output
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@46005 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/ExtractNaSeqs.pm
+++ Main/lib/perl/WorkflowSteps/ExtractNaSeqs.pm
@@ -60,9 +60,8 @@ sub run {
     } else {
 	if ($test) {
 	    $self->runCmd(0,"echo test > $workflowDataDir/$outputFile");
-	}else{
-	    $self->runCmd($test,"gusExtractSequences --outputFile $workflowDataDir/$outputFile --idSQL \"$sql\" --verbose");
 	}
+	$self->runCmd($test,"gusExtractSequences --outputFile $workflowDataDir/$outputFile --idSQL \"$sql\" --verbose");
     }
   }
 }
