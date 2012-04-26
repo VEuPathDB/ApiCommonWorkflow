793208b820409d9cbe87984f3e85b15266cee69c
Steve Fischer <sfischer@upenn.edu>
2012-04-26 16:29:20 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@47227 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/OrthomclMakeGoodProteins.pm
@@ -0,0 +1,43 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::OrthomclMakeGoodProteins;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use File::Basename;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $proteomesDir = $self->getParamValue('proteomesDir');
+  my $outputGoodProteinsFile = $self->getParamValue('outputGoodProteinsFile');
+  my $outputBadProteinsFile = $self->getParamValue('outputBadProteinsFile');
+  my $minLength = $self->getParamValue('minLength');
+  my $maxStopPercent = $self->getParamValue('maxStopPercent');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  
+  my $cmd = "orthomclFilterFasta '$workflowDataDir/$proteomesDir' minLength $maxPercentStop $workflowDataDir/$outputGoodProteinsFile $workflowDataDir/$outputBadProteinsFile";
+    
+  if ($undo) {
+      $self->runCmd(0, "rm -f $workflowDataDir/$outputGoodProteinsFile");
+      $self->runCmd(0, "rm -f $workflowDataDir/$outputBadProteinsFile");
+  }else {
+      if ($test){
+	  $self->runCmd(0, "echo test> $workflowDataDir/$outputGoodProteinsFile");
+	  $self->runCmd(0, "echo test> $workflowDataDir/$outputBadProteinsFile");
+      }else{
+	  $self->runCmd($test, $cmd);
+      }
+  }
+
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
