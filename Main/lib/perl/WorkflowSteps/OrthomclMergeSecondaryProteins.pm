f646c0f0ef16ccf20cb04eba4af8cedad25dba57
Steve Fischer <sfischer@upenn.edu>
2012-04-30 18:09:46 +0000

    initial dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@47305 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/OrthomclMergeSecondaryProteins.pm
@@ -0,0 +1,38 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::OrthomclMergeSecondaryProteins;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use File::Basename;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $inputRepGroupsFile = $self->getParamValue('inputRepGroupsFile');
+  my $inputTierOneGroupsDir = $self->getParamValue('inputTierOneGroupsDir');
+  my $outputMergedGroupsFile = $self->getParamValue('outputMergedGroupsFile');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+  
+  my $cmd = "orthomclMergeSecondaryProteins $workflowDataDir/$inputRepGroupsFile $workflowDataDir/$outputMergedGroupsFile $workflowDataDir/$inputTierOneGroupsDir/*";
+    
+  if ($undo) {
+      $self->runCmd(0, "rm -f $workflowDataDir/$outputMergedGroupsFile");
+  }else {
+      if ($test){
+	  $self->runCmd(0, "echo test> $workflowDataDir/$outputMergedGroupsFile");
+      }else{
+	  $self->runCmd($test, $cmd);
+      }
+  }
+
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
