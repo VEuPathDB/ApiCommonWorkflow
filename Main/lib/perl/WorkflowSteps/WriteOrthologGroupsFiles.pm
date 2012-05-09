3e891cd738d87b54b3699f8e6f39d36c7be34036
John Iodice <iodice@pennmedicine.upenn.edu>
2012-05-09 16:11:29 +0000

    further development and debugging
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@47639 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/WriteOrthologGroupsFiles.pm
+++ Main/lib/perl/WorkflowSteps/WriteOrthologGroupsFiles.pm
@@ -14,10 +14,12 @@ sub run {
   my $outputFile = $self->getParamValue('outputFile');
   my $startingOrthologGroupNumber = $self->getParamValue('startingOrthologGroupNumber');
   my $includeSingletons = $self->getParamValue('includeSingletons');
+  my $proteinFile = $self->getParamValue('proteinFile');
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
   my $cmd = "orthomclMclToGroups $orthomclIdPrefix $startingOrthologGroupNumber < $workflowDataDir/$inputFile > $workflowDataDir/$outputFile";
+  my $singletonCmd = "orthomclSingletons $proteinFile $orthomclIdPrefix >> $workflowDataDir/$outputFile";
 
   if ($undo) {
     $self->runCmd(0, "rm -f $workflowDataDir/$outputFile");
@@ -26,7 +28,12 @@ sub run {
 	  $self->testInputFile('inputFile', "$workflowDataDir/$inputFile");
 	  $self->runCmd(0,"echo test > $workflowDataDir/$outputFile");
       }
-      $self->runCmd($test,$cmd);
-  }
+
+      $self->runCmd($test, $cmd);
+
+      if ($includeSingletons) {
+	$self->runCmd($test, $singletonCmd);
+      }
+    }
 }
 
