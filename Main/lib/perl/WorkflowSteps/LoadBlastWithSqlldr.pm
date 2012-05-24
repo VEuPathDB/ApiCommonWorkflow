74e1f845b5e440e4be7c6c081c876f47a65336e0
John Iodice <iodice@pennmedicine.upenn.edu>
2012-05-24 16:39:47 +0000

    don't try to run orthomclInstallSchema; a previous step now handles that.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@48121 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/LoadBlastWithSqlldr.pm
+++ Main/lib/perl/WorkflowSteps/LoadBlastWithSqlldr.pm
@@ -21,51 +21,18 @@ sub run {
   my $ctlFile = "$workflowDataDir/blast.ctl";
   my $sqlldrLog = "$workflowDataDir/sqlldr.log";
   my $cmd = "sqlldr $gusLogin/$gusPassword\@$gusInstance data=$workflowDataDir/$inputFile control=$ctlFile log=$sqlldrLog rows=25000 direct=TRUE";
-  my $configFile = "$workflowDataDir/orthomclPairs.config";
 
   if ($undo) {
     $self->runCmd(0, "rm -f $ctlFile");
-    $self->runCmd($test, "orthomclDropSchema $configFile /dev/null $suffix");
-    $self->runCmd(0, "rm -f $configFile");
-
   } else {
       if ($test) {
 	  $self->testInputFile('inputFile', "$workflowDataDir/$inputFile");
       }
 
-      # make OrthoMCL config file
-      writeConfigFile($configFile, $gusInstance, $gusLogin, $gusPassword);
-
-      # create tables
-      $self->runCmd($test, "orthomclInstallSchema $configFile $workflowDataDir/orthomclInstallSchema.log $suffix");
-
       # run sqlldr (after writing its control file)
       writeControlFile($ctlFile, $suffix);
       $self->runCmd($test, $cmd);
   }
-}
-
-sub writeConfigFile {
-  my ($configFile, $instance, $login, $password) = @_;
-
-  my $dsn = "dbi:Oracle:$instance";
-  open(CONFIG, ">$configFile") || die "Can't open '$configFile' for writing";
-  print CONFIG <<"EOF";
-dbConnectString=$dsn
-dbLogin=$login
-dbPassword=$password
-dbVendor=oracle
-similarSequencesTable=apidb.SimilarSequences
-orthologTable=apidb.Ortholog
-inParalogTable=apidb.InParalog
-coOrthologTable=apidb.CoOrtholog
-interTaxonMatchView=apidb.InterTaxonMatch
-oracleIndexTablespace=indx
-percentMatchCutoff=50
-evalueExponentCutoff=-5
-EOF
-
-  close(CONFIG);
 }
 
 sub writeControlFile {
@@ -91,47 +58,3 @@ EOF
 
   close(CTL);
 }
-
-=head1
-
-# predecessor from Steps.pm
-sub loadBlastWithSqlldr {
-  my ($mgr,$blastName,$restart,$file, $ctl) = @_;
-
-  my $signal = "sqlldrLoad$blastName";
-
-  return if $mgr->startStep("Load $blastName using sqldr", $signal);
-
-  my $sqlldrFile = "$mgr->{dataDir}/similarity/$blastName/master/mainresult/blastSimForSqlldr.out";
-
-  if ($file) {
-    $sqlldrFile = "$mgr->{dataDir}/similarity/$blastName/master/mainresult/$file";
-
-    $mgr->runCmd("gunzip $sqlldrFile") if $sqlldrFile =~ /\.gz/;
-    $sqlldrFile =~ s/\.gz//;
-  }
-
-  my $ctlFile = "$mgr->{dataDir}/similarity/$blastName/master/mainresult/$ctl" if $ctl;
-
-  my $logfile = "$mgr->{myPipelineDir}/logs/$signal.log";
-
-  die "$sqlldrFile file doesn't exist\n" unless (-e $sqlldrFile);
-
-  my $propertySet = $mgr->{propertySet};
-
-  my $oracleUserPswd = $propertySet->getProp('oracleUserPswd');
-
-  my $cmd = "nohup sqlldr $oracleUserPswd control=$sqlldrFile log=$logfile rows=25000 direct=TRUE";
-
-  $cmd = "nohup sqlldr $oracleUserPswd data=$sqlldrFile control=$ctlFile log=$logfile rows=25000 direct=TRUE" if $ctl;
-
-  $cmd .= " SKIP=$restart" if $restart;
-
-  $mgr->runCmd($cmd);
-
-  $mgr->endStep($signal);
-
-
-}
-
-=cut
