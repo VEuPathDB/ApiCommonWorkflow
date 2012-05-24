8b9b117bbe6ada30bd3418279d8267491249b11a
Steve Fischer <sfischer@upenn.edu>
2012-05-24 18:32:00 +0000

    fix cacheHit bug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/102@48134 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/RunPairwiseMercatorMavid.pm
+++ Main/lib/perl/WorkflowSteps/RunPairwiseMercatorMavid.pm
@@ -150,10 +150,10 @@ sub cacheHit {
 
     # -M is a perl built-in function to return a file's age
     my $cacheIsCurrent = -e "$cacheDir/$pairName" 
-	&& -M "$cacheDir/$pairName" < "$mercatorInputDir/$orgA.fasta" # cache is younger 
-	&& -M "$cacheDir/$pairName" < "$mercatorInputDir/$orgA.gff"
-	&& -M "$cacheDir/$pairName" < "$mercatorInputDir/$orgB.fasta"
-	&& -M "$cacheDir/$pairName" < "$mercatorInputDir/$orgB.gff";
+	&& -M "$cacheDir/$pairName" < -M "$mercatorInputDir/$orgA.fasta" # cache is younger 
+	&& -M "$cacheDir/$pairName" < -M "$mercatorInputDir/$orgA.gff"
+	&& -M "$cacheDir/$pairName" < -M "$mercatorInputDir/$orgB.fasta"
+	&& -M "$cacheDir/$pairName" < -M "$mercatorInputDir/$orgB.gff";
     $self->runCmd($test,"rm -r $cacheDir/$pairName") if (!$cacheIsCurrent && -e "$cacheDir/$pairName");
     return $cacheIsCurrent;
 }
