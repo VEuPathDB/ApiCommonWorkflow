d722620c404ed94ddea9a2e60ae6602722556c5a
dborch <dborch@_.com>
2012-05-25 19:06:40 +0000

    Added MakeDerivedTables to help fix motif search
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/103@48177 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeDerivedTables.pm
@@ -0,0 +1,48 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeDerivedTables;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $tables = $self->getParamValue('tables');
+
+  my $gusHome = $self->getSharedConfig('gusHome');
+
+  my $instance = $self->getGusInstanceName();
+
+  my $tuningTablePrefix = $self->getTuningTablePrefix($organismAbbrev, $test);
+
+  my $apidbTuningPassword = $self->getConfig('apidbTuningPassword');
+  my $xmlConfigFileName="tmpConfigFile.xml";
+  my $xmlConfigFileString=
+"<?xml version='1.0'?>
+<property>
+<password>$apidbTuningPassword</password>
+<schema>ApiDBTuning</schema>
+</property>
+";
+  my $stepDir = $self->getStepDir();
+  open(F,">$stepDir/$xmlConfigFileName");
+  print F $xmlConfigFileString;
+  close F;
+
+  my $taxonId = $self->getOrganismInfo($test, $organismAbbrev)->getTaxonId();
+
+  my $cmd = "tuningManager -prefix '$tuningTablePrefix' -instance '$instance' -propFile $stepDir/$xmlConfigFileName -doUpdate -notifyEmail none -tables $tables -configFile ${gusHome}/lib/xml/tuningManager.xml -filterValue $taxonId ";
+
+
+  if ($undo) {
+     $self->runCmd(0, "echo Doing nothing for \"undo\" Tuning Manager.\n");  
+  } else {
+      $self->runCmd($test, $cmd);
+  }
+}
+
+
+
