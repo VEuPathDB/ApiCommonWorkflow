f599022987ba0bb0ac2cd81e67688f4962feee8f
Wei Li <weili1@pennmedicine.upenn.edu>
2012-05-31 19:07:54 +0000

    Ref Bug #9183
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/102@48246 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CopyExpressionProfilesToDownloadDir.pm
+++ Main/lib/perl/WorkflowSteps/CopyExpressionProfilesToDownloadDir.pm
@@ -15,26 +15,30 @@ sub run {
   # this is relative to the website files dir.
   # it will look something like downloadSite/ToxoDB/release-6.3
   my $relativeDir = $self->getParamValue('relativeDir');
+  my $experimentResourceName = $self->getParamValue('experimentResourceName');
 
   my $websiteFilesDir = $self->getWebsiteFilesDir($test);
 
   my $organismNameForFiles =
       $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
 
-  my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/transcriptExpression";
+  my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/transcriptExpression/$experimentResourceName";
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
-  my $cmd = "copyExpressionProfilesToDownloadDir --inputDir $workflowDataDir/$copyFromDir  --outputDir $copyToDir --configFile $workflowDataDir/$configFile";
+  my $cmd_mkdir = "mkdir -p $copyToDir";
+
+  my $cmd_copy = "copyExpressionProfilesToDownloadDir --inputDir $workflowDataDir/$copyFromDir  --outputDir $copyToDir --configFile $workflowDataDir/$configFile";
 
   if ($test) {
     $self->testInputFile('copyFromDir', "$workflowDataDir/$copyFromDir");
   }
 
   if ($undo) {
-    $self->runCmd(0, "rm -fr $copyToDir/*");
+    $self->runCmd(0, "rm -fr $copyToDir");
   } else {
-    $self->runCmd($test, $cmd);
+    $self->runCmd($test, $cmd_mkdir);
+    $self->runCmd($test, $cmd_copy);
   }
 
 }
