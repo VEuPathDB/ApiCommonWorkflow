d5115497142a2751f6312d70eef3d315429288c8
Wei Li <weili1@pennmedicine.upenn.edu>
2012-06-04 13:43:37 +0000

    add step to copy BAM file and index to WebServices
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@48279 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CopyBAMFileToWebServices.pm
@@ -0,0 +1,49 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CopyBAMFileToWebServices;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use ApiCommonWorkflow::Main::WorkflowSteps::WebsiteFileMaker;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # standard parameters for making download files
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $projectName = $self->getParamValue('projectName');
+  my $projectVersion = $self->getParamValue('projectVersionForWebsiteFiles');
+  my $dataName = $self->getParamValue('dataName');
+  my $service = $self->getParamValue('service');
+  my $inputFile=$self->getParamValue('inputFile');
+  my $experimentName=$self->getParamValue('experimentName');
+  my $snpStrain=$self->getParamValue('snpStrain');
+
+  # extra params for this step
+  my $webServicesRelativeDir = $self->getParamValue('relativeWebServicesDir');
+
+  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+
+  my $organismNameForFiles = $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
+
+  my $copyToDir = "$websiteFilesDir/$webServicesRelativeDir/$organismNameForFiles/bam/$experimentName/$snpStrain/";
+
+  if($undo) {
+    $self->runCmd(0, "rm -f $copyToDir/*");
+  } else{
+      if($test){
+	  $self->testInputFile('inputFile', "$inputFile");
+	  $self->runCmd(0, "mkdir -p $copyToDir");
+      }else {
+	  $self->runCmd($test, "mkdir -p $copyToDir");
+	  $self->runCmd($test, "cp $inputFile $copyToDir");
+       }
+  }
+}
+
+sub getConfigDeclaration {
+  return (
+         # [name, default, description]
+         # ['', '', ''],
+         );
+}
+
