64d30280bdf0f71739cfbc8e415c910ca8c6b85f
Wei Li <weili1@pennmedicine.upenn.edu>
2012-06-04 12:53:29 +0000

    new dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@48278 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeBWATaskInputDir.pm
@@ -0,0 +1,82 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeBWATaskInputDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameter values
+  my $taskInputDir = $self->getParamValue("taskInputDir");
+  my $readsFile = $self->getParamValue("readsFile");
+  my $hasPairedReads = $self->getBooleanParamValue("hasPairedReads");
+  my $genomicSeqsFile = $self->getParamValue("genomicSeqsFile");
+  my $bwaIndexDir = $self->getParamValue("bwaIndexDir");
+  my $strain = $self->getParamValue("strain");
+  my $consPercentCutoff = $self->getParamValue("consPercentCutoff");
+  my $snpPercentCutoff = $self->getParamValue("snpPercentCutoff");
+  my $editdistance = $self->getParamValue("SNPs");
+  my $includeIndels = $self->getParamValue("includeIndels");
+
+
+  my $taskSize = $self->getConfig("taskSize");
+  my $clusterWorkflowDataDir = $self->getClusterWorkflowDataDir();
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  if ($undo) {
+    $self->runCmd(0, "rm -rf $workflowDataDir/$taskInputDir/");
+  }else {
+      if ($test) {
+	  $self->testInputFile('readsFile', "$workflowDataDir/$readsFile");
+	  $self->testInputFile('genomicSeqsFile', "$workflowDataDir/$genomicSeqsFile");
+      }
+
+      $self->runCmd(0,"mkdir -p $workflowDataDir/$taskInputDir");
+
+      # make controller.prop file
+      $self->makeDistribJobControllerPropFile($taskInputDir, 1, $taskSize,
+				       "DJob::DistribJobTasks::HtsSnpTask", 0); 
+      # make task.prop file
+      my $taskPropFile = "$workflowDataDir/$taskInputDir/task.prop";
+      open(F, ">$taskPropFile") || die "Can't open `task prop file '$taskPropFile' for writing";
+
+      my $taskPropFileContent="
+fastaFile=$clusterWorkflowDataDir/$genomicSeqsFile
+mateA=$clusterWorkflowDataDir/$readsFile
+bwaIndex=$clusterWorkflowDataDir/$bwaIndexDir
+strain=$strain
+consPercentCutoff=$consPercentCutoff
+snpPercentCutoff=$snpPercentCutoff
+editDistance=$editDistance
+includeIndels=$includeIndels
+";
+
+      $taskPropFileContent .= "mateB=$clusterWorkflowDataDir/$readsFile.paired\n" if($hasPairedReads);
+      print F "$taskPropFileContent\n";
+       close(F);
+  }
+}
+
+sub getParamsDeclaration {
+  return ('taskInputDir',
+	  'readsFile',
+	  'hasPairedReads',
+	  'genomicSeqsFile',
+	  'bwaIndexDir',
+	  'strain',
+	  'consPercentCutoff',
+	  'snpPercentCutoff',
+	  'editdistance',
+	  'includeIndels',
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	  ['taskSize', "", ""],
+	 );
+}
+
