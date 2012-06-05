edd5c98d4d7ad6418e03ca931ecb2dde1473950e
Steve Fischer <sfischer@upenn.edu>
2012-06-05 12:56:50 +0000

    upgrade
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@48299 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/ImportOrthomclTestData.pm
+++ Main/lib/perl/WorkflowSteps/ImportOrthomclTestData.pm
@@ -9,17 +9,54 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
   my ($self, $test, $undo) = @_;
 
-  my $taxonDir = $self->getParamValue('taxaDir');
+  # three cases:
+  # 1) no taxaDir supplied:  filter based on inputProteinFile
+  # 2) taxaDir supplied, and no table called ApiDB.SimilarSequences${suffix}_1 exists:  filter based on taxa in taxaDir
+  # 3) taxaDir supplied, and ApiDB.SimilarSequences${suffix}_1 exists:  use that table (drop ApiDB.SimilarSequences$suffix and use a synonym)
+
+  my $taxaDir = $self->getParamValue('taxaDir');
   my $suffix = $self->getParamValue('suffix');
+  my $inputProteinFile = $self->getParamValue('inputProteinFile');
+
+  my $cacheTableName = "ApiDB.SimilarSequences${suffix}_c"
+  my $sql = "sql to find number of tables";
+  my $cacheTableExists = $taxaDir && $self->runSqlFetchOneRow($test, "$sql");
+
 
   if ($undo) {
-    $self->runSqlFetchOneRow($test, "truncate table apidb.SimilarSequences$suffix");
+      if ($tableExists) {
+	  $sql = "sql to remove synonym";
+	  $self->runSqlFetchOneRow($test, "$sql");
+      } else {
+	  $self->runSqlFetchOneRow($test, "truncate table apidb.SimilarSequences$suffix");
+      }
   } else {
+      if (!$taxaDir) {
+	  $self->filterOnProteinIds($inputProteinFile, $suffix);
+      } else {
+	  if ($tableExists) {
+	      $self->createSynonym($suffix);
+	  } else {
+	      $self->filterOnTaxa($taxaDir, $suffix);
+	  }
+      }
+  }
+}
 
-    chdir $taxonDir;
-    my $taxonList = join(', ', map("'$_'", <*>));
+sub filterOnProteinIds {
+}
 
-    my $sql = <<"SQL";
+# drop apidb.SimilarSequences$suffix if it exists
+# create synonym
+sub createSynonym {
+}
+
+sub filterOnTaxa {
+    chdir $taxaDir || die "Can't chdir to '$taxaDir'\n";
+    my @taxonNames = map {/(\w+).fasta/; $1; } <*.fasta>;
+    my $taxonList = join(', ', @taxonNames);
+
+    $sql = <<"SQL";
           insert into apidb.SimilarSequences$suffix
                       (query_id, subject_id, query_taxon_id, subject_taxon_id,
                        evalue_mant, evalue_exp, percent_identity, percent_match)
@@ -31,6 +68,4 @@ sub run {
 SQL
 
     $self->runSqlFetchOneRow($test, $sql);
-
-  }
 }
