046e06e3e68a18f90cf15c880c49581125d736ec
Wei Li <weili1@pennmedicine.upenn.edu>
2012-09-05 17:44:22 +0000

    new dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/TriTryp_Tschudi@49838 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/AddPolyAGenes.pm
@@ -0,0 +1,39 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::AddPolyAGenes;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $gusConfigFile= "$ENV{GUS_HOME}/config/gus.config";
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd = "addPolyAGenes.pl --gusConfigFile $gusConfigFile --commit";
+  
+  if ($undo) {
+      #need to remove the rows from apidb.PolyAGenes
+  }else {
+      if ($test){
+      }else{
+	    $self->runCmd($test, $cmd);
+      }
+  }
+
+}
+
+sub getParamDeclaration {
+  return (
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
