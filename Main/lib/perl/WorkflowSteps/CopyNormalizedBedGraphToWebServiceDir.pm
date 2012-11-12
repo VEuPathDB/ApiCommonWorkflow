8cd4011a71ebf56dd220101310fca60a7b487de3
Wei Li <weili1@pennmedicine.upenn.edu>
2012-11-12 15:38:13 +0000

    add new step to copy normalized bedgraph files to webService Dir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@51515 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CopyNormalizedBedGraphToWebServiceDir.pm
@@ -0,0 +1,59 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CopyNormalizedBedGraphToWebServiceDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use ApiCommonWorkflow::Main::Util::OrganismInfo;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameters
+  my $copyFromDir = $self->getParamValue('copyFromDir');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $relativeDir = $self->getParamValue('relativeDir');
+  my $experimentResourceName = $self->getParamValue('experimentResourceName');
+
+  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+
+  my $organismNameForFiles =
+      $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
+
+  my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/bigwig/$experimentResourceName";
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd_mkdir = "mkdir -p $copyToDir";
+
+  my $cmd_copy = "copyNormalizedBedGraphToWebServiceDir --inputDir $workflowDataDir/$copyFromDir  --outputDir $copyToDir";
+
+  if ($test) {
+    $self->testInputFile('copyFromDir', "$workflowDataDir/$copyFromDir");
+  }
+
+  if ($undo) {
+    $self->runCmd(0, "rm -fr $copyToDir");
+  } else {
+    $self->runCmd($test, $cmd_mkdir);
+    $self->runCmd($test, $cmd_copy);
+  }
+
+}
+
+sub getParamsDeclaration {
+  return (
+          'copyFromDir',
+          'organismAbbrev',
+          'relativeDir',
+          'configFile',
+         );
+}
+
+sub getConfigDeclaration {
+  return (
+         # [name, default, description]
+         # ['', '', ''],
+         );
+}
+
+
