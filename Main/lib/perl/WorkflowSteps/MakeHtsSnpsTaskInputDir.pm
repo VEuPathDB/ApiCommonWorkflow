d0481d29583477871282e558ea1f91b164af5fa0
Deborah Pinney <pinney@upenn.edu>
2012-11-13 20:42:15 +0000

    add from trunk
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/105@51568 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeHtsSnpsTaskInputDir.pm
@@ -0,0 +1,93 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeHtsSnpsTaskInputDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameter values
+  my $taskInputDir = $self->getParamValue("taskInputDir");
+  my $readsFile = $self->getParamValue("readsFile");
+  my $pairedReadsFile = $self->getParamValue("pairedReadsFile");
+  my $hasPairedReads = $self->getBooleanParamValue("hasPairedReads");
+  my $genomicSeqsFile = $self->getParamValue("genomicSeqsFile");
+  my $indexDir = $self->getParamValue("indexDir");
+  my $strain = $self->getParamValue("strain");
+  my $clusterServer = $self->getSharedConfig('clusterServer');
+
+  # expects string true/false
+  my $isColorspace = $self->getParamValue("isColorspace");
+
+  my $sraQueryString = $self->getParamValue("sraQueryString");
+
+  my $taskSize = $self->getConfig("taskSize");
+  my $clusterWorkflowDataDir = $self->getClusterWorkflowDataDir();
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  if ($undo) {
+    $self->runCmd(0, "rm -rf $workflowDataDir/$taskInputDir/");
+  }else {
+      if ($test) {
+	  $self->testInputFile('readsFile', "$workflowDataDir/$readsFile");
+	  $self->testInputFile('genomicSeqsFile', "$workflowDataDir/$genomicSeqsFile");
+      }
+
+      $self->runCmd(0,"mkdir -p $workflowDataDir/$taskInputDir");
+
+      # make controller.prop file
+      $self->makeDistribJobControllerPropFile($taskInputDir, 1, $taskSize,
+				       "DJob::DistribJobTasks::HtsSnpTask", 0); 
+      # make task.prop file
+      my $taskPropFile = "$workflowDataDir/$taskInputDir/task.prop";
+      open(F, ">$taskPropFile") || die "Can't open `task prop file '$taskPropFile' for writing";
+
+      my $taskPropFileContent="
+fastaFile=$clusterWorkflowDataDir/$genomicSeqsFile
+bowtieIndex=$clusterWorkflowDataDir/$indexDir
+strain=$strain
+isColorspace=$isColorspace
+";
+      if(length($sraQueryString)>0){
+	  $taskPropFileContent .= "mateA=none\n";
+	  $taskPropFileContent .= "mateB=none\n";
+	  $taskPropFileContent .= "sraSampleIdQueryList=$sraQueryString\n";
+
+      }else {
+	  $taskPropFileContent .= "mateA=$clusterWorkflowDataDir/$readsFile\n";
+	  $taskPropFileContent .= "sraSampleIdQueryList=none\n";
+	  if($hasPairedReads){
+	      $taskPropFileContent .= "mateB=$clusterWorkflowDataDir/$pairedReadsFile\n";
+	  }else {
+	      $taskPropFileContent .= "mateB=none\n";
+	  }
+      }
+
+
+
+      print F "$taskPropFileContent\n";
+       close(F);
+  }
+}
+
+sub getParamsDeclaration {
+  return ('taskInputDir',
+	  'readsFile',
+	  'hasPairedReads',
+	  'genomicSeqsFile',
+	  'indexDir',
+	  'strain',
+          'isColorspace',
+          'sraQueryString',
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	  ['taskSize', "", ""],
+	 );
+}
+
