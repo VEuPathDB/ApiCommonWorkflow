87ec5014a0512170f9fd1342ba7cb8a21eea6d77
Brian Brunk <brunkb@upenn.edu>
2013-03-26 03:39:12 +0000

    bug fixes for hts snp graph and code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/htssnps_coverage@54249 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/PutVarscanConsInVarscanDir.pm
@@ -0,0 +1,43 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::PutVarscanConsInVarscanDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $varscanConsDir = $self->getParamValue('varscanConsDir');
+  my $strain = $self->getParamValue('strain');
+  my $mainResultDir = $self->getParamValue('mainResultDir');
+
+
+  my $cmd = "ln -s $mainResultDir/result.varscan.cons $varscanConsDir/$strain.varscan.cons";
+  my $undoCmd = "/bin/rm $varscanConsDir/$strain.varscan.cons";
+
+  if ($undo) {
+    $self->runCmd(0, $undoCmd);
+  } else {
+    if ($test) {
+      $self->runCmd(0,"echo $cmd > $varscanConsDir/$strain.varscan.cons");
+    }else{
+      $self->runCmd($test,$cmd);
+    }
+  }
+}
+
+sub getParamDeclaration {
+  return ('varscanConsDir',
+          'strain',
+          'mainResultDir'
+         );
+}
+
+sub getConfigDeclaration {
+  return (
+      # [name, default, description]
+     );
+}
+
+1;
