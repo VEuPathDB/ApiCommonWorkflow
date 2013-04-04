347e95f74e1d87b75a63001f064fbdfecf641a49
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2013-04-04 01:50:45 +0000

    debug -- add workflowDataDir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/htssnps_coverage@54460 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/PutVarscanConsInVarscanDir.pm
+++ Main/lib/perl/WorkflowSteps/PutVarscanConsInVarscanDir.pm
@@ -8,19 +8,20 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
   my ($self, $test, $undo) = @_;
 
+  my $workflowDataDir = $self->getWorkflowDataDir();
   my $varscanConsDir = $self->getParamValue('varscanConsDir');
   my $strain = $self->getParamValue('strain');
   my $mainResultDir = $self->getParamValue('mainResultDir');
 
 
-  my $cmd = "ln -s $mainResultDir/result.varscan.cons $varscanConsDir/$strain.varscan.cons";
-  my $undoCmd = "/bin/rm $varscanConsDir/$strain.varscan.cons";
+  my $cmd = "ln -s $workflowDataDir/$mainResultDir/result.varscan.cons $workflowDataDir/$varscanConsDir/$strain.varscan.cons";
+  my $undoCmd = "/bin/rm $workflowDataDir/$varscanConsDir/$strain.varscan.cons";
 
   if ($undo) {
     $self->runCmd(0, $undoCmd);
   } else {
     if ($test) {
-      $self->runCmd(0,"echo $cmd > $varscanConsDir/$strain.varscan.cons");
+      $self->runCmd(0,"echo $cmd > $workflowDataDir/$varscanConsDir/$strain.varscan.cons");
     }else{
       $self->runCmd($test,$cmd);
     }
