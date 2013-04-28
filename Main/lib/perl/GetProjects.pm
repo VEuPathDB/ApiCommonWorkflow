503a1540895396c0d16d84b86c07f2f701b75fca
Steve Fischer <sfischer@upenn.edu>
2013-04-28 21:40:28 +0000

    improve factoring of branching commands
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@55277 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/GetProjects.pm
@@ -0,0 +1,17 @@
+package ApiCommonWorkflow::Main::GetProjects;
+
+use strict;
+
+# get all the projects required by for the api common backend
+
+# NOTE: this should be driven off of build.xml.   this file is a stopgap till we get a chance to do that.
+
+sub getProjects {
+  return ('ApiCommonData:apidb', 'ApiCommonShared:apidb', 'ApiCommonWorkflow:apidb', 'CBIL:gus', 'DoTS:apidb', 'FgpUtil:gus', 'GGTools:apidb', 'GusAppFramework:gus', 'GusSchema:gus', 'install:gus', 'ReFlow:gus', 'WDK:gus', 'WSF:gus');
+}
+
+sub getClusterProjects {
+  return ('CBIL:gus', 'GGTools:apidb', 'install:gus', 'DJob:gus');
+}
+
+1;
