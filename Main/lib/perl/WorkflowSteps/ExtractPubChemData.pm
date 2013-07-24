084f7e7e0d2d0d6eaf710a3384d125cb52e3ef76
Bindu Gajria <bindu.gajria@gmail.com>
2013-07-24 20:10:51 +0000

    fixes for Compound classes - refs #13021
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@57116 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/ExtractPubChemData.pm
@@ -0,0 +1,46 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::ExtractPubChemData;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use File::Basename;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $idFile = $self->getParamValue('idFile');
+  my $outFile =  $self->getParamValue('outFile');
+  my $type =  $self->getParamValue('type');
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd = "extractPubChemData.pl  --idFile '$workflowDataDir/$idFile' --outFile '$workflowDataDir/$outFile' --type $type'";
+
+  if ($undo) {
+    $self->runCmd(0, "rm -f $workflowDataDir/$outFile");
+  }else {
+    if ($test){
+      $self->runCmd(0, "echo test> $workflowDataDir/$outFile");
+      $self->testInputFile('inputFile', "$workflowDataDir/$idFile");
+    }else{
+      $self->runCmd($test, $cmd);
+    }
+  }
+
+}
+
+sub getParamDeclaration {
+  return (
+	  'idFile',
+	  'outFile',
+	  'extDbRlsSpec',
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
