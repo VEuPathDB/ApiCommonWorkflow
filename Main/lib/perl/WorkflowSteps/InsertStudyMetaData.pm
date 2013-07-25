b15998c58232c4f59399b1390c8882937149086d
Wei Li <weili1@pennmedicine.upenn.edu>
2013-07-25 16:17:34 +0000

    debug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/metadata_refactor_071113@57132 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/InsertStudyMetaData.pm
+++ Main/lib/perl/WorkflowSteps/InsertStudyMetaData.pm
@@ -13,15 +13,15 @@ sub run {
   my $sampleMetaDataFile = $self->getParamValue('sampleMetaDataFile');
   my $readsFile = $self->getParamValue('readsFile');
   my $samplesExtDbRlsSpec = $self->getParamValue('sampleExtDbRlsSpec');
-  my $samplesExtDbRlsSpecStr = $samplesExtDbRlsSpec ?  '--samplesExtDbRlsSpec $samplesExtDbRlsSpec' : '';
+  my $samplesExtDbRlsSpecStr = $samplesExtDbRlsSpec ?  "--samplesExtDbRlsSpec \'$samplesExtDbRlsSpec\'" : '';
   my $workflowDataDir = $self->getWorkflowDataDir();
   my $baseFileName = basename($readsFile);
   my $sampleExtDbRlsSpecTemplate = $self->getParamValue('sampleExtDbRlsSpecTemplate');
-  my $sampleExtDbRlsSpecTemplateStr = $sampleExtDbRlsSpecTemplate ?  '--sampleExtDbRlsSpecTemplate $sampleExtDbRlsSpecTemplate' : '';
+  my $sampleExtDbRlsSpecTemplateStr = $sampleExtDbRlsSpecTemplate ?  "--sampleExtDbRlsSpecTemplate \'$sampleExtDbRlsSpecTemplate\'" : '';
   my $isProfile = $self->getParamValue('isProfile');
   $isProfile = $isProfile ?  '--isProfile' : '';
   my $studyExtDbRlsSpec = $self->getParamValue('studyExtDbRlsSpec');
-  my $studyExtDbRlsSpecStr = $studyExtDbRlsSpec ?  '--studyExtDbRlsSpec $studyExtDbRlsSpec' : '';
+  my $studyExtDbRlsSpecStr = $studyExtDbRlsSpec ?  "--studyExtDbRlsSpec \'$studyExtDbRlsSpec\'" : '';
   
 
   my $args = "--studyName '$experimentName' --file $workflowDataDir/$sampleMetaDataFile --sampleId $baseFileName $studyExtDbRlsSpecStr $samplesExtDbRlsSpecStr  $sampleExtDbRlsSpecTemplateStr $isProfile $samplesExtDbRlsSpecStr";
