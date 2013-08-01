07275ca48c2312a2458aeccc6f32dd07176f83f5
Wei Li <weili1@pennmedicine.upenn.edu>
2013-08-01 16:49:29 +0000

    workflow graphs for custom ontology, protein microarray metadata and profiles, merged from metadata_refactor_071113
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@57251 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/InsertStudyMetaData.pm
@@ -0,0 +1,49 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertStudyMetaData;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use File::Basename;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+  my $experimentName = $self->getParamValue("experimentName");
+  my $sampleMetaDataFile = $self->getParamValue('sampleMetaDataFile');
+  my $studyExtDbRlsSpec = $self->getParamValue('studyExtDbRlsSpec');
+  my $readsFile = $self->getParamValue('readsFile');
+  my $baseFileName = $readsFile ? basename($readsFile) : '';
+  my $sampleIdStr  = $readsFile ? "--sampleId $baseFileName" :'';
+  my $sampleExtDbRlsSpecTemplate = $self->getParamValue('sampleExtDbRlsSpecTemplate');
+  my $sampleExtDbRlsSpecTemplateStr = $sampleExtDbRlsSpecTemplate ?  "--sampleExtDbRlsSpecTemplate \'$sampleExtDbRlsSpecTemplate\'" : '';
+  my $isProfile = $self->getParamValue('isProfile');
+  $isProfile = $isProfile ?  '--isProfile' : '';
+
+  
+
+  my $args = "--studyName '$experimentName' --file $workflowDataDir/$sampleMetaDataFile --studyExtDbRlsSpec '$studyExtDbRlsSpec'  $sampleExtDbRlsSpecTemplateStr $isProfile $sampleIdStr";
+
+ unless ($test) {
+   $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertStudyMetaData", $args);
+  } 
+}
+
+sub getParamDeclaration {
+  return ('experimentName',
+          'sampleMetaDataFile',
+          'readsFile',
+          'sampleExtDbRlsSpecTemplate',
+          'isProfile',
+          'studyExtDbRlsSpec',
+         );
+}
+
+sub getConfigDeclaration {
+  return (
+      # [name, default, description]
+     );
+}
+
+1;
