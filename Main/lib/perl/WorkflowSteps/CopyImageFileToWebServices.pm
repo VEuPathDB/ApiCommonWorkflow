2347d5e81cb31fe743cbcb89fa3d0f2c2ae6bf89
Haiming Wang <hwang@uga.edu>
2014-02-20 20:06:02 +0000

    adds a workflow step to copy image files to webservice directory - refs #15302
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@60555 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CopyImageFileToWebServices.pm
@@ -0,0 +1,45 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CopyImageFileToWebServices;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use ApiCommonWorkflow::Main::WorkflowSteps::WebsiteFileMaker;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # standard parameters for making download files
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $projectName = $self->getParamValue('projectName');
+  my $inputFile=$self->getParamValue('inputFile');
+  my $datasetName=$self->getParamValue('datasetName');
+  my $webServicesRelativeDir = $self->getParamValue('relativeWebServicesDir');
+
+  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+
+  my $organismNameForFiles = $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $copyToDir = "$websiteFilesDir/$webServicesRelativeDir/$organismNameForFiles/image/$datasetName/";
+
+  if($undo) {
+    $self->runCmd(0, "rm -f $copyToDir/*");
+  } else{
+      if($test){
+        $self->testInputFile('inputFile', "$workflowDataDir/$inputFile");
+        $self->runCmd(0, "mkdir -p $copyToDir");
+      }else {
+        $self->runCmd($test, "mkdir -p $copyToDir");
+        $self->runCmd($test, "cp $workflowDataDir/$inputFile $copyToDir");
+       }
+  }
+}
+
+sub getConfigDeclaration {
+  return (
+         # [name, default, description]
+         # ['', '', ''],
+         );
+}
+
