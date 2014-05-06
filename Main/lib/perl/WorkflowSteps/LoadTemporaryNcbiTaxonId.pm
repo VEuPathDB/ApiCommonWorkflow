a5e48b5886fe5f8934419f5c3d584201555acd07
Wei Li <weili1@pennmedicine.upenn.edu>
2014-05-06 17:16:01 +0000

    load add synthetic taxon and taxonname rows in wf, refs #16020
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@62006 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/LoadTemporaryNcbiTaxonId.pm
@@ -0,0 +1,32 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::LoadTemporaryNcbiTaxonId;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $organismFullName = $self->getParamValue('organismFullName');
+  my $ncbiTaxonId = $self->getParamValue('ncbiTaxonId');
+  my $speciesNcbiTaxonId = $self->getParamValue('speciesNcbiTaxonId');
+
+   # validate temp ncbi taxon id
+  if ($ncbiTaxonId < 9000000000) {
+      $self->error("hasTemporaryNcbiTaxonId is true but the provided ncbi taxon ID does not look like a temporary one.  (It must be greater than 9000000000 to be a temp ID)");
+  }
+
+  my ($rank, $genetic_code_id, $mitochondrial_genetic_code_id) = $self->getParentInfoFromSpeciesNcbiTaxId($test,$speciesNcbiTaxonId);
+ 
+  my $args = "--parentNcbiTaxId $speciesNcbiTaxonId --parentRank $parentRank --ncbiTaxId $ncbiTaxonId --rank 'no rank' --name '$organismFullName' --nameClass 'scientific name' --geneticCodeId $geneticCodeId --mitochondrialGeneticCodeId $mitochondrialGeneticCodeId";
+
+  $self->runPlugin($test, $undo, "GUS::Supported::Plugin::InsertTaxonAndTaxonName", $args);
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+
