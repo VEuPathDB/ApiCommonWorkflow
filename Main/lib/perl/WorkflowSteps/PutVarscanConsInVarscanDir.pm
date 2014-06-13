d0104c977eba4d7df91821d7df146acd0f3fce38
John Brestelli <jbrestell@gmail.com>
2014-06-13 11:05:45 +0000

    workaround for <b22 workflows do not need to undo mapping on cluster steps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@62657 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/PutVarscanConsInVarscanDir.pm
+++ Main/lib/perl/WorkflowSteps/PutVarscanConsInVarscanDir.pm
@@ -13,8 +13,17 @@ sub run {
   my $strain = $self->getParamValue('strain');
   my $mainResultDir = $self->getParamValue('mainResultDir');
 
+  # This is a workaround so <bld 22 workflows do not need to undo the cluster steps for hts snps
+  #   NOTE:  The script for making the coverage.txt file is in DJob svn
+  #  TODO:  After gus 4 migration ... remove the "else"
+  my $cmd;
+  if(-e "$workflowDataDir/$mainResultDir/result.coverage.txt" ) {
+    $cmd = "ln -s $workflowDataDir/$mainResultDir/result.coverage.txt $workflowDataDir/$varscanConsDir/$strain.coverage.txt";
+  }
+  else {
+    $cmd = "parseVarscanToCoverage.pl -file $workflowDataDir/$mainResultDir/result.varscan.cons.gz -outputFile $workflowDataDir/$varscanConsDir/$strain.coverage.txt";
+  }
 
-  my $cmd = "ln -s $workflowDataDir/$mainResultDir/result.coverage.txt $workflowDataDir/$varscanConsDir/$strain.coverage.txt";
   my $undoCmd = "/bin/rm $workflowDataDir/$varscanConsDir/$strain.coverage.txt";
 
   if ($undo) {
