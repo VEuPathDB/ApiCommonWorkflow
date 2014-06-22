f8c30f825e3bfb1335d2ae19ce882040432f4116
Steve Fischer <sfischer@upenn.edu>
2014-06-22 13:21:00 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@62915 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/DownloadUniprotXml.pm
@@ -0,0 +1,40 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::DownloadUniprotXml;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $outputFastaFile = $self->getParamValue('outputFastaFile');
+  my $ouputEcFile = $self->getParamValue('ouputEcFile');
+  my $reviewedStatus = $self->getParamValue('reviewedStatus');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  if ($undo) {
+    $self->runCmd(0, "rm $workflowDataDir/$outputFastaFile") if -e "$workflowDataDir/$outputFastaFile";
+    $self->runCmd(0, "rm $workflowDataDir/$ouputEcFile") if -e "$workflowDataDir/$ouputEcFile";
+    $self->runCmd(0, "rm $workflowDataDir/$taxonFile") if -e "$workflowDataDir/$taxonFile";
+  } else {
+    my $xmlFile = $self->getStepDir() . "/uniprot.xml";
+    my $logFile = $self->getStepDir() . "/wget.log";
+    my $cmd = "wget -O $xmlFile 'http://www.uniprot.org/uniprot/?query=reviewed%3ayes+AND+ec%3a*&sort=score&format=xml'";
+    $self->runCmd($test, $cmd);
+    unless ($test){
+      my $wgetLogTail = $self->runCmd($test, "tail -2 $logFile|grep -i '[saved|FINISHED]'");
+      $self->error ("Wget did not successfully run. Check log file: $logFile\n") unless ($wgetLogTail);
+    }
+
+    my $taxonFile = $self->getStepDir() . "/taxa.dat";
+
+    ## find distinct ncbi taxon ids in externalaasequence,to limit uniprot to those
+    $cmd = "apiFindNcbiTaxonIds $taxonFile";
+    $self->runCmd($test, $cmd);
+
+    $cmd = "uniprotXmlToFasta -xmlFile uniprot.xml -taxonFile $taxonFile -fastaFile uniprot.fasta -ecMappingFile uniprot.ec";
+    $self->runCmd($test, $cmd);
+  }
+}
+
