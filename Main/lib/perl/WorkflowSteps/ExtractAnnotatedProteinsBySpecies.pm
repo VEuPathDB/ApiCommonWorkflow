02c433de8fb6feca23d18cc6c5456ae5e4a2860c
John Brestelli <jbrestell@gmail.com>
2014-11-26 19:43:31 +0000

    protein source_id instead of transcript
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@65492 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/ExtractAnnotatedProteinsBySpecies.pm
+++ Main/lib/perl/WorkflowSteps/ExtractAnnotatedProteinsBySpecies.pm
@@ -15,22 +15,22 @@ sub run {
   my $genomeDbRlsId = $self->getExtDbRlsId($test, $genomeExtDbRlsSpec);
   my $taxonId = $self->getTaxonIdFromNcbiTaxId($test,$ncbiTaxonId);
 
-  my $sql = "SELECT tx.source_id,g.product,
-                    'length='||length(t.sequence),t.sequence
+  my $sql = "SELECT tas.source_id,
+                    'length='||length(tas.sequence),tas.sequence
                FROM dots.NASequence x,
                     dots.transcript tx,
                     dots.nafeature f,
                     dots.genefeature g,
                     dots.translatedaafeature a,
-                    dots.translatedaasequence t
+                    dots.translatedaasequence tas
               WHERE x.taxon_id = $taxonId
                 AND x.external_database_release_id = $genomeDbRlsId
                 AND tx.parent_id = g.na_feature_id
                 AND x.na_sequence_id = f.na_sequence_id 
                 AND f.na_feature_id = a.na_feature_id
-                AND a.aa_sequence_id = t.aa_sequence_id
+                AND a.aa_sequence_id = tas.aa_sequence_id
                 AND a.na_feature_id = tx.na_feature_id
-                AND t.length > 9";
+                AND tas.length > 9";
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
