9ab103bd210e748a17d8241673de1c91368eacd7
Wei Li <weili1@pennmedicine.upenn.edu>
2015-01-28 21:18:44 +0000

    moved "addConstraints" step to "mapWithBowtie" graph w/ a dependency on only the htsSnpsExperiment subgraph call and added a proper undo which drops all of the constraints.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@66167 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/AddConstraintsToSnpTables.pm
+++ Main/lib/perl/WorkflowSteps/AddConstraintsToSnpTables.pm
@@ -12,11 +12,15 @@ sub run {
   my $gusInstance = $self->getGusInstanceName();
   my $gusLogin = $self->getGusDatabaseLogin();
   my $gusPassword = $self->getGusDatabasePassword();
-
+  my $workflowDataDir = $self->getWorkflowDataDir();
+  my $dataDir = $self->getParamValue('dataDir');
   my $cmd = "sqlplus $gusLogin/$gusPassword\@$gusInstance \@$ENV{GUS_HOME}/lib/sql/apidbschema/addConstraintsAndIndexesToSnpTables.sql ";
 
   if ($undo) {
-    # must undo the parent
+      my $sql="select 'alter table ' || owner || '.' || table_name || ' drop constraint ' || constraint_name || ';' as drops from all_constraints where owner = 'APIDB' and table_name in ('SNP','SEQUENCEVARIATION') and constraint_type in ('R','U','P')  order by table_name";
+      my $sql_cmd="makeFileWithSql --sql \"$sql\" --outFile $workflowDataDir/$dataDir/dropConstraintsToSnpTables.sql";
+      $self->runCmd($test, $sql_cmd);
+       my $undo_cmd = "sqlplus $gusLogin/$gusPassword\@$gusInstance $workflowDataDir/$dataDir/dropConstraintsToSnpTables.sql ";
   } else {
       $self->runCmd($test, $cmd);
   }
