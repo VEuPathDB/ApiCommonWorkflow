6e8690b060f697c94016bbbd8635930ff1ee4b4e
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2015-02-02 15:15:41 +0000

    Check in progress on genomic array workflow
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@66213 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/WriteGenomicArrayStudyConfig.pm
@@ -0,0 +1,41 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::WriteGenomicArrayStudyConfig;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+  my $file = $self->getParamValue('file');
+  my $configOutputFile = $self->getParamValue('configOutputFile');
+  my $analysisName = $self->getParamValue('analysisName');
+  my $protocolName = $self->getParamValue('protocolName');
+  my $sourceIdType = $self->getParamValue('sourceIdType');
+  my $inputProtocolAppNode = $self->getParamValue('inputProtocol');
+
+  my $paramValues = "--file $file --outputFile $workflowDataDir/$configOutputFile --name '$analysisName' --protocol '$protocolName' --sourceIdType $sourceIdType";
+
+  if (defined $inputProtocolAppNode) {
+    $paramValues = $paramValues." --inputProtocolAppNodes $inputProtocolAppNode";
+    }
+
+  my $cmd = "writeStudyConfig $paramValues";
+
+  if ($undo) {
+    $self->runCmd(0, "rm $workflowDataDir/$configOutputFile");
+  } else {
+      if ($test) {
+	  $self->runCmd(0,"echo test > $workflowDataDir/$configOutputFile");
+      } 
+
+      $self->runCmd($test, $cmd);
+  }
+}
+
+
+
+1;
