5d0881b5234097f940e86bfde092da8e13499f8d
Wei Li <weili1@pennmedicine.upenn.edu>
2015-03-02 03:13:29 +0000

    move "addConstraints" back to "project.xml". WF is always droping all constrains before adding them back.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@66601 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/AddConstraintsToSnpTables.pm
+++ Main/lib/perl/WorkflowSteps/AddConstraintsToSnpTables.pm
@@ -14,15 +14,17 @@ sub run {
   my $gusPassword = $self->getGusDatabasePassword();
   my $workflowDataDir = $self->getWorkflowDataDir();
   my $dataDir = $self->getParamValue('dataDir');
-  my $cmd = "sqlplus $gusLogin/$gusPassword\@$gusInstance \@$ENV{GUS_HOME}/lib/sql/apidbschema/addConstraintsAndIndexesToSnpTables.sql ";
+  my $sql="select 'alter table ' || owner || '.' || table_name || ' drop constraint ' || constraint_name || ';' as drops from all_constraints where owner = 'APIDB' and table_name in ('SNP','SEQUENCEVARIATION') and constraint_type in ('R','U','P')  order by table_name";
+  my $sql_cmd="makeFileWithSql --sql \"$sql\" --outFile $workflowDataDir/$dataDir/dropConstraintsToSnpTables.sql";
+  $self->runCmd($test, $sql_cmd);
+  my $drop_cmd = "sqlplus $gusLogin/$gusPassword\@$gusInstance $workflowDataDir/$dataDir/dropConstraintsToSnpTables.sql ";
+  my $add_cmd = "sqlplus $gusLogin/$gusPassword\@$gusInstance \@$ENV{GUS_HOME}/lib/sql/apidbschema/addConstraintsAndIndexesToSnpTables.sql ";
 
   if ($undo) {
-      my $sql="select 'alter table ' || owner || '.' || table_name || ' drop constraint ' || constraint_name || ';' as drops from all_constraints where owner = 'APIDB' and table_name in ('SNP','SEQUENCEVARIATION') and constraint_type in ('R','U','P')  order by table_name";
-      my $sql_cmd="makeFileWithSql --sql \"$sql\" --outFile $workflowDataDir/$dataDir/dropConstraintsToSnpTables.sql";
-      $self->runCmd($test, $sql_cmd);
-       my $undo_cmd = "sqlplus $gusLogin/$gusPassword\@$gusInstance $workflowDataDir/$dataDir/dropConstraintsToSnpTables.sql ";
+     $self->runCmd(0, "rm -f $workflowDataDir/$dataDir/dropConstraintsToSnpTables.sql");
   } else {
-      $self->runCmd($test, $cmd);
+      $self->runCmd(0, $drop_cmd);
+      $self->runCmd(0, $add_cmd);
   }
 }
 
