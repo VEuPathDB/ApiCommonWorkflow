473efded0ed878828eaff35962e0309446d57de0
John Brestelli <jbrestell@gmail.com>
2015-03-06 20:47:38 +0000

    rnaseq sample rename
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@66704 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CutFpkmFilesAndRename.pm
@@ -0,0 +1,31 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CutFpkmFilesAndRename;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $inputDirectory = $self->getParamValue('inputDirectory');
+  my $outputDirectory = $self->getParamValue('outputDirectory');
+  my $sampleName = $self->getParamValue('sampleName');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd = "renameRnaseqIntensityFiles.pl --inputDirectory $workflowDataDir/$inputDirectory --outputDirectory $workflowDataDir/$outputDirectory --sampleName '$sampleName'";
+
+  if ($undo) {
+    $self->runCmd(0, "rm -f $workflowDataDir/$outputDirectory/$sampleName*");
+  } else {
+      if ($test) {
+	  $self->runCmd(0,"echo test > $workflowDataDir/$outputDirectory/$sampleName");
+      }
+    $self->runCmd($test,$cmd);
+
+  }
+}
+
+1;
