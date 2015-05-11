0cc8a056e0e62107d0758825ae48e75caed0b2f2
Wei Li <weili1@pennmedicine.upenn.edu>
2015-05-11 20:15:51 +0000

    modify the step MakeDerivedTables to make organismAbbrev an optional param
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@67726 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeDerivedTables.pm
+++ Main/lib/perl/WorkflowSteps/MakeDerivedTables.pm
@@ -10,13 +10,16 @@ sub run {
   my ($self, $test, $undo) = @_;
 
   my $organismAbbrev = $self->getParamValue('organismAbbrev');
+
   my $tables = $self->getParamValue('tables');
 
   my $gusHome = $self->getSharedConfig('gusHome');
 
   my $instance = $self->getGusInstanceName();
 
-  my $tuningTablePrefix = $self->getTuningTablePrefix($organismAbbrev, $test);
+  my $tuningTablePrefix;
+
+  my $taxonId;
 
   my $apidbTuningPassword = $self->getConfig('apidbTuningPassword');
 
@@ -40,10 +43,19 @@ sub run {
   print F $xmlConfigFileString;
   close F;
 
-  my $taxonId = $self->getOrganismInfo($test, $organismAbbrev)->getTaxonId();
+ my $cmd = "tuningManager -instance '$instance' -propFile $stepDir/$xmlConfigFileName -doUpdate -notifyEmail none -configFile ${gusHome}/lib/xml/tuningManager/tuningManager.xml  " ;
+  
+ if ($organismAbbrev){
 
-  my $cmd = "tuningManager -prefix '$tuningTablePrefix' -instance '$instance' -propFile $stepDir/$xmlConfigFileName -doUpdate -notifyEmail none -tables $tables -configFile ${gusHome}/lib/xml/tuningManager/tuningManager.xml -filterValue $taxonId ";
+	$tuningTablePrefix = $self->getTuningTablePrefix($organismAbbrev, $test);
+    
+    $taxonId = $self->getOrganismInfo($test, $organismAbbrev)->getTaxonId();
 
+    $cmd .= "-filterValue $taxonId -prefix '$tuningTablePrefix'  " ;
+
+  }
+
+ $cmd .= "-tables $tables  " if ($tables);
 
   if ($undo) {
      $self->runCmd(0, "echo Doing nothing for \"undo\" Tuning Manager.\n");  
