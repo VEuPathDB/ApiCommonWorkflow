ba341555794a06742e6e39941026df1c5390d1cb
Elisabetta Manduchi <manduchi@upenn.edu>
2015-08-19 20:51:53 +0000

    Changing software generating normalized coverage plots for ChipSeq data (histone mod and mnase)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@69414 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CopyChipSeqBedGraphToWebServiceDir.pm
@@ -0,0 +1,46 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CopyChipSeqBedGraphToWebServiceDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use ApiCommonWorkflow::Main::Util::OrganismInfo;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameters
+  my $copyFromDir = $self->getParamValue('copyFromDir');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $relativeDir = $self->getParamValue('relativeDir');
+
+  my $experimentDatasetName = $self->getParamValue('experimentDatasetName');
+  my $experimentType = $self->getParamValue('experimentType');
+
+  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+
+  my $organismNameForFiles =
+      $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
+
+  my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/bigwig/$experimentDatasetName";
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd_mkdir = "mkdir -p $copyToDir";
+
+  my $cmd_copy = "copyChipSeqBedGraphToWebServiceDir.pl --inputDir $workflowDataDir/$copyFromDir  --outputDir $copyToDir --experimentType $experimentType"; 
+
+
+  $self->testInputFile('copyFromDir', "$workflowDataDir/$copyFromDir");
+
+  if ($undo) {
+    $self->runCmd(0, "rm -fr $copyToDir");
+  } else {
+    $self->runCmd($test, $cmd_mkdir);
+    $self->runCmd($test, $cmd_copy);
+  }
+
+}
+
+1;
+
+
--- Main/lib/perl/WorkflowSteps/GenerateChipSeqCvgPlots.pm
+++ Main/lib/perl/WorkflowSteps/GenerateChipSeqCvgPlots.pm
@@ -1,4 +1,4 @@
-package ApiCommonWorkflow::Main::WorkflowSteps::GenerateHomerCvgPlots;
+package ApiCommonWorkflow::Main::WorkflowSteps::GenerateChipSeqCvgPlots;
 
 @ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
 
@@ -9,13 +9,21 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run{
     my ($self, $test, $undo) = @_;
     
-    my $mainResDir = $self->getParamValue('mainResDir');    
-    my $inBamFile = "$mainResDir/results.bam";
-    my $outDir = "$bowtieResDir/homer/";  
+    my $clusterResultsDir = $self->getParamValue('clusterResultsDir');
     my $topLevelSeqSizeFile = $self->getParamValue('topLevelSeqSizeFile');
-    
-    my $cmd = "generateHomerCvgPlots.pl --inBamFile $inBamFile --outDir $outDir --topLevelSeqSizeFile $topLevelSeqSizeFile";
-    
+    my $experimentType = $self->getParamValue('experimentType');
+    my $inputName = $self->getParamValue('inputName');
+    my $fragmentLength = $self->getParamValue('fragmentLength');
+    my $workflowDataDir = $self->getWorkflowDataDir();
+    my $inBamFile = "$workflowDataDir/$clusterResultsDir/master/mainresult/$sampleName.bam";  
+    my $outDir = "$workflowDataDir/$clusterResultsDir/master/mainresult/downstream/"; 
+
+
+# Currently software for coverage plots has been assessed only for histone modification (HOMER will be used) and MNase (danpos2 will be used) experiments. As more assessments are done, the condition below should be modified appropriately. HOMER should work well also for TF binding.    
+    if ($experimentType eq 'histonemod' || $experimentType eq 'mnase') {
+	my $cmd = "generateChipSeqCvgPlots.pl --experimentType $experimentType --inBamFile $inBamFile --outDir $outDir --topLevelSeqSizeFile $topLevelSeqSizeFile --fragmentLength $fragmentLength";
+    }	
+
     if ($undo){
         $self->runCmd(0, "rm -rf $outDir");
     }
