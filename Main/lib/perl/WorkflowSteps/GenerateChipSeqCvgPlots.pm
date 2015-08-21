e03691c72784c316a92ef023d87b69f68f1ca320
Elisabetta Manduchi <manduchi@upenn.edu>
2015-08-21 16:07:23 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@69440 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/GenerateChipSeqCvgPlots.pm
+++ Main/lib/perl/WorkflowSteps/GenerateChipSeqCvgPlots.pm
@@ -18,11 +18,7 @@ sub run{
     my $inBamFile = "$workflowDataDir/$clusterResultsDir/master/mainresult/$sampleName.bam";  
     my $outDir = "$workflowDataDir/$clusterResultsDir/master/mainresult/downstream/"; 
 
-
-# Currently software for coverage plots has been assessed only for histone modification (HOMER will be used) and MNase (danpos2 will be used) experiments. As more assessments are done, the condition below should be modified appropriately. HOMER should work well also for TF binding.    
-    if ($experimentType eq 'histonemod' || $experimentType eq 'mnase') {
-	my $cmd = "generateChipSeqCvgPlots.pl --experimentType $experimentType --inBamFile $inBamFile --outDir $outDir --topLevelSeqSizeFile $topLevelSeqSizeFile --fragmentLength $fragmentLength";
-    }	
+    my $cmd = "generateChipSeqCvgPlots.pl --experimentType $experimentType --inBamFile $inBamFile --outDir $outDir --topLevelSeqSizeFile $topLevelSeqSizeFile --fragmentLength $fragmentLength";	
 
     if ($undo){
         $self->runCmd(0, "rm -rf $outDir");
