3e7724121dc0f4a4ceeb7c8d591fb0d08d8590f7
Wei Li <weili1@pennmedicine.upenn.edu>
2016-02-05 18:46:30 +0000

    initial Dev
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@72279 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeGffBigWigFile.pm
@@ -0,0 +1,44 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeGffBigWigFile;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use ApiCommonWorkflow::Main::Util::OrganismInfo;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameters
+  my $inputsDir = $self->getParamValue('inputsDir');
+  my $chromSizesFile = $self->getParamValue('chromSizesFile');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $relativeDir = $self->getParamValue('relativeDir');
+  my $experimentDatasetName = $self->getParamValue('experimentDatasetName');
+
+  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+
+  my $organismNameForFiles =
+      $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
+
+  my $outputsDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/bigwig/$experimentDatasetName";
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd_mkdir = "mkdir -p $$outputsDir";
+
+  my $cmd_createBigWig = "createGffBigWigFile --inputDir $workflowDataDir/$inputsDir --chromSizesFile $chromSizesFile --outputDir $outputsDir"; 
+ 
+  $self->testInputFile('copyFromDir', "$workflowDataDir/$inputsDir");
+
+  if ($undo) {
+    $self->runCmd(0, "rm -fr $outputsDir");
+  } else {
+    $self->runCmd($test, $cmd_mkdir);
+    $self->runCmd($test, $cmd_createBigWig);
+  }
+
+}
+
+1;
+
+
