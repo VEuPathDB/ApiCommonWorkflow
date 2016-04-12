aac920286b6f1ef172dfb9f4ed5962d081c3c779
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2016-04-12 10:33:00 +0000

    Add MakeCNVTaskInputDir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@73902 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeCNVTaskInputDir.pm
@@ -0,0 +1,57 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeCNVTaskInputDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use warnings;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameter values
+  my $taskInputDir = $self->getParamValue("taskInputDir");
+  my $genomicSeqsFile = $self->getParamValue("genomicSeqsFile");
+  my $bamFile = $self->getParamValue("bamFile");
+  my $gtfFile = $self->getParamValue("gtfFile");
+  my $samtoolsIndex = $self->getParamValue("samtoolsIndex");
+  my $sampleName = $self->getParamValue("sampleName");
+  my $window = $self->getParamValue("window");
+
+  my $taskSize = $self->getConfig("taskSize");
+  my $clusterWorkflowDataDir = $self->getClusterWorkflowDataDir();
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  if ($undo) {
+    $self->runCmd(0, "rm -rf $workflowDataDir/$taskInputDir/");
+  }else {
+
+    $self->testInputFile('readsFile', "$workflowDataDir/$gtfFile");
+
+    $self->runCmd(0,"mkdir -p $workflowDataDir/$taskInputDir");
+
+    # make controller.prop file
+    $self->makeDistribJobControllerPropFile($taskInputDir, 1, $taskSize,
+        "DJob::DistribJobTasks::CNVTasks", 0);
+ 
+    # make task.prop file
+    my $taskPropFile = "$workflowDataDir/$taskInputDir/task.prop";
+    open(F, ">$taskPropFile") || die "Can't open task prop file '$taskPropFile' for writing";
+
+    my $taskPropFileContent="
+        genomicSeqsFile=$clusterWorkflowDataDir/$genomicSeqsFile
+        bamFile=$clusterWorkflowDataDir/$bamFile
+        gtfFile=$clusterWorkflowDataDir/$gtfFile
+        samtoolsIndex=$clusterWorkflowDataDir/$samtoolsIndex
+        sampleName=$sampleName
+        window=$window
+        ";
+	
+      
+
+    print F "$taskPropFileContent\n";
+    close(F);
+  }
+}
+
+1;
