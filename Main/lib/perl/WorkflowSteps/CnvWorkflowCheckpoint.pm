03bdfcbc0a0f275b719c8c288bc845f1bf8150e8
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2016-05-06 21:27:46 +0000

    Add steps for cnv workflow checkpoint and insert DNAseq metrics
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@74358 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/CnvWorkflowCheckpoint.pm
@@ -0,0 +1,38 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CnvWorkflowCheckpoint;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $sampleName = $self->getParamValue('sampleName');
+  my $experimentName = $self->getParamValue('experimentName');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $projectName = $self->getParamValue('projectName');
+
+  $experimentName =~ s/_CNV//;
+  my $snpSampleExtDbSpec = "$organismAbbrev"."_$experimentName"."_$sampleName"."_HTS_SNPSample_RSRC|dontcare";
+  my $snpSampleExtDbRlsId = $self->getExtDbRlsId($test, $snpSampleExtDbSpec);
+
+  my $cmd = "cnvWorkflowCheckpoint --snpSampleExtDbRlsId $snpSampleExtDbRlsId --sampleName $sampleName --experimentName $experimentName --organismAbbrev $organismAbbrev --projectName $projectName";
+
+
+  if ($undo) {
+    $self->log("Nothing to undo\n");
+  } else {
+      if ($test) {
+      $self->log("Testing CNV checkpoint\n");;
+      } 
+
+      $self->runCmd($test, $cmd);
+  }
+  
+
+}
+
+
+1;
---
+++ Main/lib/perl/WorkflowSteps/InsertDNASeqMetrics.pm
@@ -0,0 +1,32 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertDNASeqMetrics;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+
+  my $analysisDir = $self->getParamValue('analysisDir');
+  my $studyName = $self->getParamValue('studyName');
+  my $sampleName = $self->getParamValue('sampleName');
+  my $sampleExtDbSpec = $self->getParamValue('sampleExtDbSpec');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $args = "--analysisDir $workflowDataDir/$analysisDir --studyName $studyName --assayName '$sampleName (DNA Sequencing)' --sampleExtDbSpec $sampleExtDbSpec --seqVariationNodeName '$sampleName (SequenceVariation)' --protocolName 'DNA Sequencing'";
+
+  
+    #$self->testInputFile('inputDir', "$workflowDataDir/$inputDir");
+    #$self->testInputFile('configFile', "$workflowDataDir/$configFile");
+
+
+  $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertDNASeqMetrics", $args);
+
+}
+
+
+1;
