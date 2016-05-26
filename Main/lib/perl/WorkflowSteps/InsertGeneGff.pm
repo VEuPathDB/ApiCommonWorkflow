f096b623c21492e1c47b7c92a497707202836811
John Iodice <iodice@pennmedicine.upenn.edu>
2016-05-26 02:51:48 +0000

    initial check-in
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@74630 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/InsertGeneGff.pm
@@ -0,0 +1,29 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertGeneGff;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+  my $relativeDir = $self->getParamValue('relativeDir');
+  my $organismNameForFiles = $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
+  my $speciesNameForFiles = $self->getOrganismInfo($test, $organismAbbrev)->getSpeciesNameForFiles();
+  my $isSpeciesLevel = $self->getIsSpeciesLevel();
+  my $familyNameForFiles = $self->getOrganismInfo($test, $organismAbbrev)->getFamilyNameForFiles();
+  my $isFamilyLevel = $self->getIsFamilyLevel();
+  my $projectName = $self->getParamValue('projectName');
+  my $projectVersion = $self->getParamValue('projectVersionForWebsiteFiles');
+  my $fileType = $self->getParamValue('fileType');
+  my $dataName = $self->getParamValue('dataName');
+
+  my $gffFile = getDownloadFileName($websiteFilesDir, $relativeDir, $organismNameForFiles, $speciesNameForFiles, $isSpeciesLevel, $familyNameForFiles, $isFamilyLevel, $projectName, $projectVersion, $fileType, $dataName);
+
+  my $args = "--gffFile $gffFile --projectName $projectName";
+
+  $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin:InsertGeneGff:", $args);
+}
+
+1;
