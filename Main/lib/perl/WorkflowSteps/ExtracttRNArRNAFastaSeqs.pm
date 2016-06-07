02e97a3aea8d1e5a02982716fbd3ad3d07b47e20
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2016-06-07 10:23:45 +0000

    Fix sql
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@74825 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/ExtracttRNArRNAFastaSeqs.pm
+++ Main/lib/perl/WorkflowSteps/ExtracttRNArRNAFastaSeqs.pm
@@ -13,19 +13,19 @@ sub run {
 
   my $taxonId = $self->getOrganismInfo($test, $organismAbbrev)->getTaxonId();
   my $sql = 
-    "select gf.source_id, sns.sequence
-     from dots.transcript t,
-     dots.splicednasequence sns,
-     sres.taxon tn,
-     sres.sequenceontology so,
-     dots.genefeature gf
-     where tn.taxon_id = $taxonId
-     and sns.taxon_id = tn.taxon_id
-     and so.term_name in ('tRNA_encoding', 'rRNA_encoding')
-     and gf.sequence_ontology_id = so.sequence_ontology_id
-     and t.parent_id = gf.na_feature_id
-     and t.na_sequence_id = sns.na_sequence_id
-     and gf.is_pseudo = 0";
+    "select gf.source_id
+    , sns.sequence
+    from dots.transcript t
+    , dots.splicednasequence sns
+    , sres.taxon tn
+    , sres.ontologyterm ot
+    , dots.genefeature gf
+    where tn.taxon_id = $taxonId
+    and sns.taxon_id = tn.taxon_id
+    and ot.name in ('tRNA_encoding', 'rRNA_encoding')
+    and gf.sequence_ontology_id = ot.ontology_term_id
+    and t.parent_id = gf.na_feature_id
+    and t.na_sequence_id = sns.na_sequence_id";
 
    my $workflowDataDir = $self->getWorkflowDataDir();
 
