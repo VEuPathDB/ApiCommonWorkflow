cb598b35730cfd567241173586896e52f0c3658d
John Brestelli <jbrestell@gmail.com>
2016-07-07 21:11:36 +0000

    first pass at gus4 wf for spliceleader and polyA
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/gus4@75351 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/InsertSplicedLeaderAndPolyASitesGenes.pm
+++ Main/lib/perl/WorkflowSteps/InsertSplicedLeaderAndPolyASitesGenes.pm
@@ -9,27 +9,12 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
   my ($self, $test, $undo) = @_;
 
-  my $type = $self->getParamValue('type');
+  my $genomeExtDbRlsSpec = $self->getParamValue('genomeExtDbRlsSpec');
+  my $extDbRlsSpec = $self->getParamValue('extDbRlsSpec');
 
-  my $sampleName = $self->getParamValue('sampleName');
+  my $args = "--genomeExtDbRlsSpec $genomeExtDbRlsSpec --extDbRlsSpec $extDbRlsSpec";
 
-  my $organismAbbrev = $self->getParamValue('organismAbbrev');
-
-  my $tuningTablePrefix = $self->getTuningTablePrefix($organismAbbrev, $test);
-
-  my $allowed="'Splice Site' or 'Poly A'";
-
-  my $workflowDataDir = $self->getWorkflowDataDir();
-
-  my $args = "--sampleName $sampleName --tuningTablePrefix $tuningTablePrefix";
-
-  if($type eq 'Splice Site'){
-      $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertSpliceSiteGenes", $args);
-  }elsif($type eq 'Poly A'){
-      $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertPolyAGenes", $args);
-  }else{
-      $self->error("Invalide type '$type'. Allowed types are: $allowed");
-  }
+  $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertSpliceSiteGenes", $args);
 }
 
 1;
