d1e8d43c7916ebb080366625807751a235b90dcf
Dave Falke <dmfalke@gmail.com>
2017-04-07 15:21:51 +0000

    update references to renamed projects #25426
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/branches/r25426@80848 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/GetProjects.pm
+++ Main/lib/perl/GetProjects.pm
@@ -7,7 +7,7 @@ use strict;
 # NOTE: this should be driven off of build.xml.   this file is a stopgap till we get a chance to do that.
 
 sub getProjects {
-  return ('ApiCommonData:apidb', 'ApiCommonShared:apidb', 'ApiCommonWorkflow:apidb', 'CBIL:gus', 'DoTS:apidb', 'FgpUtil:gus', 'GGTools:apidb', 'GusAppFramework:gus', 'GusSchema:gus', 'TuningManager:gus', 'install:gus', 'ReFlow:gus', 'WDK:gus', 'WSF:gus');
+  return ('ApiCommonData:apidb', 'ApiCommonModel:apidb', 'ApiCommonWorkflow:apidb', 'CBIL:gus', 'DoTS:apidb', 'FgpUtil:gus', 'GGTools:apidb', 'GusAppFramework:gus', 'GusSchema:gus', 'TuningManager:gus', 'install:gus', 'ReFlow:gus', 'WDK:gus', 'WSF:gus');
 }
 
 sub getClusterProjects {
