e9a3cb2ee896e536b4f624f7a0bd4a284e9b9095
John Brestelli <jbrestell@gmail.com>
2018-05-30 18:39:14 +0000

    typo
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@89318 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/MakeMercatorGffFile.pm
+++ Main/lib/perl/WorkflowSteps/MakeMercatorGffFile.pm
@@ -32,7 +32,7 @@ sub run {
 
     if ($undo) {
       $self->runCmd(0, "rm -fr $workflowDataDir/$outputFile");
-      $self->runCmd(0, "deleteSyntenty.pl --organismAbbrev $organismAbbrev --commit");
+      $self->runCmd(0, "deleteSynteny.pl --organismAbbrev $organismAbbrev --commit");
     } else {
 	if ($test) {
 	    $self->runCmd(0,"echo hello > $workflowDataDir/$outputFile");
