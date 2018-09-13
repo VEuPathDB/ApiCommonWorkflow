72f3d783c84f2625c4b5063162cd27ac9ba40a6f
Danielle Callan <dcallan@upenn.edu>
2018-09-13 13:50:50 +0000

    let datasets loaded as biom files with taxa strings as otu ids use this as well
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@91497 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/TaxonStringToNCBI.pm
+++ Main/lib/perl/WorkflowSteps/TaxonStringToNCBI.pm
@@ -13,15 +13,22 @@ sub run {
 
     my $extDBName = $self->getParamValue('extDBName');
     my $extDBVer = $self->getExtDbVersion($test, $extDBName);
+
     my $taxonStringFile = $extDBName . "/cluster/master/mainresult/final_taxonString.tab";
-    my $taxonMappingFile = "DADA2TaxonMapping_RSRC/taxonMapping.xml";
+    if (! -e "$workflowDataDir/$taxonStringFile") {
+      #then look for file copied from manual delivery
+      $taxonStringFile = $extDBName . "/final/taxonString.tab";
+    }
+
+    my $taxonMappingFile = "OverrideTaxonMapping_RSRC/taxonMapping.xml";
 
     my $args = "--sequenceToTaxonHierarchyFile $workflowDataDir/$taxonStringFile --taxonMappingFile $workflowDataDir/$taxonMappingFile --dbRlsSpec \"$extDBName|$extDBVer\"";
 
-    #not sure about the next
-    #$self->testInputFile('aliasesFile', "$fileFullPath");
-    $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertSequenceTaxon", $args);
-
+    #run only if we found a file for input
+    #otherwise assume we're sequences rather than taxon strings
+    if (-e "$workflowDataDir/$taxonStringFile") {
+      $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertSequenceTaxon", $args);
+    }
 }
 
 
