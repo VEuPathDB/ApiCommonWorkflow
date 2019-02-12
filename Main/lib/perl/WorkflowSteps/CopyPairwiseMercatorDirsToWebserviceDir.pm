283c795a01ec48963e65e1dfdede41ed45b3fa59
Wei Li <weili1@pennmedicine.upenn.edu>
2019-02-12 20:41:34 +0000

    be able to resume from failures, refs #33491
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@95250 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CopyPairwiseMercatorDirsToWebserviceDir.pm
+++ Main/lib/perl/WorkflowSteps/CopyPairwiseMercatorDirsToWebserviceDir.pm
@@ -18,6 +18,8 @@ sub run {
 
     my $workflowDataDir = $self->getWorkflowDataDir();
 
+    my $copiedPairDirsFile ="copiedPairDirsFile.txt";
+
     if ($undo) {
 	$self->runCmd(0, "rm -fr $outputDir");
 	return;
@@ -28,12 +30,17 @@ sub run {
 
     opendir(MERCATORDIR,"$workflowDataDir/$mercatorOutputsDir") or $self->error("Could not open $workflowDataDir/$mercatorOutputsDir for reading.\n");
 
+    open(COPIED, $workflowDataDir/$copiedPairDirsFile) or die "Cannot open file $workflowDataDir/$copiedPairDirsFile for reading: $!";
+    my @copiedPairDirs =  map { chomp; $_ } <COPIED>;
+    close COPIED;
+
     foreach my $pairDir (readdir MERCATORDIR){
-      next if ($pairDir =~ m/^\./);
+      next if ($pairDir =~ m/^\./ || grep( /^$pairDir$/, @copiedPairDirs));
       $self->runCmd(0,"mkdir -p $outputDir/$pairDir");
       if (!$test) {
 	$self->runCmd($test,"cp -R $workflowDataDir/$mercatorOutputsDir/$pairDir/alignments $outputDir/$pairDir");
 	$self->runCmd($test,"cp -R $workflowDataDir/$mercatorOutputsDir/$pairDir/*.agp $outputDir/$pairDir");
+    $self->runCmd($test, "echo $pairDir >>$workflowDataDir/$copiedPairDirsFile");
       }
     }
 }
