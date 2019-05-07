e6f6ac16fc4f4a5e31274d24f2c802d5e336a0aa
Danielle Callan <dcallan@upenn.edu>
2019-05-07 14:31:04 +0000

    mbio downloadSite files
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@98013 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Main/lib/perl/WorkflowSteps/MakeMbioDerivedTables.pm
@@ -0,0 +1,51 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeMbioDerivedTables;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $tables = $self->getParamValue('tables');
+
+  my $gusHome = $self->getSharedConfig('gusHome');
+
+  my $instance = $self->getGusInstanceName();
+
+  my $apidbTuningPassword = $self->getConfig('apidbTuningPassword');
+
+  my $dblink = $self->getConfig('dblink');
+
+  my $xmlConfigFileName="tmpConfigFile.xml";
+  my $xmlConfigFileString=
+"<?xml version='1.0'?>
+<tuningProps>
+  <password>$apidbTuningPassword</password>
+  <schema>ApidbTuning</schema>
+  <housekeepingSchema>apidb</housekeepingSchema>
+  <dblink>$dblink</dblink>
+  <logTable>apidb_r.TuningTableLog$dblink</logTable>
+  <dbaEmail>eupath-admin\@pcbi.upenn.edu</dbaEmail>
+  <fromEmail>tuningMgr\@pcbi.upenn.edu</fromEmail>
+</tuningProps>
+";
+  my $stepDir = $self->getStepDir();
+  open(F,">$stepDir/$xmlConfigFileName");
+  print F $xmlConfigFileString;
+  close F;
+
+ my $cmd = "tuningManager -instance '$instance' -propFile $stepDir/$xmlConfigFileName -doUpdate -notifyEmail none -configFile ${gusHome}/lib/xml/tuningManager/microbiomeTuningManager.xml  " ;
+  
+ $cmd .= "-tables $tables  " if ($tables);
+
+  if ($undo) {
+     $self->runCmd(0, "echo Doing nothing for \"undo\" Tuning Manager.\n");  
+  } else {
+      $self->runCmd($test, $cmd);
+  }
+}
+
+1;
