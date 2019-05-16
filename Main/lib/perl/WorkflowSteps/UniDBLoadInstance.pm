6349b9721defede867b252638a9685c1b4cfce5c
Jay Humphrey <jaycolin@apidb.org>
2019-05-16 14:33:20 +0000

    force skip / force load params
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@98224 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/UniDBLoadInstance.pm
+++ Main/lib/perl/WorkflowSteps/UniDBLoadInstance.pm
@@ -12,10 +12,15 @@ sub run {
   my $databaseInstance = $self->getParamValue('databaseInstance');
   my $readerClass = $self->getParamValue('readerClass');
   my $loaderLogDir = $self->getParamValue('loaderLogDir');
+  my $forceSkipDatasetFile = $self->getParamValue('forceSkipDatasetFile');
+  my $forceLoadDatasetFile = $self->getParamValue('forceLoadDatasetFile');
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
   my $args = "--database $databaseInstance --table_reader '$readerClass' --logDir $workflowDataDir/$loaderLogDir --mode load";
+
+	$args .= " --forceSkipDatasetFile $forceSkipDatasetFile" if(-e $forceSkipDatasetFile);
+	$args .= " --forceLoadDatasetFile $forceLoadDatasetFile" if(-e $forceLoadDatasetFile);
 
   $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertUniDB", $args);
 
