d87fcd9e777413982c2190211907f46cb9bd81d2
John Brestelli <jbrestell@gmail.com>
2019-07-19 18:37:58 +0000

    make the genome file if it does not exist
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWorkflow/trunk@99704 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/perl/WorkflowSteps/CopyBAMFileToWebServices.pm
+++ Main/lib/perl/WorkflowSteps/CopyBAMFileToWebServices.pm
@@ -48,10 +48,26 @@ sub run {
 
     # We need BOTH BAM and bigwig files for jbrowse (applies to ncRNASeq and DNASEq)
     if($isBamFile) {
+
+      my $genomeFile = "${dirname}/genome.txt";
+      if(!-e $genomeFile) {
+        my $ncbiTaxonId = $self->getOrganismInfo($test, $organismAbbrev)->getNcbiTaxonId();
+        my $tuningTablePrefix = $self->getTuningTablePrefix($organismAbbrev, $test);
+
+        my $sql = "select sa.source_id||chr(9)||ns.length
+            from ApidbTuning.${tuningTablePrefix}GenomicSeqAttributes sa, dots.nasequence ns
+            where sa.is_top_level = 1
+            and sa.na_sequence_id = ns.na_sequence_id
+            and sa.NCBI_TAX_ID = $ncbiTaxonId";
+
+        $self->runCmd($test,"makeFileWithSql --outFile $genomeFile --sql \"$sql\"");
+      }
+
+
       $self->runCmd($test, "mkdir -p $bwCopyToDir");
 #      $self->runCmd($test, "bedtools genomecov -ibam $workflowDataDir/$inputFile -bg |sort -k1,1 -k2,2n >${dirname}/tmpResult.bg");
       $self->runCmd($test, "bedtools genomecov -ibam $workflowDataDir/$inputFile -bg |LC_COLLATE=C sort -k1,1 -k2,2n >${dirname}/tmpResult.bg");
-      $self->runCmd($test, "bedGraphToBigWig ${dirname}/tmpResult.bg ${dirname}/genome.txt ${bwCopyToDir}/${basename}");
+      $self->runCmd($test, "bedGraphToBigWig ${dirname}/tmpResult.bg $genomeFile ${bwCopyToDir}/${basename}");
       $self->runCmd(0, "rm -f ${dirname}/tmpResult.bg");
     }
   }
