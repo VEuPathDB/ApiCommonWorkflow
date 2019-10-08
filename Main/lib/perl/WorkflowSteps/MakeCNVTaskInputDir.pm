e32f9b775cf5f77008fb6b3bba8299767bd855c2
Kathryn Crouch <crouch.k@gmail.com>
2019-10-08 09:44:36 -0400

    Remove extra semicolon

--- Main/lib/perl/WorkflowSteps/MakeCNVTaskInputDir.pm
+++ Main/lib/perl/WorkflowSteps/MakeCNVTaskInputDir.pm
@@ -44,7 +44,7 @@ sub run {
         genomicSeqsFile=$clusterWorkflowDataDir/$genomicSeqsFile
         bamFile=$clusterWorkflowDataDir/$snpsClusterDir/$bamFile
         gtfFile=$clusterWorkflowDataDir/$gtfFile
-        geneFootprintFile=$clusterWorkflowDataDir/$geneFootprintFile;
+        geneFootprintFile=$clusterWorkflowDataDir/$geneFootprintFile
         samtoolsIndex=$clusterWorkflowDataDir/$samtoolsIndex
         sampleName=$sampleName
         window=$window
