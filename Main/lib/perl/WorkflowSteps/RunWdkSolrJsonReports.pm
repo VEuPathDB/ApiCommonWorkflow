665558d52dbd9c0222b28ffe058559c36e5cdee3
Wei Li <weili1@pennmedicine.upenn.edu>
2019-12-03 13:57:45 -0500

    dev

--- Main/lib/perl/WorkflowSteps/RunWdkSolrJsonReports.pm
+++ Main/lib/perl/WorkflowSteps/RunWdkSolrJsonReports.pm
@@ -23,20 +23,21 @@ sub run {
     if ($paramName ne "" && $paramValue ne "") {
       $paramStr = " --paramName $paramName --paramValue $paramValue";
     }
-    elsif !($paramName eq "" && $paramValue eq "") {
-      $self->error("parameters 'paramName' and 'paramValue' must both either be empty or provided together");
-    }
+    #need Steve to fix it
+    #elsif !($paramName eq "" && $paramValue eq "") {
+    #  $self->error("parameters 'paramName' and 'paramValue' must both either be empty or provided together");
+    #}
 
     my $workflowDataDir = $self->getWorkflowDataDir();
     my $stepDir = $self->getStepDir();
 
-    my $cmd = "/usr/bin/python3 $ENV{GUS_HOME}/bin/runWdkReports $batchType $batchName 'http://localhost:7781' $outputDir $paramStr";
+    my $cmd = "/usr/bin/python3 $ENV{GUS_HOME}/bin/runWdkReports $batchType $batchName 'http://localhost:7781' $outputDirFullPath $paramStr";
 
     if ($undo) {
-	$self->runCmd(0, "rm -f $outputDir/solr-json-batch_$batchType_$batchName_*");  # we don't know the timestamp, so use wildcard
+	$self->runCmd(0, "rm -f $outputDirFullPath/solr-json-batch_${batchType}_${batchName}_*");  # we don't know the timestamp, so use wildcard
     } else {
       if ($test) {
-        $self->runCmd(0,"mkdir $outputDir/solr-json-batch_$batchType_$batchName_" + time);
+        $self->runCmd(0,"mkdir $outputDirFullPath/solr-json-batch_${batchType}_${batchName}_" + time);
       }
       $self->runCmd($test, $cmd);
     }
