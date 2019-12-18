c1ced89a5b65947b168ee5f37211b79d5fe54d85
Wei Li <weili1@pennmedicine.upenn.edu>
2019-12-18 12:29:30 -0500

    New workflow step to call EbiDumper

---
+++ Main/lib/perl/WorkflowSteps/RunEbiDumper.pm
@@ -0,0 +1,38 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::RunEbiDumper;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $ncbiTaxonId = $self->getParamValue('ncbiTaxonId');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $ebiVersion = $self->getParamValue('ebiVersion');
+  my $parentDataDir = $self->getParamValue('parentDataDir');
+  
+  my $workflowDataDir = $self->getWorkflowDataDir();
+  my $datasetName = "${organismAbbrev}_primary_genome_RSRC";
+  my $initDir = "$workflowDataDir/$parentDataDir/$datasetName";
+  my $mysqlDir = "$workflowDataDir/$parentDataDir/$datasetName/data";
+  my $outputDir = "$workflowDataDir/$parentDataDir/$datasetName/out";
+
+  $self->runCmd(0, "mkdir -p $mysqlDir");
+  $self->runCmd(0, "mkdir -p $outputDir");   
+  
+  my $cmd = "ebiDumper.pl -init_directory $initDir --mysql_directory $mysqlDir --output_directory $outputDir  --schema_definition_file $ENV{GUS_HOME}/lib/xml/gusSchemaDefinitions.xml --chromosome_map_file $ENV{GUS_HOME}/conf/chromosomeMap.conf.sample --ncbi_tax_id $ncbiTaxonId --container_name $organismAbbrev --dataset_name $datasetName --dataset_version $ebiVersion";
+
+  if ($undo) {
+    $self->runCmd(0, "rm -rf $mysqlDir");
+    $self->runCmd(0, "rm -rf $outputDir");
+  } else {
+    $self->testInputFile('initSqlFile', "$initDir/*.sql");
+    $self->runCmd($test,$cmd);
+  }
+}
+
+1;
+
