f83392a7327b0d638dbcd995f319ec98d2d4076d
Wei Li <weili1@pennmedicine.upenn.edu>
2019-12-18 13:27:29 -0500

    add --project_name and --project_release

--- Main/lib/perl/WorkflowSteps/RunEbiDumper.pm
+++ Main/lib/perl/WorkflowSteps/RunEbiDumper.pm
@@ -14,6 +14,8 @@ sub run {
   my $ebiVersion = $self->getParamValue('ebiVersion');
   my $parentDataDir = $self->getParamValue('parentDataDir');
   
+  my $project_name = $self->getWorkflowConfig('name');
+  my $project_release = $self->getWorkflowConfig('version');
   my $workflowDataDir = $self->getWorkflowDataDir();
   my $datasetName = "${organismAbbrev}_primary_genome_RSRC";
   my $initDir = "$workflowDataDir/$parentDataDir/$datasetName";
@@ -23,7 +25,7 @@ sub run {
   $self->runCmd(0, "mkdir -p $mysqlDir");
   $self->runCmd(0, "mkdir -p $outputDir");   
   
-  my $cmd = "ebiDumper.pl -init_directory $initDir --mysql_directory $mysqlDir --output_directory $outputDir  --schema_definition_file $ENV{GUS_HOME}/lib/xml/gusSchemaDefinitions.xml --chromosome_map_file $ENV{GUS_HOME}/conf/chromosomeMap.conf.sample --ncbi_tax_id $ncbiTaxonId --container_name $organismAbbrev --dataset_name $datasetName --dataset_version $ebiVersion";
+  my $cmd = "ebiDumper.pl -init_directory $initDir --mysql_directory $mysqlDir --output_directory $outputDir  --schema_definition_file $ENV{GUS_HOME}/lib/xml/gusSchemaDefinitions.xml --chromosome_map_file $ENV{GUS_HOME}/conf/chromosomeMap.conf.sample --ncbi_tax_id $ncbiTaxonId --container_name $organismAbbrev --dataset_name $datasetName --dataset_version $ebiVersion --project_name $project_name --project_release $project_release";
 
   if ($undo) {
     $self->runCmd(0, "rm -rf $mysqlDir");
