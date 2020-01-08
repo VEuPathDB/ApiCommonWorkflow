c6aee9f68e281763f6e4b9cdb2d90acf4e0455d5
John Brestelli <jbrestel@upenn.edu>
2020-01-08 14:40:45 -0500

    wget the ebi core db / previously unpack steps are now incorporated here

--- Main/lib/perl/WorkflowSteps/RunEbiDumper.pm
+++ Main/lib/perl/WorkflowSteps/RunEbiDumper.pm
@@ -9,29 +9,52 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
   my ($self, $test, $undo) = @_;
 
+  my $ebiFtpUser = $self->getConfig('ebiFtpUser');
+  my $ebiFtpPassword = $self->getConfig('ebiFtpPassword');
+
   my $ncbiTaxonId = $self->getParamValue('ncbiTaxonId');
   my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $ebi2gusTag = $self->getParamValue('ebi2gusTag');
   my $ebiVersion = $self->getParamValue('ebiVersion');
-  my $parentDataDir = $self->getParamValue('parentDataDir');
-  
+  my $ebiOrganismName = $self->getParamValue('ebiOrganismName');
+  my $genomeDir = $self->getParamValue('genomeDir');
+  my $outputDir = $self->getParamValue('outputDir');
+  my $datasetName = $self->getParamValue('datasetName');
+  my $chromosomeMapFile = $self->getParamValue('chromosomeMapFile');
+
   my $project_name = $self->getWorkflowConfig('name');
   my $project_release = $self->getWorkflowConfig('version');
+
   my $workflowDataDir = $self->getWorkflowDataDir();
-  my $datasetName = "${organismAbbrev}_primary_genome_RSRC";
-  my $initDir = "$workflowDataDir/$parentDataDir/$datasetName";
-  my $mysqlDir = "$workflowDataDir/$parentDataDir/$datasetName/data";
-  my $outputDir = "$workflowDataDir/$parentDataDir/$datasetName/out";
+
+  my $fullGenomeDir = "$workflowDataDir/$genomeDir";
+  my $chromosomeMapFullPath = "$workflowDataDir/$chromosomeMapFile";
+
+  my $unpackDir = "$fullGenomeDir/unpack";
+  my $initSqlDir = "$fullGenomeDir/sql";
+  my $mysqlDir = "$fullGenomeDir/mysql_data";
+
+  my $gusSchemaDefFullPath = "${unpackDir}/gusSchemaDefinitions.xml";
 
   $self->runCmd(0, "mkdir -p $mysqlDir");
   $self->runCmd(0, "mkdir -p $outputDir");   
-  
-  my $cmd = "ebiDumper.pl -init_directory $initDir --mysql_directory $mysqlDir --output_directory $outputDir  --schema_definition_file $ENV{GUS_HOME}/lib/xml/gusSchemaDefinitions.xml --chromosome_map_file $ENV{GUS_HOME}/conf/chromosomeMap.conf.sample --ncbi_tax_id $ncbiTaxonId --container_name $organismAbbrev --dataset_name $datasetName --dataset_version $ebiVersion --project_name $project_name --project_release $project_release --ebi2gus_tag $ebiVersion --organism_abbrev $organismAbbrev";
+  $self->runCmd(0, "mkdir -p $unpackDir");   
+  $self->runCmd(0, "mkdir -p $initSqlDir");   
+
+  my $cmd = "ebiDumper.pl -init_directory $initSqlDir --mysql_directory $mysqlDir --output_directory $outputDir --schema_definition_file $gusSchemaDefFullPath --chromosome_map_file $chromosomeMapFullPath --ncbi_tax_id $ncbiTaxonId --container_name $organismAbbrev --dataset_name $datasetName --dataset_version $ebiVersion --project_name $project_name --project_release $project_release --ebi2gus_tag $ebi2gusTag --organism_abbrev $organismAbbrev";
 
   if ($undo) {
     $self->runCmd(0, "rm -rf $mysqlDir");
     $self->runCmd(0, "rm -rf $outputDir");
+    $self->runCmd(0, "rm -rf $initSqlDir");
+    $self->runCmd(0, "rm -rf $unpackDir");
   } else {
-    $self->testInputFile('initSqlFile', "$initDir/*.sql");
+    $self->runCmd("wget --ftp-user ${ebiFtpUser} --ftp-password ${ebiFtpPassword} -O ${unpackDir}/init.sql.gz ftp://ftp-private.ebi.ac.uk:/EBIout/${ebiVersion}/coredb/${ebiOrganismName}/${ebiOrganismName}_core_*.gz");
+    $self->runCmd("gunzip -c ${unpackDir}/init.sql.gz >${initSqlDir}/init.sql");
+    $self->runCmd("generateDatabaseSchemaXml >$gusSchemaDefFullPath");
+
+    $self->testInputFile('initSqlFile', "$initSqlDir/*.sql");
+
     $self->runCmd($test,$cmd);
   }
 }
