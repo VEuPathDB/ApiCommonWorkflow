a1e420dae175ebb9b18abe9c48b91fd67caf036a
Kathryn Crouch <crouch.k@gmail.com>
2020-02-06 14:02:51 -0500

    Rearrange file names for downstream steps

--- Main/lib/perl/WorkflowSteps/FixSeqIdEbiRNASeq.pm
+++ Main/lib/perl/WorkflowSteps/FixSeqIdEbiRNASeq.pm
@@ -18,20 +18,29 @@ sub run {
   my $bedCmd = "awk '{print \"${organismAbbrev}:\"\$0}'";
 
   if ($undo) {
-      $self->runCmd(0, "rm $workflowDataDir/$samplesDir/*/*.fixed.bed");
-      $self->runCmd(0, "rm $workflowDataDir/$samplesDir/*/*.fixed.tab");
+      foreach my $bed (glob "$fullPath/*/*.bed") {
+        my $oldBed = "$bed.old";
+        $self->runCmd(0, "rm $bed");
+        $self->runCmd(0, "mv $oldBed $bed");
+      }
+      foreach my $junctions (glob "$fullPath/*/junctions.tab") {
+        my $oldJunctions = "$junctions.old";
+        $self->runCmd(0, "rm $junctions");
+        $self->runCmd(0, "mv $oldJunctions $junctions");
+      }
+
   } else {
 
     foreach my $bed (glob "$fullPath/*/*.bed") {
-      my $fixedBed = $bed;
-      $fixedBed =~ s/\.bed$/.fixed.bed/;
-      $self->runCmd(0, "awk '{print \"${organismAbbrev}:\"\$0}' $bed >$fixedBed");
+      my $oldBed = "$bed.old";
+      $self->runCmd(0, "mv $bed $oldBed");
+      $self->runCmd(0, "awk '{print \"${organismAbbrev}:\"\$0}' $oldBed >$bed");
     }
 
     foreach my $junction (glob "$fullPath/*/junctions.tab") {
-      my $fixedJunction = $junction;
-      $fixedJunction =~ s/\.tab$/.fixed.tab/;
-      $self->runCmd(0, "awk '{if(NR==1) {print \$0} else{print \"${organismAbbrev}:\"\$0}}' $junction >$fixedJunction");
+      my $oldJunction = "$junction.old";
+      $self->runCmd(0, "mv $junction $oldJunction");
+      $self->runCmd(0, "awk '{if(NR==1) {print \$0} else{print \"${organismAbbrev}:\"\$0}}' $oldJunction >$junction");
     }
 
   }
