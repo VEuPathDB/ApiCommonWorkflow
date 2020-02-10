1e8c9d20c24943d9d0876db2b14202349e3a3891
Kathryn Crouch <crouch.k@gmail.com>
2020-02-10 13:53:46 -0500

    Use correct delimiter and only look in correct dirs during undo

--- Main/lib/perl/WorkflowSteps/FixSeqIdEbiRNASeq.pm
+++ Main/lib/perl/WorkflowSteps/FixSeqIdEbiRNASeq.pm
@@ -14,16 +14,13 @@ sub run {
 
   my $fullPath = "$workflowDataDir/$samplesDir";
 
-  my $junctionsCmd = "awk '{if(NR==1) {print \$0} else{print \"${organismAbbrev}:\"\$0}}'";
-  my $bedCmd = "awk '{print \"${organismAbbrev}:\"\$0}'";
-
   if ($undo) {
-      foreach my $bed (glob "$fullPath/*/*.bed") {
+      foreach my $bed (glob "$fullPath/*RR*/*.bed") {
         my $oldBed = "$bed.old";
         $self->runCmd(0, "rm $bed");
         $self->runCmd(0, "mv $oldBed $bed");
       }
-      foreach my $junctions (glob "$fullPath/*/junctions.tab") {
+      foreach my $junctions (glob "$fullPath/*RR*/junctions.tab") {
         my $oldJunctions = "$junctions.old";
         $self->runCmd(0, "rm $junctions");
         $self->runCmd(0, "mv $oldJunctions $junctions");
@@ -34,13 +31,13 @@ sub run {
     foreach my $bed (glob "$fullPath/*/*.bed") {
       my $oldBed = "$bed.old";
       $self->runCmd(0, "mv $bed $oldBed");
-      $self->runCmd(0, "awk '{print \"${organismAbbrev}:\"\$0}' $oldBed >$bed");
+      $self->runCmd(0, "awk '{print \"${organismAbbrev}_\"\$0}' $oldBed >$bed");
     }
 
     foreach my $junction (glob "$fullPath/*/junctions.tab") {
       my $oldJunction = "$junction.old";
       $self->runCmd(0, "mv $junction $oldJunction");
-      $self->runCmd(0, "awk '{if(NR==1) {print \$0} else{print \"${organismAbbrev}:\"\$0}}' $oldJunction >$junction");
+      $self->runCmd(0, "awk '{if(NR==1) {print \$0} else{print \"${organismAbbrev}_\"\$0}}' $oldJunction >$junction");
     }
 
   }
