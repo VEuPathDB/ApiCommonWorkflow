48a95176b7908c378dc7ebd7ecbd6326cc3721ea
Paul Wilkinson <pwilkinson@elm.pcbi.upenn.edu>
2020-06-01 14:29:54 -0400

    First commit of VCF region CopyVCFToWebServiceDir.pm  #40671

---
+++ Main/lib/perl/WorkflowSteps/CopyVCFToWebServiceDir.pm
@@ -0,0 +1,47 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::CopyVCFToWebServiceDir;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use ApiCommonWorkflow::Main::Util::OrganismInfo;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  # get parameters
+  my $vcfFile = $self->getParamValue('vcfFile');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $relativeDir = $self->getParamValue('relativeDir');
+
+  my $experimentDatasetName = $self->getParamValue('experimentDatasetName');
+
+  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
+
+  my $organismNameForFiles =
+      $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
+
+  my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/vcf/$experimentDatasetName";
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $cmd_mkdir = "mkdir -p $copyToDir";
+
+  my $cmd_copy = "cp $workflowDataDir/$vcfFile $copyToDir"; 
+
+
+
+  $self->testInputFile('vcfFile', "$workflowDataDir/$vcfFile");
+
+  if ($undo) {
+    $self->runCmd(0, "rm -fr $copyToDir");
+  } else {
+    $self->runCmd($test, $cmd_mkdir);
+    $self->runCmd($test, $cmd_copy);
+    $self->runCmd($test, "cp $$workflowDataDir/$vcfFile.tbi $copyToDir");
+  }
+
+}
+
+1;
+
+
