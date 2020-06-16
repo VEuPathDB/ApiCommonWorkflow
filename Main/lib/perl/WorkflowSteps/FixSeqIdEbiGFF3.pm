9f2b845256e41611c498684acece9a793afca92e
Paul Wilkinson <pwilkinson@elm.pcbi.upenn.edu>
2020-06-16 06:16:19 -0400

    First commit of GFF region script

---
+++ Main/lib/perl/WorkflowSteps/FixSeqIdEbiGFF3.pm
@@ -0,0 +1,45 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::FixSeqIdEbiGFF3;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $gff3 = $self->getParamValue('gff3File');
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  $gff3 = "$workflowDataDir/$gff3";
+
+  if ($undo) {
+
+
+
+
+
+#      foreach my $vcf (glob "$fullPath/*.vcf.gz") {
+#              my $oldGff3 = "$gff3.old";
+              $gff3 =~ s/final\///;
+              $self->runCmd(0, "rm $gff3");
+#              $self->runCmd(0, "mv $oldGff3 $gff3");
+#             }
+#             foreach my $index (glob "$fullPath/*.tbi") {
+#             $self->runCmd(0, "rm $vcf.tbi");
+#             }
+
+
+
+  } else {
+
+
+    $self->runCmd($test, "ebiGFF3RegionNameMapping.pl --GFF3File $gff3 --organism_abbrev $organismAbbrev");
+
+  }
+
+}
+
+
+1;
