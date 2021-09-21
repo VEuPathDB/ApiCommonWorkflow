110304ecd6dca707738a5716fcb83e7264f0188d
Jay Humphrey <jaycolin@apidb.org>
2021-09-21 17:48:23 -0400

    Parameterize schema

--- Main/lib/perl/WorkflowSteps/InsertStudyCharacteristics.pm
+++ Main/lib/perl/WorkflowSteps/InsertStudyCharacteristics.pm
@@ -8,11 +8,11 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 
 sub run {
   my ($self, $test, $undo) = @_;
-  my $args = join(" ", map { sprintf("--%s %s", $_, $self->getParamValue($_)) } qw/datasetName owlFile commit/);
-  my $args = sprintf("--datasetName %s --file %s --owlFile %s",
+  my $args = sprintf("--datasetName %s --file %s --owlFile --schema %s",
     $self->getParamValue('datasetName'),
     $self->getParamValue('file'),
-    $self->getParamValue('owlFile')
+    $self->getParamValue('owlFile'),
+    $self->getParamValue('schema'),
   );
   
   $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertStudyCharacteristics", $args);
