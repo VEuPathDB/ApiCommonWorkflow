1e39f729604bc7bcb4a62f0116d1caa9396fa164
Wei Li <weili1@pennmedicine.upenn.edu>
2021-10-12 13:44:55 -0400

    add MakeNCBILinkoutsFiles_Nucleotide step

---
+++ Main/lib/perl/WorkflowSteps/MakeNCBILinkoutsFiles_Nucleotide.pm
@@ -0,0 +1,19 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeNCBILinkoutsFiles_Nucleotide;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WebsiteFileMaker);
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WebsiteFileMaker;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub getWebsiteFileCmd {
+    my ($self, $downloadFileName, $test) = @_;
+
+  # get parameters
+
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+
+  my $tuningTablePrefix = $self->getTuningTablePrefix($organismAbbrev, $test);
+
+  my $cmd = "makeNCBILinkoutsFiles_Nucleotide.pl -output $downloadFileName -tuningTablePrefix $tuningTablePrefix";
+  return $cmd;
+}
