7b50d48439536a55ccda2fcb6deee1175d8b17aa
Richard Demko <rdemko2332@gmail.com>
2023-01-03 15:42:55 -0500

    Working on dnaseq analysis

---
+++ Main/lib/perl/WorkflowSteps/MakeDnaSeqSingleExperimentNextflowConfig.pm
@@ -0,0 +1,83 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeDnaSeqSingleExperimentNextflowConfig;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use warnings;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $input = $self->getParamValue("input")
+  my $fromBAM = $self->getParamValue("fromBAM")
+  my $isLocal= $self->getParamValue("isLocal")
+  my $isPaired = $self->getParamValue("isPaired") ? "paired" : "single";
+  my $analysisDir = $self->getParamValue("analysisDir")
+  my $genomeFastaFile = $self->getParamValue("genomeFastaFile")
+  my $gtfFile = $self->getParamValue("gtfFile")
+  my $clusterResultDir = $self->getParamValue("clusterResultDir")
+  my $configFileName = $self->getParamValue("configFileName")
+  my $ploidy = $self->getParamValue("ploidy")
+  my $organismAbbrev = $self->getParamValue("organismAbbrev")
+  my $footprintFile = $self->getParamValue("footprintFile")
+  my $configPath = join("/", $self->getWorkflowDataDir(),  $self->getParamValue("analysisDir"), $self->getParamValue("configFileName"));
+  my $clusterResultDir = join("/", $self->getClusterWorkflowDataDir(), $self->getParamValue("clusterResultDir"));
+  my $hisat2Threads = $self->getConfig("hisat2Threads");
+  my $samtoolsThreads = $self->getConfig("samtoolsThreads");
+  my $minCoverage = $self->getConfig("minCoverage");
+  my $winLen = $self->getConfig("winLen");
+  my $varscanPValue = $self->getConfig("varscanPValue");
+  my $varscanMinVarFreqSnp = $self->getConfig("varscanMinVarFreqSnp");
+  my $varscanMinVarFreqCons = $self->getConfig("varscanMinVarFreqCons");
+  my $maxNumberOfReads = $self->getConfig("maxNumberOfReads");
+  my $hisat2Index = $self->getConfig("hisat2Index");
+  my $createIndex = $self->getConfig("createIndex");
+  my $trimmomaticAdaptorsFile = $self->getConfig("trimmomaticAdaptorsFile");  
+  my $ebiFtpUser = $self->getConfig("ebiFtpUser");  
+  my $ebiFtpPassword = $self->getConfig("ebiFtpPassword");  
+
+  my $executor = $self->getClusterExecutor();
+  my $queue = $self->getClusterQueue();
+
+  if ($undo) {
+    $self->runCmd(0,"rm -rf $configPath");
+  } else {
+    open(F, ">", $configPath) or die "$! :Can't open config file '$configPath' for writing";
+
+    print F
+"
+params {
+  inputDir = \"$baseDir/data/haploid_paired/chr1\"
+  fromBAM = $fromBAM
+  hisat2Threads = $hisat2Threads
+  isPaired = $isPaired
+  isLocal = $isLocal
+  organismAbbrev = \"$organismAbbrev\"
+  minCoverage = $minCoverage
+  genomeFastaFile = \"$genomeFastaFile\"
+  gtfFile = \"$gtfFile\"
+  footprintFile = \"$footprintFile\"
+  winLen = $winLen 
+  ploidy= $ploidy
+  hisat2Index = \"$hisat4Index\"
+  createIndex = $createIndex
+  outputDir = \"$clusterResultDir\"
+  trimmomaticAdaptorsFile = \"$trimmomaticAdaptorsFile\"
+  varscanPValue = $varscanPvalue
+  varscanMinVarFreqSnp = $varscanMinVarFreqSnp
+  varscanMinVarFreqCons = $varscanMinVarFreqCons
+  maxNumberOfReads = $maxNumberOfReads
+}
+
+ singularity {
+     enabled = true
+     runOptions = \"--user root\"
+ }
+";
+  close(F);
+ }
+}
+
+1;
+
---
+++ Main/lib/perl/WorkflowSteps/RetrieveDnaSeqSingleExperimentResultsFromComputeCluster.pm
@@ -0,0 +1,68 @@
+package ApiCommonWorkflow::Main::RetrieveDnaSeqSingleExperimentResultsFromComputeCluster;
+
+@ISA = (ReFlow::Controller::WorkflowStepHandle);
+
+use strict;
+use warnings;
+use ReFlow::Controller::WorkflowStepHandle;
+use File::Basename;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $clusterDir = join("/", $self->getClusterWorkflowDataDir(), $self->getParamValue("clusterDir"));
+  my $targetDir = join("/", $self->getWorkflowDataDir(), $self->getParamValue("targetDir"));
+
+  my $bam = ".bam";
+  my $concatvcfgz = ".concat.vcf.gz";
+  my $indeltsv = ".indel.tsv";
+  my $resultvcfgz = "result.vcf.gz";
+  my $bambai = ".bam.bai";
+  my $concatvcfgztbi = ".concat.vcf.gz.tbi";  
+  my $pileup = ".result.pileup";
+  my $resultvcfgztbi "result.vcf.gz.tbi";
+  my $bigwig = ".bw";
+  my $consensus = "consensus.fa.gz";
+  my $varscan = ".varscan.cons.gz";
+
+  if($undo){
+    for my $fileName ($bam, $concatvcfgz, $indeltsv, $resultvcfgz, $bambai, $concatvcfgztbi, $pileup, $resultvcfgztbi, $bigwig, $consensus, $varscan){
+      $self->runCmd(0, "rm -f $targetDir/*$fileName");
+    }
+    $self->runCmd(0, "rm -rf $targetDir/CNVs");
+  }else {
+    if ($test) {
+      for my $fileName ($bam, $concatvcfgz, $indeltsv, $resultvcfgz, $bambai, $concatvcfgztbi, $pileup, $resultvcfgztbi, $bigwig, $consensus, $varscan){
+        $self->runCmd(0, "echo test > $targetDir/test$fileName");
+      }
+    } else {
+      my $tmp = "$targetDir/tmp." . int(rand(10000));
+      $self->runCmd(0, "mkdir $tmp");
+      for my $fileName ($bam, $concatvcfgz, $indeltsv, $resultvcfgz, $bambai, $concatvcfgztbi, $pileup, $resultvcfgztbi, $bigwig, $consensus, $varscan){
+        $self->copyFromCluster("$clusterDir", "*$fileName", $tmp, 0);
+      }
+      $self->copyFromCluster("$clusterDir", "CNVs/*", $tmp, 0);
+      $self->runCmd(0, "mv $tmp/* $targetDir");   
+      $self->runCmd(0, "rmdir $tmp");
+    }
+  }
+
+}
+
+sub getParamDeclaration {
+  return (
+	"clusterDir",
+	"targetDir",
+	"otuCountsFileName",
+	"pathwayAbundancesFileName",
+	"pathwayCoveragesFileName",
+	"level4ECsFileName",
+	 );
+}
+
+sub getConfigDeclaration {
+  return (
+	  # [name, default, description]
+	 );
+}
+1;
