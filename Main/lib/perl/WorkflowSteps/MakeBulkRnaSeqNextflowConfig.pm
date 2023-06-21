33843448a2b2a561218475d29ce52bcb40d0dec6
Saikou Bah <sbah@yew.penn.apidb.org>
2023-06-21 10:44:40 -0400

    Modify bulk RNA Seq Config file

--- Main/lib/perl/WorkflowSteps/MakeBulkRnaSeqNextflowConfig.pm
+++ Main/lib/perl/WorkflowSteps/MakeBulkRnaSeqNextflowConfig.pm
@@ -9,8 +9,8 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
  my ($self, $test, $undo) = @_;
 
- #my $workflowDataDir = $self->getWorkflowDataDir();
- my $workflowDataDir = $self->getClusterWorkflowDataDir();
+ my $workflowDataDir = $self->getWorkflowDataDir();
+ #my $workflowDataDir = $self->getClusterWorkflowDataDir();
  my $dir = join("/", $workflowDataDir, $self->getParamValue("analysisDir")); 
 
  open(INTRON, "$dir/maxIntronLen") or die "Cannot read max intron len from $dir/maxIntronLen\n$!\n";
