2a0146f69e12c7f6ed70edf6891fff926567050d
Saikou Bah <sbah@yew.penn.apidb.org>
2023-07-26 06:31:27 -0400

    Modify the CopyLongReadRnaBamToWebSvc module to reflect sample names

--- Main/lib/perl/WorkflowSteps/CopyLongReadRnaBamToWebSvc.pm
+++ Main/lib/perl/WorkflowSteps/CopyLongReadRnaBamToWebSvc.pm
@@ -16,12 +16,31 @@ sub run {
     my $websiteFilesDir = $self->getWebsiteFilesDir($test);
     my $organismNameForFiles = $self->getOrganismInfo($test, $organismAbbrev)->getNameForFiles();
     my $workflowDataDir = $self->getWorkflowDataDir();
+    my $configFile = $self->getParamValue('analysisConfig');
 
+   # my $configFile = "/Users/saikouybah/Documents/PerlModules/analysisConfig.xml";
+    open(my $samplesList, $configFile) or die "Could not open file '$configFile' $!";
+
+    my %SampleHash;
+    while (my $row = <$samplesList>) {
+    	chomp $row;
+    	if ($row =~"<value>" ){
+    	    my $line = $row;
+    	    $line  =~ s/ .*<value>//g;
+    	    $line  =~ s/<\/value>//g;
+    	    my @samples_list = split('\|', $line);
+    	    my $sample_name = $samples_list[0] =~ s/ /_/gr;
+    	    my $internal_name = $samples_list[1];
+
+ 	    $SampleHash{$internal_name} = $sample_name;
+
+    }
+}
 
     my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/bam/$experimentResourceName/";
     my $cmd_mkdir = "mkdir -p $copyToDir";
 
-    my $cmd_copy = "cp $workflowDataDir/$copyFromDir/*bam* $copyToDir";
+    #my $cmd_copy = "cp $workflowDataDir/$copyFromDir/*bam* $copyToDir";
     
     $self->testInputFile('copyFromDir', "$workflowDataDir/$copyFromDir");
 
@@ -29,7 +48,14 @@ sub run {
         $self->runCmd(0,"rm -rf $copyToDir");
     }else{
         $self->runCmd($test, $cmd_mkdir);
-        $self->runCmd($test, $cmd_copy);
+	
+	foreach my $key (keys %SampleHash){
+	    my $name = $SampleHash{$key};
+	    my $cmd_copy = "cp $workflowDataDir/$copyFromDir/$key.bam $copyToDir/$name.bam";
+	    $self->runCmd($test, $cmd_copy);
+           } 
+	#
+        #$self->runCmd($test, $cmd_copy);
     }
 }
 
