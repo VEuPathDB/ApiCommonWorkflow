c6e29d8eb7c5c002fdbd713f6b089f7b5a7d47a0
Mustafa Nural <mvnural@gmail.com>
2023-11-30 13:51:44 -0500

    Update ChEBI workflow step for postgres.

--- Main/lib/perl/WorkflowSteps/LoadChEBI.pm
+++ Main/lib/perl/WorkflowSteps/LoadChEBI.pm
@@ -9,20 +9,35 @@ use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 sub run {
   my ($self, $test, $undo) = @_;
 
-  my $gusInstance = $self->getGusInstanceName();
-  my $chebiLogin = 'chebi';
-  my $chebiPassword = $self->getConfig('chebiPassword');
+  my $login = $self->getGusDatabaseLogin();
+  my $password = $self->getGusDatabasePassword();
+  my $hostname = $self->getGusDatabaseHostname();
+  my $databaseName = $self->getGusDatabaseName();
+  my $connectionString = "postgresql://$login:$password\@$hostname/$databaseName";
 
   my $workflowDataDir = $self->getWorkflowDataDir();
   my $dataDir = $workflowDataDir . "/" . $self->getParamValue('dataDir'). "/" . $self->getParamValue('datasetName'). "/final" ;
 
-  my $cmd = "loadChEBI.bash -d $dataDir -i $gusInstance -u $chebiLogin -p $chebiPassword";
+  my @files = qw(
+    compounds.sql
+    chemical_data.sql
+    comments.sql
+    compound_origins.sql
+    database_accession.sql
+    names.sql
+    references.sql
+    relation.sql
+    structures.sql
+  );
 
   if ($undo) {
      $self->runCmd(0, "echo exit|sqlplus $chebiLogin/$chebiPassword\@$gusInstance \@$dataDir/disable_constraints.sql");
      $self->runCmd(0, "sqlplus $chebiLogin/$chebiPassword\@$gusInstance \@$ENV{GUS_HOME}/lib/sql/apidbschema/dropChebiTables.sql");
   } else {
     $self->runCmd($test, $cmd);
+    for my $sqlFile (@files) {
+      $self->runCmd($test, "psql --echo-all -v ON_ERROR_STOP=1 -c 'SET ROLE GUS_W' -f $dataDir/$sqlFile $connectionString");
+    }
   }
 }
 
