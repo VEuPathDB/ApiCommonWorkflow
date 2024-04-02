1ca7697bb05e5b40480b8d3e98edcaa80887b634
Saikou Y Bah <saikouybah@gmail.com>
2024-04-02 09:09:35 -0400

    Updated the project.xml to add template for the epitope analysis, add MakeEpitopeMappingNextflowConfig.pm to make the nextflow confing and the mapEpitopeNextflow.xml for the epitope mapping

---
+++ Main/lib/perl/WorkflowSteps/MakeEpitopeMappingNextflowConfig.pm
@@ -0,0 +1,56 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeEpitopeMappingNextflowConfig;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use warnings;
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+    my ($self, $test, $undo) = @_;
+   
+    my $workflowDataDir = $self->getWorkflowDataDir();
+    my $clusterWorkflowDataDir = $self->getClusterWorkflowDataDir();
+    my $results = join("/", $workflowDataDir, $self->getParamValue("clusterResultDir"));
+    my $refFasta = $self->getParamValue("refFasta");
+    my $peptidesTab = $self->getParamValue("peptidesTab");
+    my $peptideGeneFasta = $self->getParamValue("peptideGeneFasta");
+    my $taxon = $self->getParamValue("taxon");
+    my $peptideMatchResults = $self->getParamValue("peptideMatchResults");
+    my $peptidesFilteredBySpeciesFasta = $self->getParamValue("peptidesFilteredBySpeciesFasta");
+    my $peptideMatchBlastCombinedResults = $self->getParamValue("peptideMatchBlastCombinedResults");
+    my $chunkSize = $self->getParamValue("chunkSize");
+    my $configPath = join("/", $self->getWorkflowDataDir(), $self->getParamValue("configFileName"));
+
+
+   if ($undo) {
+    $self->runCmd(0,"rm -rf $configPath");
+  } else {  
+
+    open(F, ">", $configPath) or die "$! :Can't open config file '$configPath' for writing";
+ print F
+  " 
+ params {
+   refFasta = \"$refFasta\" 
+   peptidesTab = \"$peptidesTab\"
+   peptideGeneFasta = \"$peptideGeneFasta\"
+   taxon = $taxon
+   peptideMatchResults = \"$peptideMatchResults\"
+   peptidesFilteredBySpeciesFasta = \"$peptidesFilteredBySpeciesFasta\"
+   peptideMatchBlastCombinedResults = \"$peptideMatchBlastCombinedResults\"
+   chuckSize = $chunkSize
+   results = \"$results\"
+
+   }
+
+    singularity {
+    enabled = true
+    autoMounts = true
+   }
+  "
+
+  }
+
+}
+1;
