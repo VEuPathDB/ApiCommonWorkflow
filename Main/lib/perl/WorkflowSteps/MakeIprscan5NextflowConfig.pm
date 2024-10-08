600672aa08cfa7f3a04a074e0f30cb98aadc0446
Richard Demko <rdemko2332@gmail.com>
2024-10-08 09:45:39 -0400

    Removing include for non-ebi and using cluster server variable to location iprscan database

--- Main/lib/perl/WorkflowSteps/MakeIprscan5NextflowConfig.pm
+++ Main/lib/perl/WorkflowSteps/MakeIprscan5NextflowConfig.pm
@@ -17,7 +17,8 @@ sub run {
     my $fastaSubsetSize = $self->getParamValue("fastaSubsetSize");
     my $appls = $self->getParamValue("appls");
     my $outputFile = $self->getParamValue("outputFile");
-    my $interproscanDatabase = join("/", $self->getSharedConfig("consign.pmacs.upenn.edu.softwareDatabasesDirectory"),$self->getSharedConfig("interproscanDatabaseDirectory"));
+    my $clusterServer = $self->getSharedConfig("clusterServer");
+    my $interproscanDatabase = join("/", $self->getSharedConfig("$clusterServer.softwareDatabasesDirectory"),$self->getSharedConfig("interproscanDatabaseDirectory"));
     my $increasedMemory = $self->getParamValue("increasedMemory");
     my $initialMemory = $self->getParamValue("initialMemory");
     my $maxForks = $self->getParamValue("maxForks");
