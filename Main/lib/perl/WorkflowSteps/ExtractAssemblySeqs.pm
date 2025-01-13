26ed7238d8d9552e6d47430a073af6b95a39905a
Wei Li <weili1@pennmedicine.upenn.edu>
2025-01-13 18:29:59 -0500

    Debug

--- Main/lib/perl/WorkflowSteps/ExtractAssemblySeqs.pm
+++ Main/lib/perl/WorkflowSteps/ExtractAssemblySeqs.pm
@@ -15,9 +15,11 @@ sub run {
   $gusConfigFile = $self->getWorkflowDataDir() . "/$gusConfigFile";
 
   my $organismInfo = $self->getOrganismInfo($test, $organismAbbrev, $gusConfigFile);
-  my $taxonId = $organismInfo->getSpeciesTaxonId();
-  my $taxonIdList = $organismInfo->getTaxonIdList($taxonId);
+  #my $taxonId = $organismInfo->getSpeciesTaxonId();
+  #my $taxonIdList = $organismInfo->getTaxonIdList($taxonId);
 
+  my $speciesNcbiTaxonId = $self->getParamValue('speciesNcbiTaxonId');
+  my $taxonIdList = $organismInfo->getTaxonIdList($speciesNcbiTaxonId);
   my $workflowDataDir = $self->getWorkflowDataDir();
 
   my $args = "--taxon_id_list '$taxonIdList' --outputfile $workflowDataDir/$outputFile --extractonly";
