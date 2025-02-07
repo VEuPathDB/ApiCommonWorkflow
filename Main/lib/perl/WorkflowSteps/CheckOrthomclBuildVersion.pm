7940bca06de28289642918280a7ef014fc8a327b
Richard Demko <rdemko2332@gmail.com>
2025-02-07 12:44:45 -0500

    Ortho with new cluster functionality and entry used for nextflow workflows

--- Main/lib/perl/WorkflowSteps/CheckOrthomclBuildVersion.pm
+++ Main/lib/perl/WorkflowSteps/CheckOrthomclBuildVersion.pm
@@ -14,52 +14,21 @@ sub run {
   my $cachedBuildVersionFile = join("/", $orthoCacheDir,"buildVersion.txt");
   my $cachedCheckSumFile = join("/", $orthoCacheDir,"checkSum.tsv");
   my $checkSumFile = join("/", $workflowDataDir, $self->getParamValue("checkSum"));
-  my $skipMirrorFile = $self->getParamValue('skipMirrorFile');
-  my $skipIfFile = $self->getParamValue('skipIfFile');
-  $skipIfFile = join("/", $workflowDataDir, $skipIfFile);
-  $skipMirrorFile = join("/", $workflowDataDir, $skipMirrorFile);
-
   my $cachedBuildVersion = `cat $cachedBuildVersionFile`;
 
   my $diff_result = `diff $cachedCheckSumFile $checkSumFile`;
 
-  if($undo){
-      $self->runCmd(0, "rm -f $skipIfFile") if -e $skipIfFile;
-      $self->runCmd(0, "rm -f $skipMirrorFile") if -e $skipMirrorFile;
-      $self->runCmd(0, "rm -rf $workflowDataDir/coreGroups") if -e "$workflowDataDir/coreGroups";
+  if ($diff_result eq '') {
+    if ($cachedBuildVersion != $buildVersion) {
+      die "Cached build version $cachedBuildVersion and new build version $buildVersion are different even though the proteomes are the same\n";  
+    }
   }
   else {
-    if ($test) {
-      if ($diff_result eq '') {
-        if ($cachedBuildVersion ne $buildVersion) {
-          die "Cached build version $cachedBuildVersion and new build version $buildVersion are different even though the proteomes are the same\n";  
-        }
-        $self->runCmd(0, "echo test > $skipIfFile ");
-      }
-      else {
-        if ($cachedBuildVersion eq $buildVersion) {
-          die "Cached build version $cachedBuildVersion and new build version $buildVersion are identical even though the proteomes are different\n";  
-        }
-        $self->runCmd(0, "echo test > $skipMirrorFile ");
-      }
-    }
-    else {
-      if ($diff_result eq '') {
-        if ($cachedBuildVersion != $buildVersion) {
-          die "Cached build version $cachedBuildVersion and new build version $buildVersion are different even though the proteomes are the same\n";  
-        }
-        $self->runCmd(0, "echo real > $skipIfFile ");
-        $self->runCmd(0, "mkdir $workflowDataDir/coreGroups");
-        $self->runCmd(0, "cp -r $cachedCoreResults $workflowDataDir/coreGroups");
-      }
-      else {
-        if ($cachedBuildVersion eq $buildVersion) {
-          die "Cached build version $cachedBuildVersion and new build version $buildVersion are identical even though the proteomes are different\n";  
-        }
-        $self->runCmd(0, "echo real > $skipMirrorFile");
-      }
+    if ($cachedBuildVersion eq $buildVersion) {
+      die "Cached build version $cachedBuildVersion and new build version $buildVersion are identical even though the proteomes are different\n";  
     }
   }
 }
+
 
 1;
