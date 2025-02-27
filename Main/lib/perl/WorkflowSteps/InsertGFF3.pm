d1a562ff5c35b13cae522f24102403d1d74db5ec
Wei Li <weili1@pennmedicine.upenn.edu>
2025-02-27 11:15:42 -0500

    more dev

--- Main/lib/perl/WorkflowSteps/InsertGFF3.pm
+++ Main/lib/perl/WorkflowSteps/InsertGFF3.pm
@@ -13,11 +13,12 @@ sub run {
     my $experimentName = $self->getParamValue('experimentName');
     my $fileType = $self->getParamValue('fileType');
     my $gusConfigFile = $self->getParamValue('gusConfigFile');
+    my $version = $self->getParamValue('version');
     $gusConfigFile = $self->getWorkflowDataDir() . "/$gusConfigFile";
     my $workflowDataDir = $self->getWorkflowDataDir();
 
     my $gffFormat = substr($fileType, -1, 1);
-    my $gffFile = "$workflowDataDir/$copyFromDir/${experimentName}.gff"
+    my $gffFile = "$workflowDataDir/$copyFromDir/${experimentName}.gff";
     my $args = "--file $gffFile --gff3DbName ${organismAbbrev}_${experimentName}_RSRC --gff3DbVer $version --gffFormat $gffFormat --seqExtDbName ${organismAbbrev}_primary_genome_RSRC --soExtDbSpec 'SO_RSRC|%' --gusConfigFile $gusConfigFile";
   $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertGFF3", $args) if ($fileType =~ /^gff[123]$/);
 }
