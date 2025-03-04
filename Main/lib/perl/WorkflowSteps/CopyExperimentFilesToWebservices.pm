60ab56fe934f1cdbb769f7252dab362799e57fd9
John Brestelli <jbrestell@gmail.com>
2025-03-04 11:23:04 -0500

    add graph for loading genomic array experiments

--- Main/lib/perl/WorkflowSteps/CopyExperimentFilesToWebservices.pm
+++ Main/lib/perl/WorkflowSteps/CopyExperimentFilesToWebservices.pm
@@ -1,4 +1,4 @@
- package ApiCommonWorkflow::Main::WorkflowSteps::CopyBigWigFilesToWebServices;
+ package ApiCommonWorkflow::Main::WorkflowSteps::CopyExperimentFilesToWebServices;
 
 @ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
 use strict;
@@ -13,7 +13,7 @@ sub run {
     my $copyFromDir = $self->getParamValue('copyFromDir');
     my $organismAbbrev = $self->getParamValue('organismAbbrev');
     my $relativeDir = $self->getParamValue('relativeDir');
-    my $experimentResourceName = $self->getParamValue('experimentDatasetName');
+    my $experimentDatasetName = $self->getParamValue('experimentDatasetName');
     my $websiteFilesDir = $self->getWebsiteFilesDir($test);
     my $gusConfigFile = $self->getParamValue('gusConfigFile');
     $gusConfigFile = $self->getWorkflowDataDir() . "/$gusConfigFile";
@@ -21,12 +21,21 @@ sub run {
     my $organismNameForFiles = $self->getOrganismInfo($test, $organismAbbrev, $gusConfigFile)->getNameForFiles();
     my $workflowDataDir = $self->getWorkflowDataDir();
 
+    my $assayType = $self->getParamValue("assayType");
+    my $fileType = $self->getParamValue("fileType");
+    my $fileSuffix = $self->getParamValue("fileSuffix");
+    my $indexSuffix = $self->getParamValue("indexSuffix");
 
-    my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/bigwig/$experimentResourceName";
+    my $copyToDir = "$websiteFilesDir/$relativeDir/$organismNameForFiles/${assayType}/${fileType}/$experimentDatasetName";
     my $cmd_mkdir = "mkdir -p $copyToDir";
 
-    my $cmd_copy = "cp $workflowDataDir/$copyFromDir/*.bw $copyToDir";
+    my $indx_cmd_copy;
+    if($indexSuffix) {
+        $indx_cmd_copy = "cp $workflowDataDir/$copyFromDir/*.${indexSuffix} $copyToDir";
+    }
 
+
+    my $cmd_copy = "cp $workflowDataDir/$copyFromDir/*.${fileSuffix} $copyToDir";
     $self->testInputFile('copyFromDir', "$workflowDataDir/$copyFromDir");
 
     if ($undo) {
@@ -34,6 +43,7 @@ sub run {
     }else{
         $self->runCmd($test, $cmd_mkdir);
         $self->runCmd($test, $cmd_copy);
+        $self->runCmd($test, $index_cmd_copy) if($indexSuffix);
     }
 }
 
