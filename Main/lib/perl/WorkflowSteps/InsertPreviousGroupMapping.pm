11d1e26589dd4565794fcfdd215e58b55e7bbe8c
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2025-03-05 12:01:42 -0500

    add gusConfigFile

--- Main/lib/perl/WorkflowSteps/InsertPreviousGroupMapping.pm
+++ Main/lib/perl/WorkflowSteps/InsertPreviousGroupMapping.pm
@@ -9,9 +9,10 @@ sub run {
     my ($self, $test, $undo) = @_;
 
     my $workflowDataDir = $self->getWorkflowDataDir();
+	my $gusConfigFile = $workflowDataDir . "/" . $self->getParamValue('gusConfigFile');
     my $mappingFile = $workflowDataDir . "/" . $self->getParamValue('mappingFile');
   
-    my $args = " --mappingFile $mappingFile";
+    my $args = " --mappingFile $mappingFile --gusConfigFile $gusConfigFile";
 
     if($undo) {
         $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertPreviousGroupMapping", $args);
