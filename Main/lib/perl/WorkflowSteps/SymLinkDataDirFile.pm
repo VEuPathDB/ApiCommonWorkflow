b7d876a46d35b3e0dcc8f96ef796cbc1d8270e08
John Brestelli <jbrestell@gmail.com>
2025-04-04 10:33:27 -0400

    est blat for reference and non reference

--- Main/lib/perl/WorkflowSteps/SymLinkDataDirFile.pm
+++ Main/lib/perl/WorkflowSteps/SymLinkDataDirFile.pm
@@ -17,7 +17,15 @@ sub run {
       $self->runCmd(0, "rm -f $workflowDataDir/$toFile");
   } else {
     #$self->testInputFile('fromFile', "$workflowDataDir/$fromFile");
-    $self->runCmd(0, "ln -s $workflowDataDir/$fromFile $workflowDataDir/$toFile");
+
+
+    if(-e $workflowDataDir/$fromFile) {
+      $self->runCmd(0, "ln -s $workflowDataDir/$fromFile $workflowDataDir/$toFile");
+    }
+    else {
+      $self->error("Could not create a sym link.  File $workflowDataDir/$fromFile does not exist");
+    }
+
   }
 }
 
