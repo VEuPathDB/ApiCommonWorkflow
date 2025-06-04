02bf12d20a55a90820e525d23f5fc34891f8ef56
Richard Demko <rdemko2332@gmail.com>
2025-06-04 16:51:26 -0400

    Creating new ortho web ready tables. Loading ortho group blast values for orthomcl ec prediction

---
+++ Main/lib/perl/WorkflowSteps/InsertOrthoGroupBlastValues.pm
@@ -0,0 +1,25 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertOrthoGroupBlastValues;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $groupBlastValuesFile = $self->getParamValue('groupBlastValuesFile');
+    my $workflowDataDir = $self->getWorkflowDataDir();
+    my $gusConfigFile = $workflowDataDir . "/" . $self->getParamValue('gusConfigFile');
+  
+    my $args = " --groupBlastValuesFile=$workflowDataDir/$groupBlastValuesFile --gusConfigFile $gusConfigFile";
+
+    if($undo) {
+        $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertOrthoGroupBlastValues", $args);
+    } else {
+        $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertOrthoGroupBlastValues", $args);
+    }
+
+}
+
+1;
