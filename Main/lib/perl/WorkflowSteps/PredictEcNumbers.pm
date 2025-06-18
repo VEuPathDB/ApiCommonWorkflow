ebf9a8b5e118939866aad4bb9c268dec06ceaefe
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2025-06-18 10:26:28 -0400

    add gusConfigFile

--- Main/lib/perl/WorkflowSteps/PredictEcNumbers.pm
+++ Main/lib/perl/WorkflowSteps/PredictEcNumbers.pm
@@ -12,12 +12,13 @@ sub run {
   my $organismDir = $workflowDataDir."/".$self->getParamValue('organismDir');
   my $outputDir  = $self->getParamValue('outputDir');
   my $outputFile  = $workflowDataDir."/".$self->getParamValue('outputFile');
+  my $gusConfigFile = $self->getParamValue('gusConfigFile');
 
   if ($undo) {
     my $cmd = "rm $outputFile";
     $self->runCmd($test, $cmd);
   } else {
-    my $cmd = "orthomclEcPrediction $outputDir $organismDir $outputFile";
+    my $cmd = "orthomclEcPrediction $outputDir $organismDir $outputFile $gusConfigFile";
     $self->runCmd($test, $cmd);
   }
 }
