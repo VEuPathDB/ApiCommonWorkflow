52a5f57946196b7b39986f84b620d8daa802bf60
John Brestelli <39096257+jbrestel@users.noreply.github.com>
2025-06-26 12:25:07 -0400

    Bowtie (#53)
    * wip; add origins workflow
    
    * wip origins
    
    * wip; add spliced leader
    
    * rm unused graph
    
    * wip; add chipseq
    
    * wip; add readfiletype for chipseq
    
    * add paramValue organismDatasetLoaderXmlFile and gusConfigFile, and fix typo of parentDataDir
    
    * add param name gusConfigFile
    
    * use ${projectName} instead of $$projectName$$
    
    * configuration constant name: genomeFastaFile
    
    * pass genomeFastaFile from parent graphs
    
    * fix syntax issues
    
    * fix finalDir
    
    * fix syntax issue of bowtie-mapping-nextflow
    
    * fix dataDir for processChipSeq
    
    * wip; fix nextflow config and parent data dir issues
    
    * debug finalDir
    
    * Fix syntax errors in copyFromDir
    
    * add paramValue name gusConfigFile
    
    * fix parentDataDir for rnaSeqSplicedLeaderAndPolyASites_profiles
    
    * retrieve gusConfigFile from params and add it to the script
    
    * Use file copies rather than symbolic links when technologyType == SpliceSites
    
    * update profile set name for chipseq peaks
    
    * there is some tuning table needed to dump gff;  need to make a webready table
    
    ---------
    
    Co-authored-by: Sufen Hu <sufenhu@pennmedicine.upenn.edu>

---
+++ Main/lib/perl/WorkflowSteps/MakeSplicedLeaderAndPolyASitesNextflowConfig.pm
@@ -0,0 +1,84 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::MakeSplicedLeaderAndPolyASitesNextflowConfig;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use warnings;
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $datasetType = "spliceSites"; 
+  my $removePCRDuplicates = "false";
+
+  my $inputFileType = $self->getParamValue("inputFileType");
+
+  my $resultsDirectory = $self->getParamValue("resultsDirectory");
+  my $sampleSheet = $self->getParamValue("sampleSheetFile");
+  my $genomeFile = $self->getParamValue("genomeFile");
+  my $inputDir = $self->getParamValue("inputDir");
+
+  my $nextflowConfigFile = $self->getParamValue("nextflowConfigFile");
+  
+  my $hasPairedEnds = $self->getBooleanParamValue("hasPairedEnds") ? "true" : "false";
+
+  my $organismAbbrev = $self->getParamValue("organismAbbrev");
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $workingDirRelativePath = $self->getParamValue("workingDirRelativePath");
+  my $digestedInputDirPath = $self->relativePathToNextflowClusterPath($workingDirRelativePath, $inputDir);
+  
+  my $digestedGenomeFilePath = $self->relativePathToNextflowClusterPath($workingDirRelativePath, $genomeFile);
+  my $digestedOutputDir = $self->relativePathToNextflowClusterPath($workingDirRelativePath, $resultsDirectory);
+
+  my $clusterServer = $self->getSharedConfig('clusterServer');
+  my $clusterWorkflowDataDir = $self->getClusterWorkflowDataDir();
+  my $executor = $self->getClusterExecutor();
+
+  my $clusterConfigFile = "\$baseDir/conf/${executor}.config";
+
+  if ($undo) {
+      $self->runCmd(0, "rm $workflowDataDir/$nextflowConfigFile");
+  } else {
+      my $nextflowConfig = "$workflowDataDir/$nextflowConfigFile";
+      open(F, ">$nextflowConfig") || die "Can't open task prop file '$nextflowConfig' for writing";
+
+      my $configString = <<NEXTFLOW;
+params {
+    input                      = "$digestedInputDirPath"
+    outputDir                  = "$digestedOutputDir"
+    genome                     = "$digestedGenomeFilePath"
+    samplesheetFileName        = "$sampleSheet"
+    datasetType                = "$datasetType"
+    hasPairedEnds              = $hasPairedEnds
+    removePCRDuplicates        = $removePCRDuplicates                            
+    saveAlignments             = true
+    saveCoverage               = false
+    inputFileType              = "$inputFileType"
+}
+
+includeConfig "$clusterConfigFile"
+
+process {
+  maxForks = 10
+}
+
+process {
+
+    withName: bowtie2 {
+        ext.args = ''
+    }
+
+}
+                                
+
+NEXTFLOW
+
+      print F $configString;
+      close(F);
+  }
+}
+
+1;
