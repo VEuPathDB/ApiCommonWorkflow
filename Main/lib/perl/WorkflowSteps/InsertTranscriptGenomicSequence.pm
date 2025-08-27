061289e61dcceb6fda113c7e1eea0218457b0d49
steve-fischer-200 <sfischer@upenn.edu>
2025-08-27 12:42:22 -0400

    trans genomic seq

---
+++ Main/lib/perl/WorkflowSteps/InsertTranscriptGenomicSequence.pm
@@ -0,0 +1,26 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::InsertTranscriptGenomicSequence;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+  my ($self, $test, $undo) = @_;
+
+  my $organismAbbrev = $self->getParamValue('organismAbbrev');
+  my $gusConfigFile = $self->getParamValue('gusConfigFile');
+  my $project = $self->getParamValue('projectName');
+
+  my $schema = $self->getSharedConfig('webreadySchema');
+
+  my $workflowDataDir = $self->getWorkflowDataDir();
+
+  my $mode = $undo ? "delete" : "load";
+
+  my $cmd = "spliceSiteTranscripts.pl --gusConfigFile $workflowDataDir/$gusConfigFile --orgAbbrev $organismAbbrev --mode $mode --schema $schema --project $project";
+  $self->runCmd($test, $cmd);
+}
+
+1;
