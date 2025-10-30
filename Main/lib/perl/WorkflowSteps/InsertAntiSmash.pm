e92bad06c137f76022902a6a3256aa510b03ae21
Richard Demko <rdemko2332@gmail.com>
2025-10-30 12:57:10 -0400

    Creating insertantismash workflow step, changing functionality to resolve skip if file issue.

---
+++ Main/lib/perl/WorkflowSteps/InsertAntiSmash.pm
@@ -0,0 +1,28 @@
+B65;6800;1cpackage ApiCommonWorkflow::Main::WorkflowSteps::InsertAntiSmash;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my $workflowDataDir = $self->getWorkflowDataDir();
+
+    my $extDbSpec = $self->getParamValue('extDbSpec');
+
+    my $ncbiTaxonId = $self->getParamValue('ncbiTaxonId');
+
+    my $gffFile = $self->getParamValue('gffFile');
+
+    my $gffFileFullPath = "$workflowDataDir/$gffFile";
+
+    my $gusConfigFile = $workflowDataDir . "/" . $self->getParamValue('gusConfigFile');
+
+    my $args = " --gffFile $gffFileFullPath --ncbiTaxonId $ncbiTaxonId --extDbSpec $extDbSpec --gusConfigFile $gusConfigFile";
+
+    $self->testInputFile('inputGffFile', "$gffFileFullPath");
+    $self->runPlugin($test, $undo, "ApiCommonData::Load::Plugin::InsertAntiSmash", $args);
+
+}
