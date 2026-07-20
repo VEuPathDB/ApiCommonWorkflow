5cd9fe509c41a7ed31df4c760e518cfd9722fa69
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2026-07-20 13:28:03 -0400

    test tRNAScan filter

--- Main/lib/perl/WorkflowSteps/MakeTRNAScanConfig.pm
+++ Main/lib/perl/WorkflowSteps/MakeTRNAScanConfig.pm
@@ -17,6 +17,20 @@ sub run {
   my $trnascanGFFFileName = $self->getParamValue("outputGFFName");
 
   my $workingDirRelativePath = $self->getParamValue("workingDirRelativePath");
+  my $organismAbbrev = $self->getParamValue("organismAbbrev");
+  my $gusConfigFile = $self->getWorkflowDataDir() . "/" . $self->getParamValue("gusConfigFile");
+  #print STDERR "\$gusConfigFile = $gusConfigFile\n";
+
+  # use genus here for repeatMaskSpecies 
+  my $repeatMaskSpecies = $self->getOrganismInfo($test, $organismAbbrev, $gusConfigFile)->getFullName();
+  $repeatMaskSpecies =~ s/(\S+?)\s.*/$1/; 
+  #print STDERR "\$repeatMaskSpecies = $repeatMaskSpecies\n";
+
+  # Minimum tRNAscan-SE Inf score to retain a prediction (both filter paths)
+  # 60 bits is conservative and reliable across eukaryotes
+  # Lower to 50 only if a curated reference shows genuine tRNAs below 60
+  # Default 60 bits; lower only if a well-annotated
+  my $minInfScore = 60;
 
   my $workflowDataDir = $self->getWorkflowDataDir();
 
@@ -29,26 +43,37 @@ sub run {
   if ($undo) {
       $self->runCmd(0, "rm $workflowDataDir/$nextflowConfigFile");
   } else {
-      my $genomicSequenceFileInNextflowWorkingDirOnCluster = $self->relativePathToNextflowClusterPath($workingDirRelativePath, $genomicSequenceFile);
-      my $resultsDirectoryInNextflowWorkingDirOnCluster = $self->relativePathToNextflowClusterPath($workingDirRelativePath, $resultsDirectory);
-      my $nextflowConfig = "$workflowDataDir/$nextflowConfigFile";
-      open(F, ">$nextflowConfig") || die "Can't open task prop file '$nextflowConfig' for writing";
 
-      my $configString = <<NEXTFLOW;
+    my $genomicSequenceFileOnCluster = $self->relativePathToNextflowClusterPath($workingDirRelativePath, $genomicSequenceFile);
+    my $resultsDirectoryOnCluster = $self->relativePathToNextflowClusterPath($workingDirRelativePath, $resultsDirectory);
+
+    my $applyHighConfFilter = "true";
+    my $applyRepeatMask     = "true";
+
+    my $nextflowConfig = "$workflowDataDir/$nextflowConfigFile";
+    open(F, ">$nextflowConfig") || die "Cannot open '$nextflowConfig' for writing\n";
+
+    print F <<NEXTFLOW;
 params {
-  inputFilePath = "$genomicSequenceFileInNextflowWorkingDirOnCluster"
-  outputDir = "$resultsDirectoryInNextflowWorkingDirOnCluster"
+  inputFilePath = "$genomicSequenceFileOnCluster"
+  outputDir = "$resultsDirectoryOnCluster"
   outputFileName = "$trnascanOutputFileName"
   outputGFFName = "$trnascanGFFFileName"
   fastaSubsetSize = $fastaSubsetSize
+
+  applyHighConfFilter = $applyHighConfFilter
+  applyRepeatMask = $applyRepeatMask
+
+  repeatMaskSpecies = "$repeatMaskSpecies"
+
+  minInfScore = $minInfScore
 }
 
 includeConfig "$clusterConfigFile"
 
 NEXTFLOW
 
-      print F $configString;
-      close(F);
+    close F;
   }
 }
 
