bc3799d28bccd3033e8e8685d028e179ecff1adc
John Brestelli <jbrestell@gmail.com>
2026-07-31 14:38:02 -0400

    for mergeExperiment, ignore the configured branch for dnaseq and always use main; this is always rerun; artifacts not cached

---
+++ Main/lib/perl/WorkflowSteps/RunNextflowLocalMergeDNASeq.pm
@@ -0,0 +1,23 @@
+package ApiCommonWorkflow::Main::WorkflowSteps::RunNextflowLocalMergeDNASeq;
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::RunNextflowLocal);
+
+use strict;
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+# OVERRIDE
+# We always want to use main for for mergeExperiments dnaseq nextflow workflow
+# Only the processSingleExperiment should use the tag
+sub getSharedConfigRelaxed {
+    my ($self, $key) = @_;
+
+    if($key eq 'VEuPathDB/dnaseq-nextflow') {
+        return undef;
+    }
+
+    return $self->SUPER::getSharedConfigRelaxed($key);
+}
+
+
+1;
